e4cc7900c7967dc65aa439885101977418bd1e81
Antoine Tenart <atenart@redhat.com>
2022-09-28 14:46:59 +0200

    collector: new module
    Collectors are modules gathering information, mainly collecting events
    and/or appropriate data; they are at the core of the tool. This adds an
    initial implementation to allow registering collectors, using a Trait to
    describe their common behaviour as well as defining a way to manipulate
    a collection of collectors.
    
    Please look at the patch itself for more insights about the
    implementation (as it is documented in lengths there already).
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use anyhow::Result;
 
+pub(crate) mod collector;
+use collector::get_collectors;
+
 fn main() -> Result<()> {
+    let mut collectors = get_collectors()?;
+    collectors.init()?;
+    collectors.start()?;
     Ok(())
 }
