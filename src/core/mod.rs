bfad44c053c1e35a3222a7a13f74785a94181944
Antoine Tenart <atenart@redhat.com>
2022-09-30 19:02:11 +0200

    core: add a kernel_symbol module
    Simple interface to get kernel symbols addresses from their name and the
    opposite. This will be useful for probing the kernel, from configuring
    kprobes to understanding where an event is coming from.
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

---
+++ src/core/mod.rs
@@ -0,0 +1,6 @@
+//! # Core
+//!
+//! Core module, providing tools and common logic that can be used by any module
+//! within the tool.
+
+pub(crate) mod kernel_symbols;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
 
+pub(crate) mod core;
+
 fn main() -> Result<()> {
     Ok(())
 }
