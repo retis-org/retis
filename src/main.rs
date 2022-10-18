f9d77f3fc4c5ae02527d64a0a8f0475b77f5db5a
Antoine Tenart <atenart@redhat.com>
2022-10-18 17:40:56 +0200

    main: use collectors
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use anyhow::Result;
 
 mod collector;
 mod core;
+use collector::get_collectors;
 
 fn main() -> Result<()> {
+    let mut collectors = get_collectors()?;
+    collectors.init()?;
+    collectors.start()?;
     Ok(())
 }
