10a8d02aa53079af9b965b4aa49f99851c04ad93
Adrian Moreno <amorenoz@redhat.com>
2022-11-16 14:17:12 +0100

    cli: implement thin and full cli parsing
    Implement two-fold cli parsing.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -3,12 +3,11 @@
 //! Cli module, providing tools for registering and accessing command line interface arguments
 //! as well as defining the subcommands that the tool supports.
 #![allow(dead_code)] // FIXME
-                     //
-use anyhow::Result;
-use std::any::Any;
+use std::{any::Any, collections::HashMap, env, ffi::OsString, fmt::Debug};
 
+use anyhow::{bail, Result};
 use clap::error::Error as ClapError;
-use clap::{error::ErrorKind, ArgMatches, Args, Command};
+use clap::{ArgMatches, Args, Command, FromArgMatches};
 
 use super::dynamic::DynamicCommand;
 
@@ -63,6 +62,12 @@ pub(crate) trait SubCommand {
     }
 }
 
+impl Debug for dyn SubCommand {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "SubCommand ({})", self.name())
+    }
+}
+
 /// Trace packets on the Linux kernel
 ///
 /// packet-tracer is a tool for capturing networking-related events from the system using ebpf and analyzing them.
@@ -74,13 +79,86 @@ pub(crate) struct MainConfig {}
 /// During this phase, SubCommands can be added. After all SubCommands have been added, the build()
 /// method will run the thin CLI parsing that does not perform dynamic subcommand argument
 /// validation and yield a FullCli object to represent the results.
-pub(crate) struct ThinCli {}
+#[derive(Debug)]
+pub(crate) struct ThinCli {
+    subcommands: HashMap<String, Box<dyn SubCommand>>,
+}
 
 impl ThinCli {
-    /// Builds a FullCli by running a first round of CLI parsing without subcommand argument
+    /// Allocate and return a new ThinCli object that will parse the command arguments.
+    pub(crate) fn new() -> Result<Self> {
+        Ok(ThinCli {
+            subcommands: HashMap::new(),
+        })
+    }
+
+    /// Add a subcommand to the ThinCli object.
+    pub(crate) fn add_subcommand(&mut self, sub: Box<dyn SubCommand>) -> Result<&mut Self> {
+        let name = sub.name().to_string();
+        if self.subcommands.get(&name).is_some() {
+            bail!("Subcommand already registered")
+        }
+        self.subcommands.insert(name, sub);
+        Ok(self)
+    }
+
+    /// Build a FullCli by running a first round of CLI parsing without subcommand argument
     /// validation.
     pub(crate) fn build(self) -> Result<FullCli, ClapError> {
-        Err(ClapError::new(ErrorKind::DisplayHelp))
+        self.build_from(env::args_os(), false)
+    }
+
+    /// Build a FullCli by running a first round of CLI parsing with the given list of arguments.
+    pub(crate) fn build_from<I, T>(mut self, args: I, dry_run: bool) -> Result<FullCli, ClapError>
+    where
+        I: IntoIterator<Item = T>,
+        T: Into<OsString> + Clone,
+    {
+        let args: Vec<OsString> = args.into_iter().map(|x| x.into()).collect();
+        let mut command =
+            MainConfig::augment_args(Command::new("packet-tracer")).subcommand_required(true);
+        // Add thin subcommands so that the main help shows them.
+        for (_, sub) in self.subcommands.iter() {
+            command = command.subcommand(sub.thin().expect("thin command failed"));
+        }
+
+        // Determine the subcommand that was run while ignoring errors from yet-to-be-defined
+        // arguments.
+        let matches = command
+            .clone()
+            .ignore_errors(true)
+            .try_get_matches_from(args.iter())?;
+
+        let ran_subcommand = matches.subcommand_name();
+
+        if ran_subcommand.is_none()
+            || self
+                .subcommands
+                .get(&ran_subcommand.unwrap().to_string())
+                .is_none()
+        {
+            // There is no subcommand or it's invalid. Let clap handle this error since it prints
+            // nicer error messages and knows where they should be printed to.
+            let err = command
+                .try_get_matches_from_mut(args.iter())
+                .expect_err("clap should fail with no arguments");
+
+            match dry_run {
+                true => return Err(err),
+                false => err.exit(),
+            };
+        }
+
+        // A command was run, build the FullCli so we can parse it.
+        Ok(FullCli {
+            args,
+            command,
+            dry_run,
+            subcommand: self
+                .subcommands
+                .remove(&ran_subcommand.unwrap().to_string())
+                .unwrap(),
+        })
     }
 }
 
@@ -89,19 +167,219 @@ impl ThinCli {
 /// When a FullCli is created it can be used to dynamically add command line arguments to the
 /// SubCommand that was ran. After this phase, a call to run() will perform the full argument
 /// validation.
-pub(crate) struct FullCli {}
+#[derive(Debug)]
+pub(crate) struct FullCli {
+    args: Vec<OsString>,
+    command: Command,
+    dry_run: bool,
+    subcommand: Box<dyn SubCommand>,
+}
 
 impl FullCli {
     /// Perform full CLI parsing and validation
-    pub(crate) fn run(self) -> Result<CliResults, ClapError> {
-        Err(ClapError::new(ErrorKind::DisplayHelp))
+    pub(crate) fn run(mut self) -> Result<CliConfig, ClapError> {
+        // Replace the ran subcommand with the full subcommand.
+        self.command = self
+            .command
+            .mut_subcommand(self.subcommand.name(), |_| self.subcommand.full().unwrap());
+
+        // Get the matches.
+        let matches = match self.dry_run {
+            true => self.command.try_get_matches_from_mut(self.args.iter())?,
+            false => self
+                .command
+                .try_get_matches_from_mut(self.args.iter())
+                .unwrap_or_else(|e| e.exit()),
+        };
+
+        // Get main config.
+        let mut main_config = MainConfig::default();
+        main_config.update_from_arg_matches(&matches)?;
+
+        let (subcommand, matches) = matches
+            .subcommand()
+            .expect("full parsing did not find subcommand");
+        if !subcommand.to_string().eq(self.subcommand.as_ref().name()) {
+            // thin and full cli parsing should yield the same subcommand. There is no way to
+            // recover from this error, so let's just panic.
+            panic!("Thin and full parsing did not yield the same subcommand");
+        }
+
+        // Update subcommand options.
+        match self.dry_run {
+            true => self.subcommand.update_from_arg_matches(matches)?,
+            false => self
+                .subcommand
+                .update_from_arg_matches(matches)
+                .unwrap_or_else(|e| e.exit()),
+        }
+        Ok(CliConfig {
+            main_config,
+            subcommand: self.subcommand,
+        })
+    }
+
+    pub(crate) fn get_subcommand(&self) -> Result<&dyn SubCommand> {
+        Ok(self.subcommand.as_ref())
+    }
+
+    pub(crate) fn get_subcommand_mut(&mut self) -> Result<&mut dyn SubCommand> {
+        Ok(self.subcommand.as_mut())
     }
 }
 
-/// CliResult represents the result of the Full CLI parsing
-pub(crate) struct CliResults {}
+/// CliConfig represents the result of the Full CLI parsing
+#[derive(Debug)]
+pub(crate) struct CliConfig {
+    pub(crate) main_config: MainConfig,
+    pub(crate) subcommand: Box<dyn SubCommand>,
+}
 
 /// Create and register a ThinCli
 pub(crate) fn get_cli() -> Result<ThinCli> {
-    Ok(ThinCli {})
+    let cli = ThinCli::new()?;
+    Ok(cli)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use clap::error::ErrorKind;
+
+    #[derive(Debug, Default, Args)]
+    struct Sub1 {
+        #[arg(id = "sub1-arg", long)]
+        someopt: Option<String>,
+    }
+
+    impl SubCommand for Sub1 {
+        fn new() -> Result<Self> {
+            Ok(Sub1 { someopt: None })
+        }
+        fn name(&self) -> &'static str {
+            "sub1"
+        }
+        fn as_any(&self) -> &dyn Any {
+            self
+        }
+        fn thin(&self) -> Result<Command> {
+            Ok(Command::new("sub1").about("does some things"))
+        }
+        fn full(&self) -> Result<Command> {
+            Ok(Sub1::augment_args(
+                Command::new("sub1")
+                    .about("does some things")
+                    .long_about("this is a longer description"),
+            ))
+        }
+        fn update_from_arg_matches(&mut self, matches: &ArgMatches) -> Result<(), ClapError> {
+            <Self as FromArgMatches>::update_from_arg_matches(self, matches)
+        }
+    }
+
+    #[derive(Debug, Default, Args)]
+    struct Sub2 {
+        #[arg(id = "sub2-flag", long)]
+        flag: Option<bool>,
+    }
+
+    impl SubCommand for Sub2 {
+        fn new() -> Result<Self> {
+            Ok(Sub2 { flag: Some(false) })
+        }
+        fn name(&self) -> &'static str {
+            "sub2"
+        }
+        fn as_any(&self) -> &dyn Any {
+            self
+        }
+        fn thin(&self) -> Result<Command> {
+            Ok(Command::new("sub2").about("does some things"))
+        }
+        fn full(&self) -> Result<Command> {
+            Ok(Sub2::augment_args(
+                Command::new("sub2")
+                    .about("does some things")
+                    .long_about("this is a longer description"),
+            ))
+        }
+        fn update_from_arg_matches(&mut self, matches: &ArgMatches) -> Result<(), ClapError> {
+            <Self as FromArgMatches>::update_from_arg_matches(self, matches)
+        }
+    }
+
+    #[test]
+    fn cli_register_subcommands() -> Result<()> {
+        let mut cli = ThinCli::new()?;
+        assert!(cli.add_subcommand(Box::new(Sub1::new()?)).is_ok());
+        assert!(cli.add_subcommand(Box::new(Sub2::new()?)).is_ok());
+        assert!(cli.add_subcommand(Box::new(Sub1::new()?)).is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn cli_build() -> Result<()> {
+        let mut cli = ThinCli::new()?;
+        assert!(cli.add_subcommand(Box::new(Sub1::new()?)).is_ok());
+        assert!(cli.add_subcommand(Box::new(Sub2::new()?)).is_ok());
+
+        let err = cli.build_from(vec!["packet-tracer", "--help"], true);
+        assert!(err.is_err() && err.unwrap_err().kind() == ErrorKind::DisplayHelp);
+
+        Ok(())
+    }
+
+    #[test]
+    fn cli_full_help() -> Result<()> {
+        let mut cli = ThinCli::new()?;
+        assert!(cli.add_subcommand(Box::new(Sub1::new()?)).is_ok());
+        assert!(cli.add_subcommand(Box::new(Sub2::new()?)).is_ok());
+
+        let cli = cli.build_from(vec!["packet-tracer", "sub1", "--help"], true);
+        assert!(cli.is_ok());
+
+        let err = cli?.run();
+        assert!(err.is_err() && err.unwrap_err().kind() == ErrorKind::DisplayHelp);
+
+        Ok(())
+    }
+
+    #[test]
+    fn cli_sub_args() -> Result<()> {
+        let mut cli = ThinCli::new()?;
+        assert!(cli.add_subcommand(Box::new(Sub1::new()?)).is_ok());
+        assert!(cli.add_subcommand(Box::new(Sub2::new()?)).is_ok());
+
+        let cli = cli.build_from(vec!["packet-tracer", "sub1", "--sub1-arg", "foo"], true);
+        assert!(cli.is_ok());
+        let cli = cli.unwrap();
+        assert!(cli.get_subcommand().is_ok() && cli.get_subcommand().unwrap().name().eq("sub1"));
+
+        let res = cli.run();
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert!(res.subcommand.name().eq("sub1"));
+        let sub1 = res.subcommand.as_any().downcast_ref::<Sub1>();
+        assert!(sub1.is_some());
+        assert!(sub1.unwrap().someopt.as_ref().unwrap().eq("foo"));
+
+        Ok(())
+    }
+
+    #[test]
+    fn cli_sub_args_err() -> Result<()> {
+        let mut cli = ThinCli::new()?;
+        assert!(cli.add_subcommand(Box::new(Sub1::new()?)).is_ok());
+        assert!(cli.add_subcommand(Box::new(Sub2::new()?)).is_ok());
+
+        let cli = cli.build_from(vec!["packet-tracer", "sub1", "--noexists", "foo"], true);
+        assert!(cli.is_ok());
+        let cli = cli.unwrap();
+        assert!(cli.get_subcommand().is_ok() && cli.get_subcommand().unwrap().name().eq("sub1"));
+
+        let res = cli.run();
+        assert!(res.is_err() && res.unwrap_err().kind() == ErrorKind::UnknownArgument);
+
+        Ok(())
+    }
 }
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -5,7 +5,7 @@
 //!
 //! The main feature that this module adds on top of clap's CLI parsing is the ability to
 //! dynamically register module-specific arguments in a way that does not require this module to
-//! depend on the rest. In addition, since the Cli supporst subcommands, it's possible to only
+//! depend on the rest. In addition, since the Cli supports subcommands, it's possible to only
 //! register arguments to the subcommand that is actually run.
 //!
 //! In order to achieve this, the command line arguments have to be parsed twice. One first round,
