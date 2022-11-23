2daef3a4bc38cf19c7d0dfe84e02f706e09d3237
Antoine Tenart <atenart@redhat.com>
2022-11-23 16:18:43 +0100

    core: add events support
    The event support is split in two parts, one handling the BPF<>Rust
    communication and one implementing an internal representation.
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -3,6 +3,7 @@
 //! Core module, providing tools and common logic that can be used by any module
 //! within the tool.
 
+pub(crate) mod events;
 pub(crate) mod kernel_symbols;
 pub(crate) mod probe;
 pub(crate) mod workaround;
