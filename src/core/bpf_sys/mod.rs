15a7e636d85fc2c4edd08beeb6a901d77cc5ba22
Paolo Valerio <pvalerio@redhat.com>
2023-01-13 13:37:33 +0100

    core: add the ability to interact with the bpf subsys directly through bpf(2)
    Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

---
+++ src/core/bpf_sys/mod.rs
@@ -0,0 +1,6 @@
+//! # bpf_sys
+//!
+//! Module providing facilities to load non-ELF programs.
+
+#[allow(clippy::module_inception)]
+pub(crate) mod bpf_sys;
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -3,6 +3,7 @@
 //! Core module, providing tools and common logic that can be used by any module
 //! within the tool.
 
+pub(crate) mod bpf_sys;
 pub(crate) mod events;
 pub(crate) mod kernel;
 pub(crate) mod probe;
