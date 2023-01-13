3bf4f17785221671495c4d4f163ecf4a55f411fa
Paolo Valerio <pvalerio@redhat.com>
2023-01-13 13:37:33 +0100

    build.rs: turn INCLUDE_PATHS to a const array of strings
    Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ use std::{
 use libbpf_cargo::SkeletonBuilder;
 use memmap2::Mmap;
 
-static INCLUDE_PATHS: &[&str] = &[
+const INCLUDE_PATHS: &[&str] = &[
     "src/core/probe/kernel/bpf/include",
     "src/core/events/bpf/include",
 ];
