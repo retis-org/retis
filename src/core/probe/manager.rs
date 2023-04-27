061c72fbc1746559029766ec487b90fe28ef2fa0
Antoine Tenart <atenart@redhat.com>
2023-04-27 09:19:02 +0200

    core: probe: manager: rework the internals
    Main ideas:
    
    - Stop initializing probe builder early and in the manager, rather do it
      on demand while attaching the probes. This helps a lot for managing
      probes & hooks in the manager.
    
    - Stop indexing probes by their type at registration time. This simplify
      the logic a lot and is now possible thanks to the previous point.
    
    - Stop handling everything through a probe set. Generic probes & hooks
      can be stored as-is and will augment some probe sets at attach time.
    
    All in all the above helps simplifying the logic and allow to better
    work with probes & hooks in the manager.
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- src/core/probe/manager.rs
+++ src/core/probe/manager.rs
@@ -21,56 +21,51 @@ pub(super) const HOOK_MAX: usize = 10;
 /// ProbeManager is the main object providing an API for consumers to register probes, hooks, maps,
 /// etc.
 pub(crate) struct ProbeManager {
-    /// Dynamic ProbeSets, one per probe type. Used to keep track of all non-specific
-    /// probes.
-    dynamic_probes: [ProbeSet; PROBE_VARIANTS],
+    /// All generic probes.
+    generic_probes: HashMap<String, Probe>,
 
-    /// Dynamic Hooks. A list of hooks to be attached to all dynamic probes.
-    dynamic_hooks: Vec<Hook>,
+    /// Generic hooks, meant to be attached to all probes supporting it..
+    generic_hooks: Vec<Hook>,
 
-    /// Dynamic Filters. A list of filters to be attached to all dynamic probes.
+    /// Filters, meant to be attached to all probes.
     filters: Vec<Filter>,
+
+    /// Targeted sets, for hooks requesting to be specifically attached to a set
+    /// of probes. Those might also support generic hooks.
+    targeted_probes: Vec<ProbeSet>,
+
+    /// List of global probe options to enable/disable additional probes behavior at a high level.
+    global_probes_options: Vec<ProbeOption>,
+
+    /// HashMap of map names and file descriptors, to be reused in all hooks.
+    maps: HashMap<String, i32>,
 
     /// Dynamic probes requires a map that provides extra information at runtime. This is that map.
     #[cfg(not(test))]
     config_map: libbpf_rs::Map,
 
-    /// List of options to enable/disable additional probes behavior.
-    options: Vec<ProbeOption>,
-
-    // Targeted probes.
-    /// List of targeted probes, aka. probes running a specific set of hooks.
-    /// Targeted probes only have one target to keep things reasonable.
-    targeted_probes: [Vec<ProbeSet>; PROBE_VARIANTS],
-
-    // Common (both dynamic and targetted) data.
-    /// Maps. HashMap of map names and file descriptors
-    maps: HashMap<String, i32>,
+    /// Internal vec to store "used" probe builders, so we can keep a reference
+    /// on them and keep probes loaded & installed.
+    // TODO: should we change the builders to return the libbpf_rs::Link
+    // directly?
+    builders: Vec<Box<dyn ProbeBuilder>>,
 }
 
 impl ProbeManager {
     pub(crate) fn new() -> Result<ProbeManager> {
-        // Keep synced with the order of Probe::into::<usize>()!
-        let dynamic_probes: [ProbeSet; probe::PROBE_VARIANTS] = [
-            ProbeSet::new(Box::new(kprobe::KprobeBuilder::new()), true),
-            ProbeSet::new(Box::new(kretprobe::KretprobeBuilder::new()), true),
-            ProbeSet::new(Box::new(raw_tracepoint::RawTracepointBuilder::new()), true),
-            ProbeSet::new(Box::new(usdt::UsdtBuilder::new()), false),
-        ];
-        let targeted_probes: [Vec<ProbeSet>; probe::PROBE_VARIANTS] = Default::default();
-
         // When testing the kernel object is not modified later to reuse the
         // config map is this map is hidden.
         #[allow(unused_mut)]
         let mut mgr = ProbeManager {
-            dynamic_probes,
-            dynamic_hooks: Vec::new(),
+            generic_probes: HashMap::new(),
+            generic_hooks: Vec::new(),
             filters: Vec::new(),
+            targeted_probes: Vec::new(),
+            global_probes_options: Vec::new(),
+            maps: HashMap::new(),
             #[cfg(not(test))]
             config_map: init_config_map()?,
-            options: Vec::new(),
-            targeted_probes,
-            maps: HashMap::new(),
+            builders: Vec::new(),
         };
 
         #[cfg(not(test))]
@@ -85,14 +80,14 @@ impl ProbeManager {
     /// which version to keep.
     pub(crate) fn set_probe_opt(&mut self, opt: ProbeOption) -> Result<()> {
         if self
-            .options
+            .global_probes_options
             .iter()
             .any(|o| std::mem::discriminant(o) == std::mem::discriminant(&opt))
         {
             bail!("Option is already set");
         }
 
-        self.options.push(opt);
+        self.global_probes_options.push(opt);
         Ok(())
     }
 
@@ -106,28 +101,18 @@ impl ProbeManager {
     /// mgr.add_probe(Probe::raw_tracepoint(symbol).unwrap()).unwrap();
     /// ```
     pub(crate) fn add_probe(&mut self, probe: Probe) -> Result<()> {
-        let key = match &probe {
-            Probe::Kprobe(probe) | Probe::Kretprobe(probe) | Probe::RawTracepoint(probe) => {
-                probe.symbol.name()
-            }
-            Probe::Usdt(probe) => probe.name(),
-        };
+        let key = probe.key();
 
-        // First check if it is already in the generic probe list.
-        let probe_set = &mut self.dynamic_probes[probe.as_key()];
-        if probe_set.targets.contains_key(&key) {
-            return Ok(());
-        }
-
-        // Then if it is already in the targeted probe list.
-        let tgt_set = &mut self.targeted_probes[probe.as_key()];
-        for set in tgt_set.iter_mut() {
-            if set.targets.get(&key).is_some() {
+        // Check if it is already in the targeted probe list.
+        for set in self.targeted_probes.iter() {
+            if set.probes.contains_key(&key) {
                 return Ok(());
             }
         }
 
-        probe_set.targets.insert(key, probe);
+        // If not, insert it in the generic probe list, if not there already.
+        self.generic_probes.entry(key).or_insert(probe);
+
         Ok(())
     }
 
@@ -181,18 +166,17 @@ impl ProbeManager {
     /// ```
     pub(crate) fn register_kernel_hook(&mut self, hook: Hook) -> Result<()> {
         let mut max: usize = 0;
-        for tgt_set in self.targeted_probes.iter_mut() {
-            for set in tgt_set.iter_mut() {
-                if max < set.hooks.len() {
-                    max = set.hooks.len();
-                }
+        self.targeted_probes.iter_mut().for_each(|set| {
+            if max < set.hooks.len() {
+                max = set.hooks.len();
             }
-        }
-        if self.dynamic_hooks.len() + max == HOOK_MAX {
+        });
+
+        if self.generic_hooks.len() + max == HOOK_MAX {
             bail!("Hook list is already full");
         }
 
-        self.dynamic_hooks.push(hook);
+        self.generic_hooks.push(hook);
         Ok(())
     }
 
@@ -209,132 +193,135 @@ impl ProbeManager {
     /// mgr.register_hook_to(hook::DATA, Probe::kprobe(symbol).unwrap()).unwrap();
     /// ```
     pub(crate) fn register_hook_to(&mut self, hook: Hook, probe: Probe) -> Result<()> {
-        let key = match &probe {
-            Probe::Kprobe(probe) | Probe::Kretprobe(probe) | Probe::RawTracepoint(probe) => {
-                probe.symbol.name()
-            }
-            Probe::Usdt(probe) => probe.name(),
-        };
+        let key = probe.key();
 
         // First check if the target isn't already registered to the generic
         // probes list. If so, remove it from there.
-        let probe_set = &mut self.dynamic_probes[probe.as_key()];
-        probe_set.targets.remove(&key);
+        self.generic_probes.remove(&key);
 
         // Now check if we already have a targeted probe for this. If so, append
         // the new hook to it.
-        let tgt_set = &mut self.targeted_probes[probe.as_key()];
-        for set in tgt_set.iter_mut() {
-            if set.targets.get(&key).is_some() {
+        for set in self.targeted_probes.iter_mut() {
+            if set.probes.contains_key(&key) {
                 if let Probe::Usdt(_) = probe {
-                    bail!("USDT probes only support a single hook")
+                    bail!("USDT probes only support a single hook");
                 }
-                if self.dynamic_hooks.len() + set.hooks.len() == HOOK_MAX {
+
+                if self.generic_hooks.len() + set.hooks.len() == HOOK_MAX {
                     bail!("Hook list is already full");
                 }
+
                 set.hooks.push(hook);
                 return Ok(());
             }
         }
 
         // New target, let's build a new probe set.
-        let mut set = match probe {
-            Probe::Kprobe(_) => ProbeSet::new(Box::new(kprobe::KprobeBuilder::new()), true),
-            Probe::Kretprobe(_) => {
-                ProbeSet::new(Box::new(kretprobe::KretprobeBuilder::new()), true)
-            }
-            Probe::RawTracepoint(_) => {
-                ProbeSet::new(Box::new(raw_tracepoint::RawTracepointBuilder::new()), true)
-            }
-            Probe::Usdt(_) => ProbeSet::new(Box::new(usdt::UsdtBuilder::new()), false),
+        let mut set = ProbeSet {
+            supports_generic_hooks: match &probe {
+                Probe::Kprobe(_) | Probe::Kretprobe(_) | Probe::RawTracepoint(_) => true,
+                Probe::Usdt(_) => false,
+            },
+            ..Default::default()
         };
+        set.probes.insert(key, probe);
 
-        set.targets.insert(key, probe);
-
-        if self.dynamic_hooks.len() == HOOK_MAX {
+        if self.generic_hooks.len() == HOOK_MAX {
             bail!("Hook list is already full");
         }
         set.hooks.push(hook);
 
-        tgt_set.push(set);
+        self.targeted_probes.push(set);
         Ok(())
     }
 
     /// Attach all probes.
     pub(crate) fn attach(&mut self) -> Result<()> {
-        // Take care of generic probes first.
-        for set in self.dynamic_probes.iter_mut() {
-            set.hooks = self.dynamic_hooks.clone();
-            set.filters = self.filters.clone();
-            set.attach(
-                #[cfg(not(test))]
-                &mut self.config_map,
-                self.maps.clone(),
-                #[cfg(not(test))]
-                &self.options,
-            )?;
-        }
+        // First, handle generic probes.
+        let mut set = ProbeSet {
+            probes: self.generic_probes.clone(),
+            hooks: self.generic_hooks.clone(),
+            supports_generic_hooks: true,
+        };
+        self.builders.extend(set.attach(
+            &self.filters,
+            #[cfg(not(test))]
+            &mut self.config_map,
+            self.maps.clone(),
+            #[cfg(not(test))]
+            &self.global_probes_options,
+        )?);
 
-        // Then take care of targeted probes.
-        for tgt_probe in self.targeted_probes.iter_mut() {
-            for set in tgt_probe.iter_mut() {
-                // Extend targeted hooks with dynamic ones.
-                if set.supports_dynamic {
-                    set.hooks.extend(self.dynamic_hooks.iter().cloned());
-                    set.filters = self.filters.clone();
+        // Then targeted ones.
+        self.targeted_probes
+            .iter_mut()
+            .try_for_each(|set| -> Result<()> {
+                if set.supports_generic_hooks {
+                    set.hooks.extend(self.generic_hooks.clone());
                 }
-                set.attach(
+                self.builders.extend(set.attach(
+                    &self.filters,
                     #[cfg(not(test))]
                     &mut self.config_map,
                     self.maps.clone(),
                     #[cfg(not(test))]
-                    &self.options,
-                )?;
-            }
-        }
+                    &self.global_probes_options,
+                )?);
+                Ok(())
+            })?;
 
         Ok(())
     }
 }
 
+#[derive(Default)]
 struct ProbeSet {
-    builder: Box<dyn ProbeBuilder>,
-    targets: HashMap<String, Probe>,
+    probes: HashMap<String, Probe>,
     hooks: Vec<Hook>,
-    filters: Vec<Filter>,
-    supports_dynamic: bool,
+    supports_generic_hooks: bool,
 }
 
 impl ProbeSet {
-    /// Creates a new empty ProbeSet.
-    fn new(builder: Box<dyn ProbeBuilder>, supports_dynamic: bool) -> ProbeSet {
-        ProbeSet {
-            builder,
-            targets: HashMap::new(),
-            hooks: Vec::new(),
-            filters: Vec::new(),
-            supports_dynamic,
-        }
-    }
-
     /// Attach all the probes and hook in the ProbeSet.
     fn attach(
         &mut self,
+        filters: &[Filter],
         #[cfg(not(test))] config_map: &mut libbpf_rs::Map,
         maps: HashMap<String, i32>,
         #[cfg(not(test))] options: &[ProbeOption],
-    ) -> Result<()> {
-        if self.targets.is_empty() {
-            return Ok(());
+    ) -> Result<Vec<Box<dyn ProbeBuilder>>> {
+        if self.probes.is_empty() {
+            debug!("No probe in probe set");
+            return Ok(Vec::new());
         }
 
-        // Initialize the probe builder, only once for all targets.
-        let map_fds = maps.into_iter().collect();
-        self.builder
-            .init(map_fds, self.hooks.clone(), self.filters.clone())?;
+        let mut builders: HashMap<usize, Box<dyn ProbeBuilder>> = HashMap::new();
+        let map_fds: Vec<(String, i32)> = maps.into_iter().collect();
 
-        // Then handle all probes in the set.
-        for (_, probe) in self.targets.iter_mut() {
+        self.probes.iter_mut().try_for_each(|(_, probe)| {
+            // Make a new builder if none if found for the current type. Builder
+            // are shared for all probes of the same type within this set.
+            match builders.contains_key(&probe.type_key()) {
+                false => {
+                    let mut builder: Box<dyn ProbeBuilder> = match probe {
+                        Probe::Kprobe(_) => Box::new(kprobe::KprobeBuilder::new()),
+                        Probe::Kretprobe(_) => Box::new(kretprobe::KretprobeBuilder::new()),
+                        Probe::RawTracepoint(_) => {
+                            Box::new(raw_tracepoint::RawTracepointBuilder::new())
+                        }
+                        Probe::Usdt(_) => Box::new(usdt::UsdtBuilder::new()),
+                    };
+
+                    // Initialize the probe builder, only once for all targets.
+                    builder.init(map_fds.clone(), self.hooks.clone(), filters.to_owned())?;
+
+                    builders.insert(probe.type_key(), builder);
+                }
+                true => (),
+            }
+            // Unwrap as we just made sure the probe builder would be available.
+            let builder = builders.get_mut(&probe.type_key()).unwrap();
+
             // First load the probe configuration.
             #[cfg(not(test))]
             match probe {
@@ -350,13 +337,12 @@ impl ProbeSet {
 
             // Finally attach a probe to the target.
             debug!("Attaching probe to {}", probe);
-            self.builder.attach(probe)?;
-        }
+            builder.attach(probe)
+        })?;
 
         // All probes loaded, issue an info log.
-        info!("{} probe(s) loaded", self.targets.len());
-
-        Ok(())
+        info!("{} probe(s) loaded", self.probes.len());
+        Ok(builders.drain().map(|(_, v)| v).collect())
     }
 }
 
