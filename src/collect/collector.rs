amorenoz/packet-tracer

core: refactor garbage collector into common struct
Sharing code between ovs and skb tracking

Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

src/collect/collector.rs
@@ -20,7 +20,7 @@ use crate::{
         },
         kernel::Symbol,
         probe::{self, Probe, ProbeManager},
-        tracking::skb_tracking::init_tracking,
+        tracking::{garbage::TrackingGC, skb_tracking::init_tracking},
     },
     module::{get_modules, ModuleId, Modules},
 };
@@ -66,6 +66,7 @@ pub(crate) struct Collectors {
     cli: CliConfig,
     factory: Box<dyn EventFactory>,
     known_kernel_types: HashSet<String>,
+    tracking_gc: Option<TrackingGC>,
 }
 
 impl Collectors {
@@ -104,6 +105,7 @@ impl Collectors {
             known_kernel_types: HashSet::new(),
             cli,
             factory,
+            tracking_gc: None,
         })
     }
 
@@ -154,7 +156,7 @@ impl Collectors {
 
         // Initialize tracking & filters.
         if self.known_kernel_types.contains("struct sk_buff *") {
-            init_tracking(&mut self.probes)?;
+            self.tracking_gc = Some(init_tracking(&mut self.probes)?);
         }
         Self::setup_filters(&mut self.probes, collect)?;
 

retis-org/retis

src/collect/collector.rs
@@ -1,7 +1,6 @@
 use std::{
     collections::HashSet,
     process::{Command, Stdio},
-    thread::JoinHandle,
     time::Duration,
 };
 
@@ -25,7 +24,7 @@ use crate::{
         kernel::{symbol::matching_functions_to_symbols, Symbol},
         probe::{self, Probe, ProbeManager},
         signals::Running,
-        tracking::skb_tracking::init_tracking,
+        tracking::{gc::TrackingGC, skb_tracking::init_tracking},
     },
     module::{get_modules, ModuleId, Modules},
 };
@@ -83,8 +82,8 @@ pub(crate) struct Collectors {
     cli: CliConfig,
     factory: Box<dyn EventFactory>,
     known_kernel_types: HashSet<String>,
-    gc_handle: Option<JoinHandle<()>>,
     run: Running,
+    tracking_gc: Option<TrackingGC>,
 }
 
 impl Collectors {
@@ -123,8 +122,8 @@ impl Collectors {
             known_kernel_types: HashSet::new(),
             cli,
             factory,
-            gc_handle: None,
             run: Running::new(),
+            tracking_gc: None,
         })
     }
 
@@ -204,7 +203,7 @@ impl Collectors {
 
         // Initialize tracking & filters.
         if self.known_kernel_types.contains("struct sk_buff *") {
-            self.gc_handle = init_tracking(&mut self.probes, self.run.clone())?;
+            self.tracking_gc = Some(init_tracking(&mut self.probes)?);
         }
         Self::setup_filters(&mut self.probes, collect)?;
 
@@ -232,6 +231,11 @@ impl Collectors {
             Some(factories) => factories,
             None => bail!("No section factory found, aborting"),
         };
+
+        if let Some(gc) = &mut self.tracking_gc {
+            gc.start(self.run.clone())?;
+        }
+
         self.factory.start(section_factories)?;
 
         self.probes.attach()?;
@@ -260,8 +264,8 @@ impl Collectors {
         // termination got performed implicitly by the signal handler.
         // The print-out is just for consistency.
         debug!("Stopping tracking gc");
-        if let Some(gc) = self.gc_handle.take() {
-            gc.join().or_else(|_| bail!("failed to stop tracking gc"))?;
+        if let Some(gc) = &mut self.tracking_gc {
+            gc.join()?;
         }
 
         debug!("Stopping events");