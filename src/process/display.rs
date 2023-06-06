2216dd9589ceaa22ef25e8564f1fb883610b1b10
Adrian Moreno <amorenoz@redhat.com>
2023-06-06 10:37:03 +0200

    process: display sorted series in text format.
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- src/process/display.rs
+++ src/process/display.rs
@@ -2,6 +2,7 @@ use std::io::Write;
 
 use anyhow::Result;
 
+use super::series::EventSeries;
 use crate::core::events::*;
 
 enum PrintSingleFormat {
@@ -41,6 +42,73 @@ impl PrintSingle {
         };
 
         Ok(self.writer.write_all(event.as_bytes())?)
+    }
+
+    /// Flush underlying writers.
+    pub(crate) fn flush(&mut self) -> Result<()> {
+        Ok(self.writer.flush()?)
+    }
+}
+
+/// Handles event series formatting and writing to a `Write`.
+pub(crate) struct PrintSeries {
+    writer: Box<dyn Write>,
+    format: PrintSingleFormat,
+}
+
+impl PrintSeries {
+    pub(crate) fn text(writer: Box<dyn Write>, format: DisplayFormat) -> Self {
+        Self {
+            writer,
+            format: PrintSingleFormat::Text(format),
+        }
+    }
+
+    pub(crate) fn json(writer: Box<dyn Write>) -> Self {
+        Self {
+            writer,
+            format: PrintSingleFormat::Json,
+        }
+    }
+
+    fn indent(n_spaces: usize, lines: String) -> String {
+        lines
+            .split('\n')
+            .map(|line| format!("{}{}", " ".repeat(n_spaces), line))
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
+
+    /// Process events one by one (format & print).
+    pub(crate) fn process_one(&mut self, series: &EventSeries) -> Result<()> {
+        let mut content = String::new();
+        match self.format {
+            PrintSingleFormat::Text(format) => match format {
+                DisplayFormat::SingleLine => {
+                    if let Some(first) = series.events.first() {
+                        content.push_str(&format!("\n{}\n", first.display(format)));
+                    }
+                    for event in series.events.iter().skip(1) {
+                        content.push_str(&Self::indent(2, format!("+ {}", event.display(format))));
+                        content.push('\n');
+                    }
+                }
+                DisplayFormat::MultiLine => {
+                    if let Some(first) = series.events.first() {
+                        content.push('\n');
+                        content.push_str(&format!("{}", first.display(format)));
+                        content.push('\n');
+                    }
+                    for event in series.events.iter().skip(1) {
+                        content.push_str(&Self::indent(2, format!("+ {}", event.display(format))));
+                        content.push('\n');
+                    }
+                }
+            },
+            PrintSingleFormat::Json => content.push_str(&format!("{}\n", series.to_json())),
+        };
+
+        Ok(self.writer.write_all(content.as_bytes())?)
     }
 
     /// Flush underlying writers.
