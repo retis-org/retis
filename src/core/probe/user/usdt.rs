retis-org/retis

commit 74b0dfdcf8f8e2edeebb3254be60fcd53bf3d0ad

user: add usdt support
The Probe::Usdt data (UsdtProbe), is built using a Process and a string
representing the target.

Only 1 hook is supported so it's just a simplified version of the
already existing probes.

Signed-off-by: Adrián Moreno <amorenoz@redhat.com>

src/core/probe/user/usdt.rs
@@ -1,0 +1,83 @@
+use anyhow::{anyhow, bail, Result};
+
+use crate::core::probe::builder::*;
+use crate::core::probe::{get_ebpf_debug, Hook, Probe};
+
+mod usdt_bpf {
+    include!("bpf/.out/usdt.skel.rs");
+}
+use usdt_bpf::UsdtSkelBuilder;
+
+#[derive(Default)]
+pub(crate) struct UsdtBuilder {
+    links: Vec<libbpf_rs::Link>,
+    map_fds: Vec<(String, i32)>,
+    hooks: Vec<Hook>,
+}
+
+impl ProbeBuilder for UsdtBuilder {
+    fn new() -> UsdtBuilder {
+        UsdtBuilder::default()
+    }
+
+    fn init(&mut self, map_fds: Vec<(String, i32)>, hooks: Vec<Hook>) -> Result<()> {
+        self.map_fds = map_fds;
+        if hooks.len() > 1 {
+            bail!("USDT Probes only support a single hook");
+        }
+        self.hooks = hooks;
+        Ok(())
+    }
+
+    fn attach(&mut self, probe: &Probe) -> Result<()> {
+        let probe = match probe {
+            Probe::Usdt(usdt) => usdt,
+            _ => bail!("Wrong probe type"),
+        };
+
+        let mut skel = UsdtSkelBuilder::default();
+        skel.obj_builder.debug(get_ebpf_debug());
+        let skel = skel.open()?;
+
+        let open_obj = skel.obj;
+        reuse_map_fds(&open_obj, &self.map_fds)?;
+
+        let mut obj = open_obj.load()?;
+        let prog = obj
+            .prog_mut("probe_usdt")
+            .ok_or_else(|| anyhow!("Couldn't get program"))?;
+        let mut links = replace_hooks(prog.fd(), &self.hooks)?;
+        self.links.append(&mut links);
+
+        self.links
+            .push(prog.attach_usdt(probe.pid, &probe.path, &probe.provider, &probe.name)?);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::core::probe::user::{proc::Process, UsdtProbe};
+
+    use ::probe::probe as define_usdt;
+
+    #[test]
+    #[cfg_attr(not(feature = "test_cap_bpf"), ignore)]
+    fn init_and_attach_usdt() {
+        define_usdt!(test_builder, usdt, 1);
+
+        let mut builder = UsdtBuilder::new();
+
+        let p = Process::from_pid(std::process::id() as i32).unwrap();
+
+        // It's for now, the probes below won't do much.
+        assert!(builder.init(Vec::new(), Vec::new()).is_ok());
+        assert!(builder
+            .attach(&Probe::Usdt(
+                UsdtProbe::new(&p, "test_builder::usdt").unwrap()
+            ))
+            .is_ok());
+    }
+}

Cargo.toml
@@ -14,7 +14,7 @@ btf-rs = {git = "https://github.com/net-trace/btf-rs"}
 byteorder = "1.4.3"
 clap = { version = "4.0", features = ["derive", "string"] }
 elf = "0.7.0"
-libbpf-rs = "0.19"
+libbpf-rs = {git = "https://github.com/libbpf/libbpf-rs.git", rev= "52ab250"}
 libbpf-sys = "1.0"
 libc = "0.2"
 log = "0.4"

build.rs
@@ -12,7 +12,11 @@ const FILTER_INCLUDE_PATH: &str = "src/core/filters/packets/bpf/include";
 const BINDGEN_HEADER: &str = "src/core/bpf_sys/include/bpf-sys.h";
 const INCLUDE_PATHS: &[&str] = &[
     "src/core/probe/kernel/bpf/include",
+    "src/core/probe/user/bpf/include",
     "src/core/events/bpf/include",
+    // Taking errno.h from libc instead of linux headers.
+    // TODO: Remove when we fix proper header dependencies.
+    "/usr/include/x86_64-linux-gnu",
 ];
 
 // Not super fail safe (not using Path).
@@ -143,6 +147,7 @@ fn main() {
     // core::probe::kernel
     build_probe("src/core/probe/kernel/bpf/kprobe.bpf.c");
     build_probe("src/core/probe/kernel/bpf/raw_tracepoint.bpf.c");
+    build_probe("src/core/probe/user/bpf/usdt.bpf.c");
 
     // module::skb_tracking
     build_hook("src/module/skb_tracking/bpf/tracking_hook.bpf.c");

src/core/probe/manager.rs
@@ -10,6 +10,7 @@ use super::*;
 use super::{
     builder::ProbeBuilder,
     kernel::{kprobe, raw_tracepoint},
+    user::usdt,
 };
 use crate::core::events::bpf::BpfEvents;
 
@@ -47,8 +48,10 @@ impl ProbeManager {
         let dynamic_probes: [ProbeSet; probe::PROBE_VARIANTS] = [
             ProbeSet::new(Box::new(kprobe::KprobeBuilder::new())),
             ProbeSet::new(Box::new(raw_tracepoint::RawTracepointBuilder::new())),
+            ProbeSet::new(Box::new(usdt::UsdtBuilder::new())),
         ];
-        let targeted_probes: [Vec<ProbeSet>; probe::PROBE_VARIANTS] = [Vec::new(), Vec::new()];
+        let targeted_probes: [Vec<ProbeSet>; probe::PROBE_VARIANTS] = Default::default();
+
         // When testing the kernel object is not modified later to reuse the
         // config map is this map is hidden.
         let mut mgr = ProbeManager {
@@ -80,6 +83,7 @@ impl ProbeManager {
     pub(crate) fn add_probe(&mut self, probe: Probe) -> Result<()> {
         let key = match &probe {
             Probe::Kprobe(probe) | Probe::RawTracepoint(probe) => probe.symbol.name(),
+            Probe::Usdt(probe) => probe.name(),
         };
 
         // First check if it is already in the generic probe list.
@@ -117,7 +121,7 @@ impl ProbeManager {
         Ok(())
     }
 
-    /// Request a hook to be attached to all probes.
+    /// Request a hook to be attached to all kernel probes.
     ///
     /// ```
     /// mod hook {
@@ -126,9 +130,9 @@ impl ProbeManager {
     ///
     /// [...]
     ///
-    /// mgr.register_hook(Hook::from(hook::DATA))?;
+    /// mgr.register_kernel_hook(Hook::from(hook::DATA))?;
     /// ```
-    pub(crate) fn register_hook(&mut self, hook: Hook) -> Result<()> {
+    pub(crate) fn register_kernel_hook(&mut self, hook: Hook) -> Result<()> {
         let mut max: usize = 0;
         for tgt_set in self.targeted_probes.iter_mut() {
             for set in tgt_set.iter_mut() {
@@ -160,6 +164,7 @@ impl ProbeManager {
     pub(crate) fn register_hook_to(&mut self, hook: Hook, probe: Probe) -> Result<()> {
         let key = match &probe {
             Probe::Kprobe(probe) | Probe::RawTracepoint(probe) => probe.symbol.name(),
+            Probe::Usdt(probe) => probe.name(),
         };
 
         // First check if the target isn't already registered to the generic
@@ -172,6 +177,9 @@ impl ProbeManager {
         let tgt_set = &mut self.targeted_probes[probe.as_key()];
         for set in tgt_set.iter_mut() {
             if set.targets.get(&key).is_some() {
+                if let Probe::Usdt(_) = probe {
+                    bail!("USDT probes only support a single hook")
+                }
                 if self.dynamic_hooks.len() + set.hooks.len() == HOOK_MAX {
                     bail!("Hook list is already full");
                 }
@@ -184,6 +192,7 @@ impl ProbeManager {
         let mut set = ProbeSet::new(match probe {
             Probe::Kprobe(_) => Box::new(kprobe::KprobeBuilder::new()),
             Probe::RawTracepoint(_) => Box::new(raw_tracepoint::RawTracepointBuilder::new()),
+            Probe::Usdt(_) => Box::new(usdt::UsdtBuilder::new()),
         });
 
         set.targets.insert(key, probe);
@@ -269,6 +278,7 @@ impl ProbeSet {
                         libbpf_rs::MapFlags::NO_EXIST,
                     )?;
                 }
+                _ => (),
             }
 
             // Finally attach a probe to the target.
@@ -319,8 +329,8 @@ mod tests {
         let events = BpfEvents::new().unwrap();
         let mut mgr = ProbeManager::new(&events).unwrap();
 
-        assert!(mgr.register_hook(Hook::from(HOOK)).is_ok());
-        assert!(mgr.register_hook(Hook::from(HOOK)).is_ok());
+        assert!(mgr.register_kernel_hook(Hook::from(HOOK)).is_ok());
+        assert!(mgr.register_kernel_hook(Hook::from(HOOK)).is_ok());
 
         assert!(mgr
             .register_hook_to(Hook::from(HOOK), kprobe!("kfree_skb_reason"))
@@ -336,11 +346,11 @@ mod tests {
             .is_ok());
 
         for _ in 0..HOOK_MAX - 4 {
-            assert!(mgr.register_hook(Hook::from(HOOK)).is_ok());
+            assert!(mgr.register_kernel_hook(Hook::from(HOOK)).is_ok());
         }
 
         // We should hit the hook limit here.
-        assert!(mgr.register_hook(Hook::from(HOOK)).is_err());
+        assert!(mgr.register_kernel_hook(Hook::from(HOOK)).is_err());
 
         assert!(mgr
             .register_hook_to(Hook::from(HOOK), kprobe!("kfree_skb_reason"))

src/core/probe/probe.rs
@@ -3,6 +3,7 @@ use std::{collections::HashMap, fmt};
 use anyhow::{bail, Result};
 
 use super::kernel::KernelProbe;
+use super::user::UsdtProbe;
 use crate::core::kernel;
 
 /// Probe types supported by this program. This is the main object given to
@@ -11,6 +12,8 @@ use crate::core::kernel;
 pub(crate) enum Probe {
     Kprobe(KernelProbe),
     RawTracepoint(KernelProbe),
+    #[allow(dead_code)]
+    Usdt(UsdtProbe),
 }
 
 impl Probe {
@@ -32,7 +35,7 @@ impl Probe {
 }
 
 // Use mem::variant_count::<Probe>() when available in stable.
-pub(crate) const PROBE_VARIANTS: usize = 2;
+pub(crate) const PROBE_VARIANTS: usize = 3;
 
 impl Probe {
     /// We do use probe types as indexes, the following makes it easy.
@@ -40,6 +43,7 @@ impl Probe {
         match self {
             Probe::Kprobe(_) => 0,
             Probe::RawTracepoint(_) => 1,
+            Probe::Usdt(_) => 2,
         }
     }
 }
@@ -50,6 +54,7 @@ impl fmt::Display for Probe {
         match self {
             Probe::Kprobe(symbol) => write!(f, "kprobe:{}", symbol),
             Probe::RawTracepoint(symbol) => write!(f, "raw_tracepoint:{}", symbol),
+            Probe::Usdt(symbol) => write!(f, "usdt {}", symbol),
         }
     }
 }

src/core/probe/user/bpf/include/user_common.h
@@ -1,0 +1,38 @@
+#ifndef __CORE_PROBE_USER_BPF_COMMON__
+#define __CORE_PROBE_USER_BPF_COMMON__
+
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+
+#include "events.h"
+
+/* Helper to define a USDT hook (mostly in collectors) while not having to
+ * duplicate the common part everywhere.
+ * This also ensure hooks are doing the right thing and should help with
+ * maintenance.
+ *
+ * To define a USDT hook in a collector hook, say hook.bpf.c,
+ * ```
+ * #include <user_common.h>
+ *
+ * DEFINE_USDT_HOOK(
+ *	do_something(ctx, event);
+ *	return 0;
+ * )
+ *
+ * char __license[] SEC("license") = "GPL";
+ * ```
+ *
+ * Do not forget to add the hook to build.rs
+ */
+#define DEFINE_USDT_HOOK(inst)							\
+	SEC("ext/hook")								\
+	int hook(struct pt_regs *ctx, struct trace_raw_event *event)		\
+	{									\
+		/* Let the verifier be happy */					\
+		if (!ctx || !event)						\
+			return 0;						\
+		inst								\
+	}
+
+#endif // __CORE_PROBE_USER_BPF_COMMON__

src/core/probe/user/bpf/usdt.bpf.c
@@ -1,0 +1,43 @@
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/usdt.bpf.h>
+
+#include <user_common.h>
+
+/* Hook placeholder */
+__attribute__ ((noinline))
+int hook0(struct pt_regs *ctx, struct trace_raw_event *event) {
+	volatile int ret = 0;
+	if (!ctx || !event)
+		return 0;
+	return ret;
+}
+
+SEC("usdt")
+int probe_usdt(struct pt_regs *ctx)
+{
+	struct pt_regs ctx_fp = *ctx;
+	struct common_event *e;
+	struct trace_raw_event *event;
+
+	event = get_event();
+	if (!event)
+		return 0;
+
+	e = get_event_section(event, COMMON, 1, sizeof(*e));
+	if (!e) {
+		discard_event(event);
+		return 0;
+	}
+	e->symbol = PT_REGS_IP(ctx);
+	e->timestamp = bpf_ktime_get_ns();
+
+	/* UST only supports a single hook. */
+	hook0(&ctx_fp, event);
+
+	send_event(event);
+
+	return 0;
+}
+
+char __license[] SEC("license") = "GPL";

src/core/probe/user/mod.rs
@@ -9,7 +9,5 @@ pub(crate) mod user;
 #[allow(unused_imports)]
 pub(crate) use user::*;
 
-mod proc;
-// Re-export user.rs
-#[allow(unused_imports)]
-pub(crate) use proc::*;
+pub(crate) mod proc;
+pub(crate) mod usdt;

commit 9baa6fc2ca7f409d249ea8ad804cedacef7277cf

usdt: remove ManuallyDrop<> workaround.
Normally when usdts get loaded the usdt manager gets lazily allocated
and subsequently released when the object gets dropped/destroyed.

Before this change, the object got dropped during the .attach()
leading to the release of the underlying usdt manager resource.  The
subsequent drop of the usdt links, which requires the manager in order
to complete all the operations, then led to a dereference of the
dangling pointer previously released and a related segmentation fault.

Fix it by avoiding to release it before it is needed and adding it to
the builder as an Option<>, so that possible subsequent users may
check whether or not it's been released.

Fixes: 74b0dfdcf8f8 ("user: add usdt support")
Fixes: cb6bd2f77ae4 ("usdt: add ManullyDrop to the links vector.")
Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

src/core/probe/user/usdt.rs
@@ -1,5 +1,3 @@
-use std::mem::ManuallyDrop;
-
 use anyhow::{anyhow, bail, Result};
 
 use crate::core::filters::Filter;
@@ -13,10 +11,8 @@ use usdt_bpf::UsdtSkelBuilder;
 
 #[derive(Default)]
 pub(crate) struct UsdtBuilder {
-    // FIXME: Marked as manually dropped because the process
-    // segfaults apparently in libbpf when the drop in place
-    // happens.
-    links: ManuallyDrop<Vec<libbpf_rs::Link>>,
+    links: Vec<libbpf_rs::Link>,
+    obj: Option<libbpf_rs::Object>,
     map_fds: Vec<(String, i32)>,
     hooks: Vec<Hook>,
 }
@@ -62,6 +58,8 @@ impl ProbeBuilder for UsdtBuilder {
 
         self.links
             .push(prog.attach_usdt(probe.pid, &probe.path, &probe.provider, &probe.name)?);
+        self.obj = Some(obj);
+
         Ok(())
     }
 }