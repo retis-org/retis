84f84b4c03438841d15ab4959e8ce9be489fa7d4
Antoine Tenart <atenart@redhat.com>
2023-08-02 14:37:09 +0200

    tests: group test setups for reuse
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- tests/test_skb.py
+++ tests/test_skb.py
@@ -1,38 +1,7 @@
-import pytest
 import time
-
-from pyroute2 import IPRoute
 
 from testlib import Retis, assert_events_present
 
-
-@pytest.fixture
-def two_ports_skb(netns):
-    '''Fixture that creates two netns connected through a veth pair.'''
-    ipr = IPRoute()
-
-    # Create netns & a veth pair
-    ns0 = netns.add('ns0')
-    ns1 = netns.add('ns1')
-    ipr.link('add', ifname='veth01', peer='veth10', kind='veth')
-
-    # Wait until links appear
-    peer, veth = ipr.poll(
-        ipr.link, 'dump', timeout=5, ifname=lambda x: x in ('veth01', 'veth10')
-    )
-
-    # Move ifaces to netns
-    ipr.link('set', ifname='veth01', net_ns_fd='ns0')
-    ipr.link('set', ifname='veth10', net_ns_fd='ns1')
-
-    # Setup ifaces
-    ns0.link('set', ifname='veth01', state='up')
-    ns1.link('set', ifname='veth10', state='up')
-    ns0.addr('add', index=ns0.link_lookup(ifname='veth01')[0], address='10.0.42.1', prefixlen=24)
-    ns1.addr('add', index=ns1.link_lookup(ifname='veth10')[0], address='10.0.42.2', prefixlen=24)
-
-    ipr.close()
-    yield netns
 
 def test_skb_sanity(two_ports_skb):
     ns = two_ports_skb
