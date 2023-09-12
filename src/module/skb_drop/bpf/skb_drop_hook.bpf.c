retis-org/retis

commit 915947cd1999cc310a537b9cc0879b9c0176f8fc

module: skb_drop: do not fail on old kernels
Do not fail if a kernel is old enough not to support skb drop reasons,
so that the skb-drop reasons still work (installing probes at the right
places) and especially for the dropmon profile to work (which still
provides useful data).

Additionally on kernel not supporting drop reasons, fake reporting the
NOT_SPECIFIED reason.

Signed-off-by: Antoine Tenart <atenart@redhat.com>

src/module/skb_drop/bpf/skb_drop_hook.bpf.c
@@ -5,20 +5,19 @@
 #include <common.h>
 
 struct skb_drop_event {
-	u32 drop_reason;
+	s32 drop_reason;
 } __attribute__((packed));
 
 DEFINE_HOOK(F_AND, RETIS_F_PACKET_PASS,
 	struct skb_drop_event *e;
 
-	if (!retis_arg_valid(ctx, skb_drop_reason))
-		return 0;
-
 	e = get_event_section(event, COLLECTOR_SKB_DROP, 1, sizeof(*e));
 	if (!e)
 		return 0;
 
-	e->drop_reason = retis_get_skb_drop_reason(ctx);
+	e->drop_reason = retis_arg_valid(ctx, skb_drop_reason) ?
+		retis_get_skb_drop_reason(ctx) : -1;
+
 	return 0;
 )
 

src/module/skb_drop/skb_drop.rs
@@ -1,4 +1,5 @@
 use anyhow::{bail, Result};
+use log::warn;
 
 use super::{skb_drop_hook, SkbDropEventFactory};
 use crate::{
@@ -6,7 +7,7 @@ use crate::{
     collect::Collector,
     core::{
         events::EventSectionFactory,
-        inspect::inspector,
+        inspect::{inspector, kernel_version::KernelVersionReq},
         kernel::Symbol,
         probe::{Hook, Probe, ProbeManager},
     },
@@ -29,6 +30,8 @@ impl Collector for SkbDropModule {
     }
 
     fn can_run(&mut self, _: &CliConfig) -> Result<()> {
+        let inspector = inspector()?;
+
         // It makes no sense to use Retis on a kernel older enough not to have
         // the skb:kfree_skb tracepoint (it was introduced in 2009), we might
         // fail earlier anyway. So do not handle the error case nicely.
@@ -36,11 +39,23 @@ impl Collector for SkbDropModule {
 
         // But we could see a kernel where skb:kfree_skb does not access a drop
         // reason, so check this and handle it nicely.
-        match inspector()?
+        match inspector
             .kernel
             .parameter_offset(&symbol, "enum skb_drop_reason")
         {
-            Err(_) | Ok(None) => bail!("Skb drop reasons are not retrievable on this kernel"),
+            Err(_) | Ok(None) => {
+                let kver = inspector.kernel.version();
+
+                // Skb drop reasons were introduced in kernel v5.17 and are not
+                // a build config option; if not found in such case bail out. On
+                // older kernel, still allow the collector to run, with a
+                // warning.
+                if KernelVersionReq::parse(">= 5.17")?.matches(kver) {
+                    bail!("Could not retrieve skb drop reasons from the kernel");
+                } else {
+                    warn!("This kernel doesn't provide skb drop reasons");
+                }
+            }
             _ => (),
         }
 

src/module/skb_drop/event.rs
@@ -2,7 +2,6 @@ use std::{collections::HashMap, fmt};
 
 use anyhow::Result;
 use btf_rs::Type;
-use log::warn;
 use plain::Plain;
 
 use crate::{
@@ -39,32 +38,30 @@ pub(crate) struct SkbDropEventFactory {
     /// `parse_drop_reasons` on a machine not collecting events. A `Some` value
     /// with an empty map means we couldn't retrieve the drop reasons from the
     /// running kernel.
-    reasons: Option<HashMap<u32, String>>,
+    reasons: Option<HashMap<i32, String>>,
 }
 
 impl SkbDropEventFactory {
     pub(crate) fn parse_drop_reasons(&mut self) -> Result<()> {
         let mut reasons = HashMap::new();
 
-        if let (btf, Type::Enum(r#enum)) = inspector()?
+        if let Ok((btf, Type::Enum(r#enum))) = inspector()?
             .kernel
             .btf
-            .resolve_type_by_name("skb_drop_reason")?
+            .resolve_type_by_name("skb_drop_reason")
         {
             for member in r#enum.members.iter() {
                 if member.val() < 0 {
                     continue;
                 }
                 reasons.insert(
-                    u32::try_from(member.val())?,
+                    member.val(),
                     btf.resolve_name(member)?
                         .trim_start_matches("SKB_")
                         .trim_start_matches("DROP_REASON_")
                         .to_string(),
                 );
             }
-        } else {
-            warn!("Can't retrieve skb drop reason definitions from the kernel. Events will contain raw data (enum int value).");
         }
 
         self.reasons = Some(reasons);
@@ -85,7 +82,10 @@ impl RawEventSectionFactory for SkbDropEventFactory {
         // Unwrap as we just made sure it was Some(..).
         let drop_reason = match self.reasons.as_ref().unwrap().get(&drop_reason) {
             Some(r) => r.clone(),
-            None => format!("{}", drop_reason),
+            None => match drop_reason {
+                -1 => "NOT_SPECIFIED".to_string(),
+                _ => drop_reason.to_string(),
+            },
         };
 
         Ok(Box::new(SkbDropEvent { drop_reason }))
@@ -95,7 +95,7 @@ impl RawEventSectionFactory for SkbDropEventFactory {
 #[derive(Default)]
 #[repr(C, packed)]
 struct BpfSkbDropEvent {
-    drop_reason: u32,
+    drop_reason: i32,
 }
 
 unsafe impl Plain for BpfSkbDropEvent {}

commit 392a3a20aaee8bab21af96a3f7b9230c29d63e9c

module: skb_drop: do not generate fake reason for all events
Commit 915947cd1999 ("module: skb_drop: do not fail on old kernels")
introduced a "fake" event to support old kernels not have drop reasons
support. However this led to including the fake reason in every event on
those old kernels, and for every event not including a genuine reason on
newer kernels.

Fix this by:
- Only attaching the probe to the skb:free_skb tracepoint in case the
  kernel doesn't support drop reasons.
- Check the drop reasons enum exists in BPF side to better support both
  cases.

Fixes: 915947cd1999 ("module: skb_drop: do not fail on old kernels")
Signed-off-by: Antoine Tenart <atenart@redhat.com>

src/module/skb_drop/bpf/skb_drop_hook.bpf.c
@@ -15,8 +15,13 @@ DEFINE_HOOK(F_AND, RETIS_F_PACKET_PASS,
 	if (!e)
 		return 0;
 
-	e->drop_reason = retis_arg_valid(ctx, skb_drop_reason) ?
-		retis_get_skb_drop_reason(ctx) : -1;
+	if (bpf_core_type_exists(enum skb_drop_reason)) {
+		if (!retis_arg_valid(ctx, skb_drop_reason))
+			return 0;
+		e->drop_reason = retis_get_skb_drop_reason(ctx);
+	} else {
+		e->drop_reason = -1;
+	}
 
 	return 0;
 )

src/module/skb_drop/skb_drop.rs
@@ -14,11 +14,15 @@ use crate::{
     module::{Module, ModuleId},
 };
 
-pub(crate) struct SkbDropModule {}
+pub(crate) struct SkbDropModule {
+    reasons_available: bool,
+}
 
 impl Collector for SkbDropModule {
     fn new() -> Result<Self> {
-        Ok(Self {})
+        Ok(Self {
+            reasons_available: true,
+        })
     }
 
     fn known_kernel_types(&self) -> Option<Vec<&'static str>> {
@@ -54,6 +58,7 @@ impl Collector for SkbDropModule {
                     bail!("Could not retrieve skb drop reasons from the kernel");
                 } else {
                     warn!("This kernel doesn't provide skb drop reasons");
+                    self.reasons_available = false;
                 }
             }
             _ => (),
@@ -63,13 +68,23 @@ impl Collector for SkbDropModule {
     }
 
     fn init(&mut self, _: &CliConfig, probes: &mut ProbeManager) -> Result<()> {
-        let symbol = Symbol::from_name("skb:kfree_skb")?;
+        let mut probe = Probe::raw_tracepoint(Symbol::from_name("skb:kfree_skb")?)?;
+        let hook = Hook::from(skb_drop_hook::DATA);
 
-        if let Err(e) = probes.register_probe(Probe::raw_tracepoint(symbol)?) {
+        if self.reasons_available {
+            probes.register_kernel_hook(hook)?;
+        } else {
+            // If the kernel doesn't support drop reasons, only attach the hook
+            // to the skb:kfree_skb tracepoint (otherwise we would have a fake
+            // reason attached to all events).
+            probe.add_hook(hook)?;
+        }
+
+        if let Err(e) = probes.register_probe(probe) {
             bail!("Could not attach to skb:kfree_skb: {}", e);
         }
 
-        probes.register_kernel_hook(Hook::from(skb_drop_hook::DATA))
+        Ok(())
     }
 }
 