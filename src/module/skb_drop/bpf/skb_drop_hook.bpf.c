retis-org/retis

commit 392a3a20aaee8bab21af96a3f7b9230c29d63e9c

module: skb_drop: do not generate fake reason for all events
Commit 915947cd1999 ("module: skb_drop: do not fail on old kernels")
introduced a "fake" event to support old kernels not have drop reasons
support. However this led to including the fake reason in every event on
those old kernels, and for every event not including a genuine reason on
newer kernels.

Fix this by:
- Only attaching the probe to the skb:free_skb tracepoint in case the
  kernel doesn't support drop reasons.
- Check the drop reasons enum exists in BPF side to better support both
  cases.

Fixes: 915947cd1999 ("module: skb_drop: do not fail on old kernels")
Signed-off-by: Antoine Tenart <atenart@redhat.com>

src/module/skb_drop/bpf/skb_drop_hook.bpf.c
@@ -15,8 +15,13 @@ DEFINE_HOOK(F_AND, RETIS_F_PACKET_PASS,
 	if (!e)
 		return 0;
 
-	e->drop_reason = retis_arg_valid(ctx, skb_drop_reason) ?
-		retis_get_skb_drop_reason(ctx) : -1;
+	if (bpf_core_type_exists(enum skb_drop_reason)) {
+		if (!retis_arg_valid(ctx, skb_drop_reason))
+			return 0;
+		e->drop_reason = retis_get_skb_drop_reason(ctx);
+	} else {
+		e->drop_reason = -1;
+	}
 
 	return 0;
 )

src/module/skb_drop/skb_drop.rs
@@ -14,11 +14,15 @@ use crate::{
     module::{Module, ModuleId},
 };
 
-pub(crate) struct SkbDropModule {}
+pub(crate) struct SkbDropModule {
+    reasons_available: bool,
+}
 
 impl Collector for SkbDropModule {
     fn new() -> Result<Self> {
-        Ok(Self {})
+        Ok(Self {
+            reasons_available: true,
+        })
     }
 
     fn known_kernel_types(&self) -> Option<Vec<&'static str>> {
@@ -54,6 +58,7 @@ impl Collector for SkbDropModule {
                     bail!("Could not retrieve skb drop reasons from the kernel");
                 } else {
                     warn!("This kernel doesn't provide skb drop reasons");
+                    self.reasons_available = false;
                 }
             }
             _ => (),
@@ -63,13 +68,23 @@ impl Collector for SkbDropModule {
     }
 
     fn init(&mut self, _: &CliConfig, probes: &mut ProbeManager) -> Result<()> {
-        let symbol = Symbol::from_name("skb:kfree_skb")?;
+        let mut probe = Probe::raw_tracepoint(Symbol::from_name("skb:kfree_skb")?)?;
+        let hook = Hook::from(skb_drop_hook::DATA);
 
-        if let Err(e) = probes.register_probe(Probe::raw_tracepoint(symbol)?) {
+        if self.reasons_available {
+            probes.register_kernel_hook(hook)?;
+        } else {
+            // If the kernel doesn't support drop reasons, only attach the hook
+            // to the skb:kfree_skb tracepoint (otherwise we would have a fake
+            // reason attached to all events).
+            probe.add_hook(hook)?;
+        }
+
+        if let Err(e) = probes.register_probe(probe) {
             bail!("Could not attach to skb:kfree_skb: {}", e);
         }
 
-        probes.register_kernel_hook(Hook::from(skb_drop_hook::DATA))
+        Ok(())
     }
 }
 

commit 4c5f08be67ed426174f42e41cd699d5e42c8e114

module: skb_drop: do sent empty event sections
The previous fix wasn't complete and in case a kernel was supporting
drop reasons but the hook was attached to a probe not having access to
one, empty zeroed event sections were still reported.

Fixes: 392a3a20aaee ("module: skb_drop: do not generate fake reason for all events")
Fixes: 915947cd1999 ("module: skb_drop: do not fail on old kernels")
Signed-off-by: Antoine Tenart <atenart@redhat.com>

src/module/skb_drop/bpf/skb_drop_hook.bpf.c
@@ -11,17 +11,21 @@ struct skb_drop_event {
 DEFINE_HOOK(F_AND, RETIS_F_PACKET_PASS,
 	struct skb_drop_event *e;
 
+	/* Check if the kernel knows about skb drop reasons, and if so check we
+	 * can retrieve it. This should be the common case. In case the kernel
+	 * doesn't know skb drop reasons, this hook will generate fake events
+	 * and will only be attached to specific hooks.
+	 */
+	if (bpf_core_type_exists(enum skb_drop_reason) &&
+	    !retis_arg_valid(ctx, skb_drop_reason))
+		return 0;
+
 	e = get_event_section(event, COLLECTOR_SKB_DROP, 1, sizeof(*e));
 	if (!e)
 		return 0;
 
-	if (bpf_core_type_exists(enum skb_drop_reason)) {
-		if (!retis_arg_valid(ctx, skb_drop_reason))
-			return 0;
-		e->drop_reason = retis_get_skb_drop_reason(ctx);
-	} else {
-		e->drop_reason = -1;
-	}
+	e->drop_reason = bpf_core_type_exists(enum skb_drop_reason) ?
+		retis_get_skb_drop_reason(ctx) : -1;
 
 	return 0;
 )