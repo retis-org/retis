8916ffddbd7916c1e0db37616cb67a0e55acbc92
Antoine Tenart <atenart@redhat.com>
2023-11-20 12:44:10 +0100

    tree: hook non-core drop reasons
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- src/core/probe/kernel/inspect.rs
+++ src/core/probe/kernel/inspect.rs
@@ -11,8 +11,8 @@ pub(super) fn inspect_symbol(symbol: &Symbol) -> Result<ProbeConfig> {
     if let Some(offset) = symbol.parameter_offset("struct sk_buff *")? {
         cfg.offsets.sk_buff = offset as i8;
     }
-    if let Some(offset) = symbol.parameter_offset("enum skb_drop_reason")? {
-        cfg.offsets.skb_drop_reason = offset as i8;
+    if let Some(offset) = drop_reason_offset(symbol)? {
+        cfg.offsets.skb_drop_reason = offset;
     }
     if let Some(offset) = symbol.parameter_offset("struct net_device *")? {
         cfg.offsets.net_device = offset as i8;
@@ -28,6 +28,23 @@ pub(super) fn inspect_symbol(symbol: &Symbol) -> Result<ProbeConfig> {
     }
 
     Ok(cfg)
+}
+
+/// Find the offset of drop reason enums. We do not distinguish them as they're
+/// part of the same (virtual) enum.
+fn drop_reason_offset(symbol: &Symbol) -> Result<Option<i8>> {
+    Ok(
+        #[allow(clippy::manual_map)]
+        if let Some(offset) = symbol.parameter_offset("enum skb_drop_reason")? {
+            Some(offset as i8)
+        } else if let Some(offset) = symbol.parameter_offset("enum mac80211_drop_reason")? {
+            Some(offset as i8)
+        } else if let Some(offset) = symbol.parameter_offset("enum ovs_drop_reason")? {
+            Some(offset as i8)
+        } else {
+            None
+        },
+    )
 }
 
 #[cfg(test)]
