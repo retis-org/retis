retis-org/retis

commit 1bb047ba166b58a436c1f101bcc569e0184be1c3

module: skb: bpf: fix verifier issue on older kernels
Signed-off-by: Antoine Tenart <atenart@redhat.com>

src/module/skb/bpf/skb_hook.bpf.c
@@ -388,6 +388,12 @@ static __always_inline int process_packet(struct retis_raw_event *event,
 	unsigned char *head, *data;
 	struct skb_packet_event *e;
 	u16 mac, network;
+	/* Due to verifier issues on some (old) kernel versions, namely
+	 * 5.15.0-88-generic on Ubuntu 22.04, size is limited to 0xff when
+	 * retrieving packets starting at the mac offset and to 0xef for packets
+	 * starting at the network offset. The difference in mask is due to the
+	 * fake eth header added in the later case.
+	 */
 	int size;
 
 	head = BPF_CORE_READ(skb, head);
@@ -413,7 +419,8 @@ static __always_inline int process_packet(struct retis_raw_event *event,
 			return 0;
 
 		e->len = len - mac_offset;
-		bpf_probe_read_kernel(e->packet, size, head + mac);
+		e->capture_len = size & 0xff;
+		bpf_probe_read_kernel(e->packet, size & 0xff, head + mac);
 	/* Valid network offset with an unset or invalid mac offset: we can fake
 	 * the eth header.
 	 */
@@ -445,14 +452,14 @@ static __always_inline int process_packet(struct retis_raw_event *event,
 		eth->h_proto = etype;
 
 		e->len = len - network_offset + sizeof(*eth);
-		bpf_probe_read_kernel(e->packet + sizeof(*eth), size,
+		e->capture_len = size & 0xef;
+		bpf_probe_read_kernel(e->packet + sizeof(*eth), size & 0xef,
 				      head + network);
 	/* Can't guess any useful packet offset */
 	} else {
 		return 0;
 	}
 
-	e->capture_len = size;
 	return 0;
 }
 

commit 3afddf5ef4fa4fb5b1208b52294b6664dab5b676

module: skb: fix raw packet retrieval for max capture size
Turns out 256 & 0xff == 0... Fix the max capture size by lowering the
buffer size to fit in 0xff.

Fixes: 1bb047ba166b ("module: skb: bpf: fix verifier issue on older kernels")
Signed-off-by: Antoine Tenart <atenart@redhat.com>

src/module/skb/bpf.rs
@@ -349,7 +349,7 @@ pub(super) struct RawPacketEvent {
     /// Lenght of the capture. <= len.
     capture_len: u32,
     /// Raw packet data.
-    packet: [u8; 256],
+    packet: [u8; 255],
 }
 
 pub(super) fn unmarshal_packet(raw_section: &BpfRawSection) -> Result<SkbPacketEvent> {

src/module/skb/bpf/skb_hook.bpf.c
@@ -126,7 +126,7 @@ struct skb_data_ref_event {
 struct skb_packet_event {
 	u32 len;
 	u32 capture_len;
-#define PACKET_CAPTURE_SIZE	256
+#define PACKET_CAPTURE_SIZE	255
 	u8 packet[PACKET_CAPTURE_SIZE];
 } __attribute__((packed));
 