retis-org/retis

commit 3afddf5ef4fa4fb5b1208b52294b6664dab5b676

module: skb: fix raw packet retrieval for max capture size
Turns out 256 & 0xff == 0... Fix the max capture size by lowering the
buffer size to fit in 0xff.

Fixes: 1bb047ba166b ("module: skb: bpf: fix verifier issue on older kernels")
Signed-off-by: Antoine Tenart <atenart@redhat.com>

src/module/skb/bpf/skb_hook.bpf.c
@@ -126,7 +126,7 @@ struct skb_data_ref_event {
 struct skb_packet_event {
 	u32 len;
 	u32 capture_len;
-#define PACKET_CAPTURE_SIZE	256
+#define PACKET_CAPTURE_SIZE	255
 	u8 packet[PACKET_CAPTURE_SIZE];
 } __attribute__((packed));
 

src/module/skb/bpf.rs
@@ -349,7 +349,7 @@ pub(super) struct RawPacketEvent {
     /// Lenght of the capture. <= len.
     capture_len: u32,
     /// Raw packet data.
-    packet: [u8; 256],
+    packet: [u8; 255],
 }
 
 pub(super) fn unmarshal_packet(raw_section: &BpfRawSection) -> Result<SkbPacketEvent> {

commit 98e9882ab666c73bd4d6ac70e112bc08c62adf52

module: skb: fix (again) skb hook for old kernels
Same as 1bb047ba166b ("module: skb: bpf: fix verifier issue on older
kernels") but that had a flaw and was broken by a recent change that
fixed it.

Could not find a better way. If not using &= no size check works. This
has the downside of limiting the capture size of L3-only packets to
0x7f; but that is not that bad. Many packets headers should be within
that limit. Another working solution would be to increase our buffer
size to `PACKET_CAPTURE_SIZE + sizeof(ethhdr)`, which allows to capture
up to PACKET_CAPTURE_SIZE of data in each case.

Some of the verifier errors:

- 248: (85) call bpf_probe_read_kernel#113
  R2 min value is negative, either use unsigned or 'var &= const'

- 248: (85) call bpf_probe_read_kernel#113
  R2 unbounded memory access, use 'var &= const' or 'if (var < const)'

Fixes: 3afddf5ef4fa ("module: skb: fix raw packet retrieval for max capture size")
Signed-off-by: Antoine Tenart <atenart@redhat.com>

src/module/skb/bpf/skb_hook.bpf.c
@@ -387,13 +387,6 @@ static __always_inline int process_packet(struct retis_raw_event *event,
 	unsigned char *head, *data;
 	struct skb_packet_event *e;
 	u16 mac, network;
-	/* Due to verifier issues on some (old) kernel versions, namely
-	 * 5.15.0-88-generic on Ubuntu 22.04, size is limited to 0xff when
-	 * retrieving packets starting at the mac offset and to 0xef for packets
-	 * starting at the network offset. The difference in mask is due to the
-	 * fake eth header added in the later case.
-	 */
-	int size;
 
 	head = BPF_CORE_READ(skb, head);
 	headroom = BPF_CORE_READ(skb, data) - head;
@@ -409,6 +402,7 @@ static __always_inline int process_packet(struct retis_raw_event *event,
 
 	/* Best case: mac offset is set and valid */
 	if (is_mac_data_valid(skb)) {
+		volatile int size;
 		int mac_offset;
 
 		mac_offset = mac - headroom;
@@ -421,9 +415,15 @@ static __always_inline int process_packet(struct retis_raw_event *event,
 		if (!e)
 			return 0;
 
+		/* Due to verifier issues on some (old) kernel versions, namely
+		 * 5.15.0-88-generic on Ubuntu 22.04, size is volatile and
+		 * limited to 0xff.
+		 */
+		size &= 0xff;
+
 		e->len = len - mac_offset;
-		e->capture_len = size & 0xff;
-		bpf_probe_read_kernel(e->packet, size & 0xff, head + mac);
+		e->capture_len = size;
+		bpf_probe_read_kernel(e->packet, size, head + mac);
 	/* Valid network offset with an unset or invalid mac offset: we can fake
 	 * the eth header.
 	 */
@@ -431,6 +431,7 @@ static __always_inline int process_packet(struct retis_raw_event *event,
 		u16 etype = BPF_CORE_READ(skb, protocol);
 		struct ethhdr *eth;
 		int network_offset;
+		volatile int size;
 
 		/* We do need the ethertype to be set at the skb level here,
 		 * otherwise we can't guess what kind of packet this is.
@@ -454,9 +455,16 @@ static __always_inline int process_packet(struct retis_raw_event *event,
 		__builtin_memset(eth, 0, sizeof(*eth));
 		eth->h_proto = etype;
 
+		/* Due to verifier issues on some (old) kernel versions, namely
+		 * 5.15.0-88-generic on Ubuntu 22.04, size is volatile and
+		 * limited to 0x7f. The difference in mask with the above is due
+		 * to the fake eth header added in the later case.
+		 */
+		size &= 0x7f;
+
 		e->len = len - network_offset + sizeof(*eth);
-		e->capture_len = size & 0xef;
-		bpf_probe_read_kernel(e->packet + sizeof(*eth), size & 0xef,
+		e->capture_len = size;
+		bpf_probe_read_kernel(e->packet + sizeof(*eth), size,
 				      head + network);
 	/* Can't guess any useful packet offset */
 	} else {