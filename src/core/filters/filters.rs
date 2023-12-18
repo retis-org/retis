a5fb104c624611ab32dd6039e8f618899d6bc5e5
Paolo Valerio <pvalerio@redhat.com>
2023-12-18 10:28:41 +0100

    filters: handle L3 filter in the custom handler
    Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

--- src/core/filters/filters.rs
+++ src/core/filters/filters.rs
@@ -2,7 +2,7 @@
 use std::{collections::HashMap, sync::Mutex};
 
 use anyhow::{bail, Result};
-use log::debug;
+use log::{debug, error};
 use once_cell::sync::Lazy;
 
 use crate::core::{
@@ -62,69 +62,77 @@ pub(crate) unsafe extern "C" fn fixup_filter_load_fn(
     _opts: *mut libbpf_sys::bpf_prog_load_opts,
     _cookie: ::std::os::raw::c_long,
 ) -> std::os::raw::c_int {
-    let magic = FilterPacketType::L2 as u32;
-    let filter = get_filter(magic);
+    for magic in [FilterPacketType::L2 as u32, FilterPacketType::L3 as u32] {
+        let filter = get_filter(magic);
 
-    let f = if let Some(f) = filter {
-        match f {
-            Filter::Packet(_, bf) => bf.0,
-            // fail if non packet filter is encountered
-            _ => return -1,
+        let f = if let Some(f) = filter {
+            match f {
+                Filter::Packet(_, bf) => bf.0,
+                // fail if non packet filter is encountered
+                _ => return -1,
+            }
+        } else {
+            let mut default_filter = eBpfProg::new();
+            default_filter.add(eBpfInsn::mov32(MovInfo::Imm {
+                dst: BpfReg::R0,
+                imm: 0x40000_i32,
+            }));
+            default_filter.to_bytes()
+        };
+
+        let filter: &[libbpf_sys::bpf_insn] = unsafe {
+            std::slice::from_raw_parts(
+                f.as_slice().as_ptr() as *const libbpf_sys::bpf_insn,
+                f.len() / std::mem::size_of::<libbpf_sys::bpf_insn>(),
+            )
+        };
+
+        let (insns, insns_cnt) = unsafe {
+            (
+                libbpf_sys::bpf_program__insns(prog),
+                libbpf_sys::bpf_program__insn_cnt(prog),
+            )
+        };
+
+        let insns = unsafe { std::slice::from_raw_parts(insns, insns_cnt as usize) };
+        let mut prog_ext = insns.to_vec().clone();
+
+        let inject_pos = match prog_ext.iter().enumerate().find_map(|(pos, insn)| {
+            (insn.code == (bpf_sys::BPF_JMP | bpf_sys::BPF_CALL) && insn.imm == magic as i32)
+                .then_some(pos)
+        }) {
+            Some(p) => p,
+            None => {
+                let none;
+                let prog_name = unsafe { libbpf_sys::bpf_program__name(prog) };
+                let prog_name = if prog_name.is_null() {
+                    none = String::from("[none]");
+                    &none
+                } else {
+                    unsafe { std::ffi::CStr::from_ptr(prog_name) }
+                        .to_str()
+                        .unwrap()
+                };
+
+                debug!("No inline position found for {}", prog_name);
+                // Always succeed as probes may not require filtering at all.
+                return 0;
+            }
+        };
+
+        prog_ext.splice(inject_pos..inject_pos + filter.len(), filter.to_vec());
+
+        let ret = libbpf_sys::bpf_program__set_insns(
+            prog,
+            prog_ext.as_mut_slice().as_mut_ptr(),
+            prog_ext.len() as u64,
+        );
+
+        if ret != 0 {
+            error!("unable to inline filter with magic {:#x} ({ret})", magic);
+            return ret;
         }
-    } else {
-        let mut default_filter = eBpfProg::new();
-        default_filter.add(eBpfInsn::mov32(MovInfo::Imm {
-            dst: BpfReg::R0,
-            imm: 0x40000_i32,
-        }));
-        default_filter.to_bytes()
-    };
+    }
 
-    let filter: &[libbpf_sys::bpf_insn] = unsafe {
-        std::slice::from_raw_parts(
-            f.as_slice().as_ptr() as *const libbpf_sys::bpf_insn,
-            f.len() / std::mem::size_of::<libbpf_sys::bpf_insn>(),
-        )
-    };
-
-    let (insns, insns_cnt) = unsafe {
-        (
-            libbpf_sys::bpf_program__insns(prog),
-            libbpf_sys::bpf_program__insn_cnt(prog),
-        )
-    };
-
-    let insns = unsafe { std::slice::from_raw_parts(insns, insns_cnt as usize) };
-    let mut prog_ext = insns.to_vec().clone();
-
-    let inject_pos = match prog_ext.iter().enumerate().find_map(|(pos, insn)| {
-        (insn.code == (bpf_sys::BPF_JMP | bpf_sys::BPF_CALL) && insn.imm == magic as i32)
-            .then_some(pos)
-    }) {
-        Some(p) => p,
-        None => {
-            let none;
-            let prog_name = unsafe { libbpf_sys::bpf_program__name(prog) };
-            let prog_name = if prog_name.is_null() {
-                none = String::from("[none]");
-                &none
-            } else {
-                unsafe { std::ffi::CStr::from_ptr(prog_name) }
-                    .to_str()
-                    .unwrap()
-            };
-
-            debug!("No inline position found for {}", prog_name);
-            // Always succeed as probes may not require filtering at all.
-            return 0;
-        }
-    };
-
-    prog_ext.splice(inject_pos..inject_pos + filter.len(), filter.to_vec());
-
-    libbpf_sys::bpf_program__set_insns(
-        prog,
-        prog_ext.as_mut_slice().as_mut_ptr(),
-        prog_ext.len() as u64,
-    )
+    0
 }
