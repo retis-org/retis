retis-org/retis

commit d97427e00c2cb263f30fba359a0426fcc3f8f25b

log: add some error print-outs
Add some sample logs in the error path.

Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

src/core/filters/meta/bpf/include/meta_filter.h
@@ -79,8 +79,10 @@ static __always_inline long meta_process_ops(struct retis_meta_ctx *ctx)
 	u32 i;
 
 	val = bpf_map_lookup_elem(&filter_meta_map, &k);
-	if (!val)
+	if (!val) {
+		log_error("Failed to lookup meta-filter target");
 		return -1;
+	}
 
 	/* process target */
 	ctx->data = &val->t.u;
@@ -89,13 +91,16 @@ static __always_inline long meta_process_ops(struct retis_meta_ctx *ctx)
 
 	for (i = 1, k = 1; i < nmeta; k++, i++) {
 		val = bpf_map_lookup_elem(&filter_meta_map, &k);
-		if (!val)
+		if (!val) {
+			log_error("Failed to lookup meta-filter member at index %u", i);
 			return -1;
+		}
 
 		/* Load Pointer */
 		if (val->l.type == PTR_BIT) {
 			if (bpf_probe_read_kernel(&ptr, sizeof(void *),
-						  (char *)ctx->base + (val->l.offt)))
+						  (char *)ctx->base + (val->l.offt)) ||
+			    !ptr)
 				return -1;
 
 			ctx->base = (void *)ptr;
@@ -128,7 +133,7 @@ bool cmp_num(u64 operand1, u64 operand2, bool sign_bit, u8 cmp_type)
 	case RETIS_LE:
 		return sign_bit ? ((s64)operand1 <= (s64)operand2) : ((u64)operand1 <= (u64)operand2);
 	default:
-		/* FIXME: report error. */
+		log_error("Wrong comparison operator %d", cmp_type);
 		break;
 	}
 

src/core/events/bpf/include/events.h
@@ -3,6 +3,8 @@
 
 #include <vmlinux.h>
 
+#include <common_defs.h>
+
 /* Please keep the below in sync with its Rust counterpart. */
 #define EVENTS_MAX		8 * 1024
 #define RAW_EVENT_DATA_SIZE	1024 - 2 /* Remove the size field */
@@ -84,8 +86,10 @@ static __always_inline void *get_event_section(struct retis_raw_event *event,
 	u16 left = RAW_EVENT_DATA_SIZE - event->size;
 	void *section;
 
-	if (sizeof(*header) + size > left || event->size > sizeof(event->data))
+	if (sizeof(*header) + size > left || event->size > sizeof(event->data)) {
+		log_error("Failed to get event section: no space left");
 		return NULL;
+	}
 
 	header = (struct retis_raw_event_section_header *)
 			(event->data + event->size);

src/core/probe/bpf/include/common_defs.h
@@ -4,8 +4,6 @@
 #include <vmlinux.h>
 #include <bpf/bpf_helpers.h>
 
-#include <events.h>
-
 /* Keep in sync with its Rust counterpart in crate::core::probe */
 #define PROBE_MAX	1024
 

commit 4531bf8fba4da54809a42a583c71bc6f438f4d94

meta: do not return failure early in the case of NULL ptr
With the cited commit a NULL check snuck which introduced an early
no match in the case a ptr was followed by subsequent operations.
While this is generally correct, it may become misleading for cases
like:

retis collect -m 'sk_buff.dev.name != "lo"' -f "icmp"
  -p "tp:net:netif_receive_skb" -p "kprobe:__ip_local_out" -c skb --skb-sections all \
  --cmd "ping -c 3 8.8.8.8"

where one would expect events for both tx and rx, whereas only rx are
generated because the NULL check prevents filter_bytes() from being
processed.

Removing the check is still safe as all the reads are probed and so
any potentially subsequent read should return error.

Fixes: d97427e00c2c ("log: add some error print-outs")
Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

src/core/filters/meta/bpf/include/meta_filter.h
@@ -104,8 +104,7 @@ static __always_inline long meta_process_ops(struct retis_meta_ctx *ctx)
 		/* Load Pointer */
 		if (val->l.type == PTR_BIT) {
 			if (bpf_probe_read_kernel(&ptr, sizeof(void *),
-						  (char *)ctx->base + (val->l.offt)) ||
-			    !ptr)
+						  (char *)ctx->base + (val->l.offt)))
 				return -1;
 
 			ctx->base = (void *)ptr;