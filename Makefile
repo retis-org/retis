retis-org/retis

commit 8c796182bb3cfdee4792e654711d4e59c98511cd

tree: make retis a workspace
No mayor modifications, apart from file movement. A bit of Makefile and
path adjustment here and there.

Benchmarks (retis workspace) and retis-events unit tests were sharing
the same test data (test_events.json). This no longer works with
workspaces so duplicate the test file to decouple both things which
could evolve independently.

Makefile
@@ -42,20 +42,20 @@ ifeq ($(NOVENDOR),)
 
     LIBBPF_SYS_LIBBPF_BASE_PATH := $(dir $(shell cargo metadata --format-version=1 | jq -r '.packages | .[] | select(.name == "libbpf-sys") | .manifest_path'))
     LIBBPF_SYS_LIBBPF_INCLUDES :=  $(wildcard $(addprefix $(LIBBPF_SYS_LIBBPF_BASE_PATH)/libbpf/src/, $(LIBBPF_API_HEADERS)))
-    LIBBPF_INCLUDES := $(ROOT_DIR)/src/.out
+    LIBBPF_INCLUDES := $(ROOT_DIR)/retis/src/.out
 endif
 
 # Taking errno.h from libc instead of linux headers.
 # TODO: Remove when we fix proper header dependencies.
-INCLUDES_ALL := $(abspath $(wildcard $(shell find src -type d -path '*/bpf/include') \
+INCLUDES_ALL := $(abspath $(wildcard $(shell find retis/src -type d -path '*/bpf/include') \
                                      /usr/include/x86_64-linux-gnu))
 INCLUDES_ALL += $(LIBBPF_INCLUDES)
 
 INCLUDES := $(addprefix -I, $(INCLUDES_ALL))
 
-EBPF_PROBES := $(abspath $(wildcard src/core/probe/*/bpf))
+EBPF_PROBES := $(abspath $(wildcard retis/src/core/probe/*/bpf))
 
-EBPF_HOOKS := $(abspath $(wildcard src/module/*/bpf))
+EBPF_HOOKS := $(abspath $(wildcard retis/src/module/*/bpf))
 
 all: debug
 

commit c71179105724b67718c85b93cb61a608eac64990

Makefile: fix install target.
Since 8c796182bb3c ("tree: make retis a workspace") files were moved
around to accomodate the new workspace structure. Because of that install
target stopped working with the following error:

[...]
[CARGO] building retis (release) ...
error: found a virtual manifest at `[...]/retis/Cargo.toml` instead
  of a package manifest
make: *** [Makefile:63: install] Error 101

Fix it specifying the updated path to cargo install.

Fixes: 8c796182bb3c ("tree: make retis a workspace")
Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

Makefile
@@ -60,7 +60,7 @@ EBPF_HOOKS := $(abspath $(wildcard retis/src/module/*/bpf))
 all: debug
 
 install: release
-	$(CARGO) $(CARGO_OPTS) install $(CARGO_INSTALL_OPTS) --path=. --offline --frozen
+	$(CARGO) $(CARGO_OPTS) install $(CARGO_INSTALL_OPTS) --path=$(ROOT_DIR)/retis --offline --frozen
 
 define build
 	$(call out_console,CARGO,$(strip $(2)) ...)