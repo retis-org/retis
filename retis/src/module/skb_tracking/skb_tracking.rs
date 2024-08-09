6b4b641e092c20c20eb6753840e6764773f65591
Antoine Tenart <atenart@redhat.com>
2024-08-09 14:38:29 +0200

    module: allow modules not to provide an event factory
    This makes modules not required to produce events, as they can provide
    value by doing other scoped work (eg. installing probes).
    
    This shows again the limits of our module abstraction and it might be
    removed at some point to clean things up a bit.
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- retis/src/module/skb_tracking/skb_tracking.rs
+++ retis/src/module/skb_tracking/skb_tracking.rs
@@ -45,8 +45,8 @@ impl Module for SkbTrackingModule {
     fn collector(&mut self) -> &mut dyn Collector {
         self
     }
-    fn section_factory(&self) -> Result<Box<dyn EventSectionFactory>> {
-        Ok(Box::new(SkbTrackingEventFactory {}))
+    fn section_factory(&self) -> Result<Option<Box<dyn EventSectionFactory>>> {
+        Ok(Some(Box::new(SkbTrackingEventFactory {})))
     }
 }
 
