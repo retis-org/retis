f66f86843bf46578f00943846f2044da38ca9e72
Paolo Valerio <pvalerio@redhat.com>
2024-09-27 11:44:28 +0200

    events: declare and generate bindings for common_event
    Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

--- retis/src/core/events/bpf/include/events.h
+++ retis/src/core/events/bpf/include/events.h
@@ -124,7 +124,7 @@ static __always_inline u16 get_event_size(struct retis_raw_event *event)
 struct common_event {
 	u64 timestamp;
 	u32 smp_id;
-};
+} __binding;
 
 struct common_task_event {
 	u64 pid;
