retis-org/retis

commit f28c3d175b388127d5725edbb13c746087e740b0

Makefile: augment RUSTFLAGS globally in release
Add the warnings flag to RUSTFLAGS globally in the release target to
prevent targets depending on this one to build Retis again when
executed.

For example `make install` was unconditionally rebuilding Retis even if
a release build was done before, this was because the RUSTFLAGS differed
between the targets. We now set it for all future targets, so the build
states are consistent.

Reported-by: Paolo Valerio <pvalerio@redhat.com>
Signed-off-by: Antoine Tenart <atenart@redhat.com>

Makefile
@@ -72,7 +72,7 @@ endef
 debug: ebpf
 	$(call build, build, building retis (debug))
 
-release: RUSTFLAGS += -D warnings
+release: $(eval RUSTFLAGS += -D warnings)
 release: ebpf
 	$(call build, build --release, building retis (release))
 

commit 4618e33c13c2a1179e556bfc2ba7b651846039d2

Makefile: scope RUSTFLAGS eval under release
Previously this was evaluated at parsing time making it global.
This is still not ideal as calling multiple targets in a single run
like:

make release all

will still make RUSTFLAGS visible for the all target, but fixes the
rebuild-all issue faced in envs using rust-analyzer (or simply locally
running cargo build).

Fixes: f28c3d175b38 ("Makefile: augment RUSTFLAGS globally in release")
Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

Makefile
@@ -72,8 +72,8 @@ endef
 debug: ebpf
 	$(call build, build, building retis (debug))
 
-release: $(eval RUSTFLAGS += -D warnings)
 release: ebpf
+	$(eval RUSTFLAGS += -D warnings)
 	$(call build, build --release, building retis (release))
 
 test: ebpf