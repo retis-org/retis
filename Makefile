retis-org/retis

commit f28c3d175b388127d5725edbb13c746087e740b0

Makefile: augment RUSTFLAGS globally in release
Add the warnings flag to RUSTFLAGS globally in the release target to
prevent targets depending on this one to build Retis again when
executed.

For example `make install` was unconditionally rebuilding Retis even if
a release build was done before, this was because the RUSTFLAGS differed
between the targets. We now set it for all future targets, so the build
states are consistent.

Reported-by: Paolo Valerio <pvalerio@redhat.com>
Signed-off-by: Antoine Tenart <atenart@redhat.com>

Makefile
@@ -72,7 +72,7 @@ endef
 debug: ebpf
 	$(call build, build, building retis (debug))
 
-release: RUSTFLAGS += -D warnings
+release: $(eval RUSTFLAGS += -D warnings)
 release: ebpf
 	$(call build, build --release, building retis (release))
 

commit 5c0421d7177f43402126753c25fd4dfd91242286

Makefile: remove RUSTFLAGS eval
Previously this was evaluated at parsing time making it global.
This is still not ideal as calling multiple targets in a single run
like:

make release all

The patch avoids exporting RUSTFLAGS altogether allowing it to be
specified during make invocation.

Also, as suggested by Adrián avoid to export CFLAGS to avoid
rebuilding vprintf, libbpf-rs.

Fixes: f28c3d175b38 ("Makefile: augment RUSTFLAGS globally in release")
Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

Makefile
@@ -9,8 +9,9 @@ ARCH := $(if $(CARGO_BUILD_TARGET),$(firstword $(subst -, ,$(CARGO_BUILD_TARGET)
 
 RELEASE_VERSION = $(shell tools/localversion)
 RELEASE_NAME ?= $(shell $(CARGO) metadata --no-deps --format-version=1 | jq -r '.packages | .[] | select(.name=="retis") | .metadata.misc.release_name')
+RELEASE_FLAGS = -Dwarnings
 
-export ARCH CFLAGS CLANG LCC OBJCOPY RELEASE_NAME RELEASE_VERSION RUSTFLAGS
+export ARCH CLANG LCC OBJCOPY RELEASE_NAME RELEASE_VERSION
 
 PRINT = printf
 
@@ -60,27 +61,28 @@ EBPF_HOOKS := $(abspath $(wildcard retis/src/module/*/bpf))
 all: debug
 
 install: release
+	RUSTFLAGS="$(RUSTFLAGS) $(RELEASE_FLAGS)" \
 	$(CARGO) $(CARGO_OPTS) install $(CARGO_INSTALL_OPTS) --path=$(ROOT_DIR)/retis --offline --frozen
 
 define build
 	$(call out_console,CARGO,$(strip $(2)) ...)
 	jobs=$(patsubst -j%,%,$(filter -j%,$(MAKEFLAGS))); \
 	CARGO_BUILD_JOBS=$${jobs:-1} \
+	RUSTFLAGS="$(RUSTFLAGS) $(3)" \
 	$(CARGO) $(CARGO_OPTS) $(1) $(CARGO_CMD_OPTS)
 endef
 
 debug: ebpf
-	$(call build, build, building retis (debug))
+	$(call build,build,building retis (debug))
 
-release: $(eval RUSTFLAGS += -D warnings)
 release: ebpf
-	$(call build, build --release, building retis (release))
+	$(call build,build --release,building retis (release),$(RELEASE_FLAGS))
 
 test: ebpf
-	$(call build, test, building and running tests)
+	$(call build,test,building and running tests)
 
 bench: ebpf
-	$(call build, build -F benchmark --release, building benchmarks)
+	$(call build,build -F benchmark --release,building benchmarks)
 
 ifeq ($(NOVENDOR),)
 $(LIBBPF_INCLUDES): $(LIBBPF_SYS_LIBBPF_INCLUDES)
@@ -94,11 +96,11 @@ $(EBPF_PROBES): OUT_NAME := PROBE
 $(EBPF_HOOKS):  OUT_NAME := HOOK
 $(EBPF_PROBES) $(EBPF_HOOKS): $(LIBBPF_INCLUDES)
 	$(call out_console,$(OUT_NAME),building $@ ...)
-	CFLAGS_INCLUDES="$(INCLUDES)" \
+	CFLAGS="$(INCLUDES) $(CFLAGS)" \
 	$(MAKE) -r -f $(ROOT_DIR)/ebpf.mk -C $@
 
 clean-ebpf:
-	$(call out_console,CLEAN,cleaning ebpf progs...)
+	$(call out_console,CLEAN,cleaning ebpf progs ...)
 	for i in $(EBPF_PROBES) $(EBPF_HOOKS); do \
 	    $(MAKE) -r -f $(ROOT_DIR)/ebpf.mk -C $$i clean; \
 	done

ebpf.mk
@@ -21,7 +21,6 @@ BPF_CFLAGS := -target bpf \
               -Werror \
               -D__TARGET_ARCH_$(BPF_ARCH) \
 	      -O2
-CFLAGS += $(CFLAGS_INCLUDES)
 
 ALL_REQ := $(OBJS)
 