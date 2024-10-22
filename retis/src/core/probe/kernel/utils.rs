870a768eac275a723ccd0caf65db11246ed1d991
Antoine Tenart <atenart@redhat.com>
2024-10-22 10:44:22 +0200

    retis: pcap: unify user given probe format
    Reuse the same logic that is used elsewhere to parse probes, so the user
    has a unified experience. Also this allows using the same defaults and
    abbreviations.
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- retis/src/core/probe/kernel/utils.rs
+++ retis/src/core/probe/kernel/utils.rs
@@ -5,34 +5,53 @@ use crate::core::{
     probe::Probe,
 };
 
-/// Parse a user defined probe (through cli parameters) and extract its type and
-/// target. Filter results using symbols.
-pub(crate) fn parse_probe<F>(probe: &str, filter: F) -> Result<Vec<Probe>>
-where
-    F: Fn(&Symbol) -> bool,
-{
-    // We only need a lightweight version of `ProbeType` here; this simplifies
-    // the below logic.
-    enum ProbeType {
-        Kprobe,
-        Kretprobe,
-        RawTracepoint,
-    }
-    use ProbeType::*;
+/// Probe type for probes given through cli arguments.
+pub(crate) enum CliProbeType {
+    Kprobe,
+    Kretprobe,
+    RawTracepoint,
+}
 
-    let (r#type, target) = match probe.split_once(':') {
+impl CliProbeType {
+    pub(crate) fn to_str(&self) -> &'static str {
+        use CliProbeType::*;
+        match self {
+            Kprobe => "kprobe",
+            Kretprobe => "kretprobe",
+            RawTracepoint => "raw_tracepoint",
+        }
+    }
+}
+
+/// Parses a probe given as a cli argument and returns its type and the probe
+/// with the type identifier (if any).
+pub(crate) fn parse_cli_probe(input: &str) -> Result<(CliProbeType, &str)> {
+    use CliProbeType::*;
+
+    Ok(match input.split_once(':') {
         Some((type_str, target)) => match type_str {
             "kprobe" | "k" => (Kprobe, target),
             "kretprobe" | "kr" => (Kretprobe, target),
             "raw_tracepoint" | "tp" => (RawTracepoint, target),
             // If a single ':' was found in the probe name but we didn't match
             // any known type, defaults to trying using it as a raw tracepoint.
-            _ if probe.chars().filter(|c| *c == ':').count() == 1 => (RawTracepoint, probe),
+            _ if input.chars().filter(|c| *c == ':').count() == 1 => (RawTracepoint, input),
             x => bail!("Invalid TYPE {}. See the help.", x),
         },
         // If no ':' was found, defaults to kprobe.
-        None => (Kprobe, probe),
-    };
+        None => (Kprobe, input),
+    })
+}
+
+/// Parse a user defined probe (through cli parameters) and convert it to our
+/// probe representation (`Probe`).
+pub(crate) fn probe_from_cli<F>(probe: &str, filter: F) -> Result<Vec<Probe>>
+where
+    F: Fn(&Symbol) -> bool,
+{
+    use CliProbeType::*;
+
+    let (r#type, target) = parse_cli_probe(probe)?;
 
     // Convert the target to a list of matching ones for probe types
     // supporting it.
@@ -61,41 +80,41 @@ where
 #[cfg(test)]
 mod tests {
     #[test]
-    fn parse_probe() {
+    fn probe_from_cli() {
         let filter = |_: &_| true;
 
         // Valid probes.
-        assert!(super::parse_probe("consume_skb", filter).is_ok());
-        assert!(super::parse_probe("kprobe:kfree_skb_reason", filter).is_ok());
-        assert!(super::parse_probe("k:kfree_skb_reason", filter).is_ok());
-        assert!(super::parse_probe("skb:kfree_skb", filter).is_ok());
-        assert!(super::parse_probe("tp:skb:kfree_skb", filter).is_ok());
-        assert!(super::parse_probe("tcp_v6_*", filter).is_ok());
-        assert!(super::parse_probe("kprobe:tcp_v6_*", filter).is_ok());
-        assert!(!super::parse_probe("kprobe:tcp_v6_*", filter)
+        assert!(super::probe_from_cli("consume_skb", filter).is_ok());
+        assert!(super::probe_from_cli("kprobe:kfree_skb_reason", filter).is_ok());
+        assert!(super::probe_from_cli("k:kfree_skb_reason", filter).is_ok());
+        assert!(super::probe_from_cli("skb:kfree_skb", filter).is_ok());
+        assert!(super::probe_from_cli("tp:skb:kfree_skb", filter).is_ok());
+        assert!(super::probe_from_cli("tcp_v6_*", filter).is_ok());
+        assert!(super::probe_from_cli("kprobe:tcp_v6_*", filter).is_ok());
+        assert!(!super::probe_from_cli("kprobe:tcp_v6_*", filter)
             .unwrap()
             .is_empty());
-        assert!(super::parse_probe("kretprobe:tcp_*", filter).is_ok());
-        assert!(super::parse_probe("kr:tcp_*", filter).is_ok());
-        assert!(super::parse_probe("tp:skb:kfree_*", filter).is_ok());
-        assert!(super::parse_probe("tp:*skb*", filter).is_ok());
+        assert!(super::probe_from_cli("kretprobe:tcp_*", filter).is_ok());
+        assert!(super::probe_from_cli("kr:tcp_*", filter).is_ok());
+        assert!(super::probe_from_cli("tp:skb:kfree_*", filter).is_ok());
+        assert!(super::probe_from_cli("tp:*skb*", filter).is_ok());
 
         // Invalid probe: symbol does not exist.
-        assert!(super::parse_probe("foobar", filter).is_err());
-        assert!(super::parse_probe("kprobe:foobar", filter).is_err());
-        assert!(super::parse_probe("tp:42:foobar", filter).is_err());
-        assert!(super::parse_probe("tp:kfree_*", filter).is_err());
-        assert!(super::parse_probe("*foo*", filter).is_err());
+        assert!(super::probe_from_cli("foobar", filter).is_err());
+        assert!(super::probe_from_cli("kprobe:foobar", filter).is_err());
+        assert!(super::probe_from_cli("tp:42:foobar", filter).is_err());
+        assert!(super::probe_from_cli("tp:kfree_*", filter).is_err());
+        assert!(super::probe_from_cli("*foo*", filter).is_err());
 
         // Invalid probe: wrong TYPE.
-        assert!(super::parse_probe("kprobe:skb:kfree_skb", filter).is_err());
-        assert!(super::parse_probe("foo:kfree_skb", filter).is_err());
+        assert!(super::probe_from_cli("kprobe:skb:kfree_skb", filter).is_err());
+        assert!(super::probe_from_cli("foo:kfree_skb", filter).is_err());
 
         // Invalid probe: empty parts.
-        assert!(super::parse_probe("", filter).is_err());
-        assert!(super::parse_probe("kprobe:", filter).is_err());
-        assert!(super::parse_probe("tp:", filter).is_err());
-        assert!(super::parse_probe("tp:skb:", filter).is_err());
-        assert!(super::parse_probe(":kfree_skb_reason", filter).is_err());
+        assert!(super::probe_from_cli("", filter).is_err());
+        assert!(super::probe_from_cli("kprobe:", filter).is_err());
+        assert!(super::probe_from_cli("tp:", filter).is_err());
+        assert!(super::probe_from_cli("tp:skb:", filter).is_err());
+        assert!(super::probe_from_cli(":kfree_skb_reason", filter).is_err());
     }
 }
