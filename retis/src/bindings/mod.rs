366a51a3934ed48f33b7c65ffe3dd4d02eb49ca2
Paolo Valerio <pvalerio@redhat.com>
2024-10-28 15:23:06 +0100

    skb_tracking: declare and generate bindings
    Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

--- retis/src/bindings/mod.rs
+++ retis/src/bindings/mod.rs
@@ -72,3 +72,11 @@ impl nft_event {
     }
 }
 
+pub(crate) mod skb_tracking_uapi;
+use skb_tracking_uapi::*;
+
+unsafe impl plain::Plain for tracking_config {}
+unsafe impl plain::Plain for tracking_info {}
+
+pub(crate) mod tracking_hook_uapi;
+
