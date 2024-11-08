retis-org/retis

commit d5350df262241ea1c9b1e6c29534763f5465ccac

events: add our own formatter
Add our own formatter and an associated configuration struct. The
scope of a configuration object is expected to be within a single
`display()` call.

Using our own formatter provides better flexibility over how we print
our events and better performances.

Flexibility improvements:

- Indentation is handled at the formatter level. Thanks to this
  sub-sections and event types can now print multi-line events.

- Delimiter can be used while printing parts of an event.

Performances improvements:

- An internal buffer is used which helps reducing write calls as events
  usually have many write!() call in sequences.

- This buffer is also used to only apply the formatting logic (eg.
  indentation) once in a while, reducing the costs.

Before the series,

1M_time_single_single: 988812µs
1M_time_single_multi: 995100µs
1M_time_series_single: 1043459µs
1M_time_series_multi: 1039526µs

After the series,

1M_time_single_single: 842520µs
1M_time_single_multi: 877022µs
1M_time_series_single: 971998µs
1M_time_series_multi: 997519µs

Also the benchmark does not use stdout() which has a lock, so benefits
should be slightly better.

Signed-off-by: Antoine Tenart <atenart@redhat.com>

retis-events/src/display.rs
@@ -1,4 +1,9 @@
-use std::fmt;
+use std::{
+    fmt::{self, Write},
+    result, str,
+};
+
+use log::warn;
 
 use super::TimeSpec;
 
@@ -45,6 +50,165 @@ impl DisplayFormat {
     }
 }
 
+/// `Formatter` implements `std::fmt::Write` and controls how events are being
+/// displayed.  This is similar to `std::fmt::Formatter` but with our own
+/// constraints.
+///
+/// It supports the following capabilities: indentation and itemization. Each of
+/// those are always context-based: the capabilities and their configuration can
+/// change over time and might end based on input (eg. itemization).
+pub struct Formatter<'a, 'inner> {
+    inner: &'a mut fmt::Formatter<'inner>,
+    pub conf: FormatterConf,
+    /// Indentation level (in spaces).
+    level: usize,
+    /// True if the next input is the start of a block (aka. first call to
+    /// `flush_buf`).
+    first: bool,
+    /// True if the next input is the start of a line.
+    start: bool,
+    /// Buffer holding the output before being flushed.
+    buf: String,
+}
+
+impl<'a, 'inner> Formatter<'a, 'inner> {
+    pub fn new(
+        inner: &'a mut fmt::Formatter<'inner>,
+        conf: FormatterConf,
+    ) -> Formatter<'a, 'inner> {
+        let level = conf.level;
+
+        Self {
+            inner,
+            conf,
+            level,
+            first: true,
+            start: true,
+            buf: String::with_capacity(4096usize),
+        }
+    }
+
+    /// Directly implement write_fmt to avoid the need of an explicit
+    /// `use fmt::Write` by every user. See the `std::write` documentation.
+    #[inline]
+    pub fn write_fmt(&mut self, args: fmt::Arguments<'_>) -> result::Result<(), fmt::Error> {
+        <Self as fmt::Write>::write_fmt(self, args)
+    }
+
+    pub fn flush_buf(&mut self) -> result::Result<(), fmt::Error> {
+        let first = self.first;
+        match self.buf.is_empty() {
+            true => return Ok(()),
+            false => self.first = false,
+        }
+
+        let mut lines = self.buf.split('\n');
+
+        // Compute the prefix including the itemization char, if any.
+        let mut prefix = " ".repeat(self.level);
+        if first && self.level >= 2 {
+            if let Some(item) = self.conf.item {
+                prefix.replace_range(self.level - 2..self.level - 1, &item.to_string());
+            }
+        }
+
+        if let Some(line) = lines.next() {
+            if self.start {
+                self.start = false;
+                self.inner.write_str(&prefix)?;
+            }
+            self.inner.write_str(line)?;
+        }
+
+        // Reset the itemization char, if any.
+        if first && self.level >= 2 && self.conf.item.is_some() {
+            prefix = " ".repeat(self.level);
+        }
+
+        lines.try_for_each(|line| {
+            self.inner.write_char('\n')?;
+            self.inner.write_str(&prefix)?;
+            self.inner.write_str(line)
+        })?;
+
+        if self.buf.ends_with('\n') {
+            self.inner.write_char('\n')?;
+            self.start = true;
+        }
+
+        self.buf.clear();
+        Ok(())
+    }
+}
+
+impl fmt::Write for Formatter<'_, '_> {
+    fn write_str(&mut self, s: &str) -> result::Result<(), fmt::Error> {
+        if self.conf.level != self.level {
+            if !self.buf.is_empty() {
+                self.flush_buf()?;
+            }
+            self.level = self.conf.level;
+        }
+
+        self.buf.push_str(s);
+        Ok(())
+    }
+}
+
+impl Drop for Formatter<'_, '_> {
+    fn drop(&mut self) {
+        if !self.buf.is_empty() {
+            self.flush_buf().expect("Could not flush Formatter buffer");
+        }
+    }
+}
+
+/// Configuration for the `Formatter`. It can be shared between multiple
+/// `EventDisplay::display` calls but its scope is restricted to a single call.
+/// This means a base configuration can be shared for multiple
+/// `EventDisplay::display` call but any modification made within an
+/// `EventDisplay::display` call won't be visibile outside.
+#[derive(Clone, Default)]
+pub struct FormatterConf {
+    level: usize,
+    saved_levels: Vec<usize>,
+    item: Option<char>,
+}
+
+impl FormatterConf {
+    pub fn new() -> Self {
+        Self::with_level(0)
+    }
+
+    pub fn with_level(level: usize) -> Self {
+        Self {
+            level,
+            ..Default::default()
+        }
+    }
+
+    /// Increase the indentation level by `diff`.
+    pub fn inc_level(&mut self, diff: usize) {
+        self.saved_levels.push(self.level);
+        self.level += diff;
+    }
+
+    /// Reset the indentation level to its previous value.
+    pub fn reset_level(&mut self) {
+        match self.saved_levels.pop() {
+            Some(level) => {
+                self.level = level;
+            }
+            None => warn!("Cannot reset the indentation level"),
+        }
+    }
+
+    /// Set an itemization char to be printed at the start of output, or None.
+    pub fn set_item(&mut self, item: Option<char>) {
+        self.item = item;
+    }
+}
+
 /// Trait controlling how an event or an event section (or any custom type
 /// inside it) is displayed. It works by providing an helper returning an
 /// implementation of the std::fmt::Display trait, which can be used later to
@@ -52,7 +216,11 @@ impl DisplayFormat {
 /// arguments, unlike a plain std::fmt::Display implementation.
 pub trait EventDisplay<'a>: EventFmt {
     /// Display the event using the default event format.
-    fn display(&'a self, format: &'a DisplayFormat) -> Box<dyn fmt::Display + 'a>;
+    fn display(
+        &'a self,
+        format: &'a DisplayFormat,
+        conf: &'a FormatterConf,
+    ) -> Box<dyn fmt::Display + 'a>;
 }
 
 /// Trait controlling how an event or an event section (or any custom type
@@ -64,26 +232,33 @@ pub trait EventDisplay<'a>: EventFmt {
 /// members if any.
 pub trait EventFmt {
     /// Default formatting of an event.
-    fn event_fmt(&self, f: &mut fmt::Formatter, format: &DisplayFormat) -> fmt::Result;
+    fn event_fmt(&self, f: &mut Formatter, format: &DisplayFormat) -> fmt::Result;
 }
 
 impl<'a, T> EventDisplay<'a> for T
 where
     T: EventFmt,
 {
-    fn display(&'a self, format: &'a DisplayFormat) -> Box<dyn fmt::Display + 'a> {
+    fn display(
+        &'a self,
+        format: &'a DisplayFormat,
+        conf: &'a FormatterConf,
+    ) -> Box<dyn fmt::Display + 'a> {
         struct DefaultDisplay<'a, U> {
             myself: &'a U,
             format: &'a DisplayFormat,
+            conf: &'a FormatterConf,
         }
         impl<U: EventFmt> fmt::Display for DefaultDisplay<'_, U> {
             fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                self.myself.event_fmt(f, self.format)
+                self.myself
+                    .event_fmt(&mut Formatter::new(f, self.conf.clone()), self.format)
             }
         }
         Box::new(DefaultDisplay {
             myself: self,
             format,
+            conf,
         })
     }
 }
@@ -95,7 +270,7 @@ where
 ///
 /// ```
 /// use std::fmt;
-/// use retis_events::DelimWriter;
+/// use retis_events::{Formatter, FormatterConf, DelimWriter};
 ///
 /// struct Flags {
 ///     opt1: bool,
@@ -103,15 +278,17 @@ where
 /// }
 /// impl fmt::Display for Flags {
 ///     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-///         write!(f, "flags")?;
+///         let mut f = Formatter::new(f, FormatterConf::new());
+///
+///         write!(&mut f, "flags")?;
 ///         let mut space = DelimWriter::new(' ');
 ///         if self.opt1 {
-///             space.write(f)?;
-///             write!(f, "opt1");
+///             space.write(&mut f)?;
+///             write!(&mut f, "opt1");
 ///          }
 ///         if self.opt2 {
-///             space.write(f)?;
-///             write!(f, "opt2")?;
+///             space.write(&mut f)?;
+///             write!(&mut f, "opt2")?;
 ///          }
 ///          Ok(())
 ///     }
@@ -129,7 +306,7 @@ impl DelimWriter {
     }
 
     /// If it's not the first time it's called, write the delimiter.
-    pub fn write(&mut self, f: &mut fmt::Formatter) -> fmt::Result {
+    pub fn write(&mut self, f: &mut Formatter) -> fmt::Result {
         match self.first {
             true => self.first = false,
             false => write!(f, "{}", self.delim)?,

retis-events/src/common.rs
@@ -2,7 +2,7 @@ use std::fmt;
 
 use chrono::{DateTime, Utc};
 
-use crate::{event_section, event_type, *};
+use crate::*;
 
 /// Startup event section. Contains global information about a collection as a
 /// whole, with data gathered at collection startup time.
@@ -15,7 +15,7 @@ pub struct StartupEvent {
 }
 
 impl EventFmt for StartupEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, _: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, _: &DisplayFormat) -> fmt::Result {
         write!(f, "Retis version {}", self.retis_version)
     }
 }
@@ -44,7 +44,7 @@ pub struct CommonEvent {
 }
 
 impl EventFmt for CommonEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, format: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, format: &DisplayFormat) -> fmt::Result {
         match format.time_format {
             TimeFormat::MonotonicTimestamp => write!(f, "{}", self.timestamp)?,
             TimeFormat::UtcDate => match format.monotonic_offset {

retis-events/src/ct.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 
 use super::*;
-use crate::{event_section, event_type};
+use crate::{event_section, event_type, Formatter};
 
 #[event_type]
 #[derive(Default)]
@@ -128,7 +128,7 @@ pub struct CtConnEvent {
 }
 
 impl EventFmt for CtEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, _: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, _: &DisplayFormat) -> fmt::Result {
         use CtState::*;
         match self.state {
             Established => write!(f, "ct_state ESTABLISHED ")?,
@@ -152,7 +152,7 @@ impl EventFmt for CtEvent {
 }
 
 impl CtEvent {
-    fn format_conn(conn: &CtConnEvent, f: &mut fmt::Formatter) -> fmt::Result {
+    fn format_conn(conn: &CtConnEvent, f: &mut Formatter) -> fmt::Result {
         match (&conn.orig.proto, &conn.reply.proto) {
             (CtProto::Tcp(tcp_orig), CtProto::Tcp(tcp_reply)) => {
                 write!(

retis-events/src/events.rs
@@ -121,7 +121,7 @@ impl Event {
 }
 
 impl EventFmt for Event {
-    fn event_fmt(&self, f: &mut std::fmt::Formatter, format: &DisplayFormat) -> std::fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, format: &DisplayFormat) -> std::fmt::Result {
         // First format the first event line starting with the always-there
         // {common} section, followed by the {kernel} or {user} one.
         self.0
@@ -151,15 +151,19 @@ impl EventFmt for Event {
         }
 
         // Separator between each following sections.
-        let sep = if format.multiline { "\n  " } else { " " };
+        let sep = if format.multiline { '\n' } else { ' ' };
 
         // If we have a stack trace, show it.
         if let Some(kernel) = self.get_section::<KernelEvent>(SectionId::Kernel) {
             if let Some(stack) = &kernel.stack_trace {
+                f.conf.inc_level(4);
                 write!(f, "{sep}")?;
                 stack.event_fmt(f, format)?;
+                f.conf.reset_level();
             }
         }
+
+        f.conf.inc_level(2);
 
         // Finally show all sections.
         (SectionId::Skb.to_u8()..SectionId::_MAX.to_u8())
@@ -171,6 +175,7 @@ impl EventFmt for Event {
                 section.event_fmt(f, format)
             })?;
 
+        f.conf.reset_level();
         Ok(())
     }
 }

retis-events/src/kernel.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 
 use super::*;
-use crate::{event_section, event_type};
+use crate::{event_section, event_type, Formatter};
 
 #[event_section("kernel")]
 pub struct KernelEvent {
@@ -14,7 +14,7 @@ pub struct KernelEvent {
 }
 
 impl EventFmt for KernelEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, _: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, _: &DisplayFormat) -> fmt::Result {
         write!(
             f,
             "[{}] {}",
@@ -42,7 +42,7 @@ impl StackTrace {
 }
 
 impl EventFmt for StackTrace {
-    fn event_fmt(&self, f: &mut fmt::Formatter, format: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, format: &DisplayFormat) -> fmt::Result {
         let last = self.0.len() - 1;
         if format.multiline {
             self.0.iter().enumerate().try_for_each(|(i, sym)| {

retis-events/src/nft.rs
@@ -1,7 +1,7 @@
 use std::{fmt, str};
 
 use super::*;
-use crate::event_section;
+use crate::{event_section, Formatter};
 
 /// Nft event section
 #[event_section("nft")]
@@ -17,7 +17,7 @@ pub struct NftEvent {
 }
 
 impl EventFmt for NftEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, _: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, _: &DisplayFormat) -> fmt::Result {
         write!(
             f,
             "table {} ({}) chain {} ({})",

retis-events/src/ovs.rs
@@ -4,7 +4,7 @@ use anyhow::{bail, Result};
 use serde::{de::Error as Derror, ser::Error as Serror, Deserialize, Deserializer, Serializer};
 
 use super::*;
-use crate::{event_section, event_type};
+use crate::{event_section, event_type, Formatter};
 
 ///The OVS Event
 #[derive(PartialEq)]
@@ -16,7 +16,7 @@ pub struct OvsEvent {
 }
 
 impl EventFmt for OvsEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, format: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, format: &DisplayFormat) -> fmt::Result {
         self.event.event_fmt(f, format)
     }
 }
@@ -58,7 +58,7 @@ pub enum OvsEventType {
 }
 
 impl EventFmt for OvsEventType {
-    fn event_fmt(&self, f: &mut fmt::Formatter, format: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, format: &DisplayFormat) -> fmt::Result {
         use OvsEventType::*;
         let disp: &dyn EventFmt = match self {
             Upcall(e) => e,
@@ -102,7 +102,7 @@ pub struct UpcallEvent {
 }
 
 impl EventFmt for UpcallEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, _: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, _: &DisplayFormat) -> fmt::Result {
         write!(
             f,
             "upcall{} port {} cpu {}",
@@ -134,7 +134,7 @@ pub struct UpcallEnqueueEvent {
 }
 
 impl EventFmt for UpcallEnqueueEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, _: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, _: &DisplayFormat) -> fmt::Result {
         write!(
             f,
             "upcall_enqueue{} ({}/{}) q {} ret {}",
@@ -158,7 +158,7 @@ pub struct UpcallReturnEvent {
 }
 
 impl EventFmt for UpcallReturnEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, _: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, _: &DisplayFormat) -> fmt::Result {
         write!(
             f,
             "upcall_ret ({}/{}) ret {}",
@@ -218,7 +218,7 @@ impl OperationEvent {
 }
 
 impl EventFmt for OperationEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, _: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, _: &DisplayFormat) -> fmt::Result {
         write!(
             f,
             "flow_{} q {} ts {} ({})",
@@ -250,7 +250,7 @@ pub struct RecvUpcallEvent {
 }
 
 impl EventFmt for RecvUpcallEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, _: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, _: &DisplayFormat) -> fmt::Result {
         // FIXME: there are more fields.
         write!(
             f,
@@ -276,7 +276,7 @@ pub struct ActionEvent {
 }
 
 impl EventFmt for ActionEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, _: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, _: &DisplayFormat) -> fmt::Result {
         if self.recirc_id != 0 {
             write!(f, "[recirc_id {:#x}] ", self.recirc_id)?;
         }

retis-events/src/skb.rs
@@ -4,7 +4,7 @@ use super::{
     net::{etype_str, protocol_str, RawPacket},
     *,
 };
-use crate::{event_section, event_type};
+use crate::{event_section, event_type, Formatter};
 
 /// Skb event section.
 #[event_section("skb")]
@@ -38,7 +38,7 @@ pub struct SkbEvent {
 }
 
 impl EventFmt for SkbEvent {
-    fn event_fmt(&self, f: &mut fmt::Formatter, _: &DisplayFormat) -> fmt::Result {
+    fn event_fmt(&self, f: &mut Formatter, _: &DisplayFormat) -> fmt::Result {
         let mut len = 0;
 
         let mut space = DelimWriter::new(' ');

commit ae62c110060f126063637e71ed68a4b428af17b4

events: formatting: fix newline at end of buffer handling
When a newline is found at the end of the buffer it was printed twice.
This is because splitting such string will result in having an empty
last split. The logic was then printing a new line for the split and
because one was at the end of the buffer.

This also prevents from printing the prefix and fixes cases where a
double newline is found in the middle of the buffer.

Fixes: d5350df26224 ("events: add our own formatter")
Reported-by: Adrian Moreno <amorenoz@redhat.com>
Signed-off-by: Antoine Tenart <atenart@redhat.com>

retis-events/src/display.rs
@@ -125,14 +125,18 @@ impl<'a, 'inner> Formatter<'a, 'inner> {
             prefix = " ".repeat(self.level);
         }
 
+        // If the buffer ends with a newline, the last split will be empty. In
+        // such case only print the newline.
         lines.try_for_each(|line| {
             self.inner.write_char('\n')?;
-            self.inner.write_str(&prefix)?;
-            self.inner.write_str(line)
+            if !line.is_empty() {
+                self.inner.write_str(&prefix)?;
+                self.inner.write_str(line)?;
+            }
+            Ok(())
         })?;
 
         if self.buf.ends_with('\n') {
-            self.inner.write_char('\n')?;
             self.start = true;
         }
 