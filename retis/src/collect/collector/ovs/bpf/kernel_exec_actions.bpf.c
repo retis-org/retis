8d69f72cc9c305af8b1be8d04da51870df7b8240
Paolo Valerio <pvalerio@redhat.com>
2024-11-10 17:29:06 +0100

    ovs: extract ufid and generate the related event.
    While at it, n_{mask,cache}_hit have been included.
    
    Ideally, this should be done with fexit indexing by sw_flow_key, but
    given that is missing, it reuses the existing inflight_exec map.
    
    Signed-off-by: Paolo Valerio <pvalerio@redhat.com>
    Co-authored-by: Adrian Moreno <amorenoz@redhat.com>
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- retis/src/collect/collector/ovs/bpf/kernel_exec_actions.bpf.c
+++ retis/src/collect/collector/ovs/bpf/kernel_exec_actions.bpf.c
@@ -6,11 +6,16 @@ DEFINE_HOOK(F_AND, RETIS_F_PACKET_PASS,
 	u32 queue_id;
 	u64 tid = bpf_get_current_pid_tgid();
 	struct execute_actions_ctx ectx = {};
+	struct execute_actions_ctx *pectx;
 	struct sk_buff *skb;
 
 	skb = retis_get_sk_buff(ctx);
 	if (!skb)
 		return 0;
+
+	pectx = bpf_map_lookup_elem(&inflight_exec, &tid);
+	if (!pectx)
+		pectx = &ectx;
 
 	queue_id = queue_id_gen_skb(skb);
 
@@ -19,13 +24,18 @@ DEFINE_HOOK(F_AND, RETIS_F_PACKET_PASS,
 		 * command and store the current queue_id so that further
 		 * actions will use the same one regardless of packet
 		 * modifications. */
-		ectx.queue_id = queue_id;
-		ectx.command = true;
+		pectx->queue_id = queue_id;
+		pectx->command = true;
 	}
 	bpf_map_delete_elem(&flow_exec_tracking, &queue_id);
-	ectx.skb = skb;
 
-	if (!bpf_map_update_elem(&inflight_exec, &tid, &ectx, BPF_ANY))
+	if (pectx->skb && pectx->skb != skb) {
+		log_error("skb stored while processing differs when executing actions");
+		pectx->skb = skb;
+	}
+
+	if ((pectx == &ectx) &&
+	    !bpf_map_update_elem(&inflight_exec, &tid, pectx, BPF_ANY))
 		return 0;
 
 	return 0;
