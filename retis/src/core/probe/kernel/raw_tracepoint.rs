retis-org/retis

commit 125136798f5fdff5232be1a153f5453f2319d296

tree: update libbpf-rs/sys
The main change is the introduction of a workaround because the libbpf
skeleton now require a storage to be provided. A detailed explanation
about this is provided alongside the workaround itself.

Other than that, the usual API breakages when upgrading libbpf-rs:

- Accessing maps & programs from (open)objects is now done using an
  iterator instead of providing the name

- Some map operations were moved into a new trait: MapCore.

- The API to access rodata from the skeleton has slightly changed.

Signed-off-by: Antoine Tenart <atenart@redhat.com>

retis/src/core/probe/kernel/raw_tracepoint.rs
@@ -5,10 +5,13 @@
 //! in two parts, the Rust code (here) and the eBPF one
 //! (bpf/raw_tracepoint.bpf.c and its auto-generated part in bpf/.out/).
 
-use std::os::fd::{AsFd, AsRawFd, RawFd};
+use std::{
+    mem::MaybeUninit,
+    os::fd::{AsFd, AsRawFd, RawFd},
+};
 
 use anyhow::{anyhow, bail, Result};
-use libbpf_rs::skel::SkelBuilder;
+use libbpf_rs::skel::{OpenSkel, Skel, SkelBuilder};
 
 use crate::core::{filters::Filter, probe::builder::*, probe::*};
 
@@ -22,7 +25,6 @@ pub(crate) struct RawTracepointBuilder {
     hooks: Vec<Hook>,
     filters: Vec<Filter>,
     links: Vec<libbpf_rs::Link>,
-    obj: Option<libbpf_rs::Object>,
     map_fds: Vec<(String, RawFd)>,
 }
 
@@ -45,30 +47,32 @@ impl ProbeBuilder for RawTracepointBuilder {
     }
 
     fn attach(&mut self, probe: &Probe) -> Result<()> {
-        let mut skel = RawTracepointSkelBuilder::default().open()?;
+        let mut open_object = MaybeUninit::uninit();
+        let mut skel = RawTracepointSkelBuilder::default().open(&mut open_object)?;
 
         let probe = match probe.r#type() {
             ProbeType::RawTracepoint(probe) => probe,
             _ => bail!("Wrong probe type {}", probe),
         };
 
-        skel.rodata_mut().ksym = probe.symbol.addr()?;
-        skel.rodata_mut().nargs = probe.symbol.nargs()?;
-        skel.rodata_mut().nhooks = self.hooks.len() as u32;
-        skel.rodata_mut().log_level = log::max_level() as u8;
+        skel.maps.rodata_data.ksym = probe.symbol.addr()?;
+        skel.maps.rodata_data.nargs = probe.symbol.nargs()?;
+        skel.maps.rodata_data.nhooks = self.hooks.len() as u32;
+        skel.maps.rodata_data.log_level = log::max_level() as u8;
 
         self.filters.iter().for_each(|f| {
             if let Filter::Meta(m) = f {
-                skel.rodata_mut().nmeta = m.0.len() as u32
+                skel.maps.rodata_data.nmeta = m.0.len() as u32
             }
         });
 
-        let open_obj = skel.obj;
-        reuse_map_fds(&open_obj, &self.map_fds)?;
+        reuse_map_fds(skel.open_object_mut(), &self.map_fds)?;
 
-        let mut obj = open_obj.load()?;
-        let prog = obj
-            .prog_mut("probe_raw_tracepoint")
+        let skel = skel.load()?;
+        let prog = skel
+            .object()
+            .progs_mut()
+            .find(|p| p.name() == "probe_raw_tracepoint")
             .ok_or_else(|| anyhow!("Couldn't get program"))?;
 
         let mut links = replace_hooks(prog.as_fd().as_raw_fd(), &self.hooks)?;
@@ -76,7 +80,6 @@ impl ProbeBuilder for RawTracepointBuilder {
 
         self.links
             .push(prog.attach_raw_tracepoint(probe.symbol.attach_name())?);
-        self.obj = Some(obj);
         Ok(())
     }
 

retis/src/core/probe/user/usdt.rs
@@ -1,11 +1,15 @@
-use std::os::fd::{AsFd, AsRawFd, RawFd};
+use std::{
+    mem::MaybeUninit,
+    os::fd::{AsFd, AsRawFd, RawFd},
+};
 
 use anyhow::{anyhow, bail, Result};
-use libbpf_rs::skel::SkelBuilder;
+use libbpf_rs::skel::{OpenSkel, Skel, SkelBuilder};
 
-use crate::core::filters::Filter;
-use crate::core::probe::builder::*;
-use crate::core::probe::{Hook, Probe, ProbeType};
+use crate::core::{
+    filters::Filter,
+    probe::{builder::*, Hook, Probe, ProbeType},
+};
 
 mod usdt_bpf {
     include!("bpf/.out/usdt.skel.rs");
@@ -15,7 +19,6 @@ use usdt_bpf::UsdtSkelBuilder;
 #[derive(Default)]
 pub(crate) struct UsdtBuilder {
     links: Vec<libbpf_rs::Link>,
-    obj: Option<libbpf_rs::Object>,
     map_fds: Vec<(String, RawFd)>,
     hooks: Vec<Hook>,
 }
@@ -45,21 +48,23 @@ impl ProbeBuilder for UsdtBuilder {
             _ => bail!("Wrong probe type"),
         };
 
-        let mut skel = UsdtSkelBuilder::default().open()?;
-        skel.rodata_mut().log_level = log::max_level() as u8;
-        let open_obj = skel.obj;
-        reuse_map_fds(&open_obj, &self.map_fds)?;
+        let mut open_object = MaybeUninit::uninit();
+        let mut skel = UsdtSkelBuilder::default().open(&mut open_object)?;
+        skel.maps.rodata_data.log_level = log::max_level() as u8;
 
-        let mut obj = open_obj.load()?;
-        let prog = obj
-            .prog_mut("probe_usdt")
+        reuse_map_fds(skel.open_object_mut(), &self.map_fds)?;
+
+        let skel = skel.load()?;
+        let prog = skel
+            .object()
+            .progs_mut()
+            .find(|p| p.name() == "probe_usdt")
             .ok_or_else(|| anyhow!("Couldn't get program"))?;
         let mut links = replace_hooks(prog.as_fd().as_raw_fd(), &self.hooks)?;
         self.links.append(&mut links);
 
         self.links
             .push(prog.attach_usdt(probe.pid, &probe.path, &probe.provider, &probe.name)?);
-        self.obj = Some(obj);
 
         Ok(())
     }

retis/Cargo.toml
@@ -37,8 +37,8 @@ clap_complete = "4.4"
 elf = "0.7"
 events = {version = "1.4", path = "../retis-events", package="retis-events" }
 flate2 = "1.0"
-libbpf-rs = "0.22"
-libbpf-sys = "=1.2"
+libbpf-rs = "0.24"
+libbpf-sys = "1.5"
 libc = "0.2"
 log = { version = "0.4", features = ["std"] }
 memoffset = "0.9"
@@ -63,8 +63,8 @@ thiserror = "1.0"
 
 [build-dependencies]
 bindgen = "0.69"
-libbpf-cargo = "0.22"
-libbpf-sys = "1.0"
+libbpf-cargo = "0.24"
+libbpf-sys = "1.5"
 memmap2 = "0.9"
 
 [dev-dependencies]

retis/src/core/probe/builder.rs
@@ -2,7 +2,10 @@
 //!
 //! ProbeBuilder defines the ProbeBuider trait and some useful utility functions
 //!
-use std::os::fd::RawFd;
+use std::{
+    ffi::OsStr,
+    os::fd::{BorrowedFd, RawFd},
+};
 
 use anyhow::{anyhow, Result};
 
@@ -33,12 +36,18 @@ pub(super) trait ProbeBuilder {
 }
 
 pub(super) fn reuse_map_fds(
-    open_obj: &libbpf_rs::OpenObject,
+    open_obj: &mut libbpf_rs::OpenObject,
     map_fds: &[(String, RawFd)],
 ) -> Result<()> {
     for map in map_fds.iter() {
-        if let Some(open_map) = open_obj.map(map.0.clone()) {
-            open_map.reuse_fd(map.1)?;
+        if let Some(mut open_map) = open_obj
+            .maps_mut()
+            .find(|m| m.name() == <String as AsRef<OsStr>>::as_ref(&map.0))
+        {
+            // Map fds are always valid (they come from libbpf-rs itself) and
+            // the map objects are not destroyed until the object is dropped so
+            // the fd remains valid here.
+            open_map.reuse_fd(unsafe { BorrowedFd::borrow_raw(map.1) })?;
         } else {
             // This object does not have this particular map.
             continue;
@@ -58,18 +67,20 @@ pub(super) fn replace_hooks(fd: RawFd, hooks: &[Hook]) -> Result<Vec<libbpf_rs::
         // We have to explicitly use a Vec below to avoid having an unknown size
         // at build time.
         let map_fds: Vec<(String, RawFd)> = hook.maps.clone().into_iter().collect();
-        reuse_map_fds(&open_obj, &map_fds)?;
+        reuse_map_fds(&mut open_obj, &map_fds)?;
 
-        let open_prog = open_obj
-            .prog_mut("hook")
+        let mut open_prog = open_obj
+            .progs_mut()
+            .find(|p| p.name() == "hook")
             .ok_or_else(|| anyhow!("Couldn't get hook program"))?;
 
         open_prog.set_prog_type(libbpf_rs::ProgramType::Ext);
         open_prog.set_attach_target(fd, Some(target))?;
 
-        let mut obj = open_obj.load()?;
+        let obj = open_obj.load()?;
         links.push(
-            obj.prog_mut("hook")
+            obj.progs_mut()
+                .find(|p| p.name() == "hook")
                 .ok_or_else(|| anyhow!("Couldn't get hook program"))?
                 .attach_trace()?,
         );

retis/src/core/probe/kernel/kernel.rs
@@ -3,6 +3,8 @@
 use std::{collections::HashMap, fmt};
 
 use anyhow::{bail, Result};
+#[cfg(not(test))]
+use libbpf_rs::MapCore;
 
 use super::inspect::inspect_symbol;
 use crate::{

retis/src/core/probe/kernel/kprobe.rs
@@ -7,23 +7,23 @@
 use std::os::fd::{AsFd, AsRawFd, RawFd};
 
 use anyhow::{anyhow, bail, Result};
-use libbpf_rs::skel::SkelBuilder;
+use libbpf_rs::skel::{OpenSkel, Skel};
 
-use crate::core::{filters::Filter, probe::builder::*, probe::*};
+use crate::core::{filters::Filter, probe::builder::*, probe::*, workaround::*};
 
 mod kprobe_bpf {
     include!("bpf/.out/kprobe.skel.rs");
 }
-use kprobe_bpf::KprobeSkelBuilder;
+use kprobe_bpf::*;
 
 #[derive(Default)]
-pub(crate) struct KprobeBuilder {
+pub(crate) struct KprobeBuilder<'a> {
     links: Vec<libbpf_rs::Link>,
-    obj: Option<libbpf_rs::Object>,
+    skel: Option<SkelStorage<KprobeSkel<'a>>>,
 }
 
-impl ProbeBuilder for KprobeBuilder {
-    fn new() -> KprobeBuilder {
+impl<'a> ProbeBuilder for KprobeBuilder<'a> {
+    fn new() -> KprobeBuilder<'a> {
         KprobeBuilder::default()
     }
 
@@ -33,39 +33,41 @@ impl ProbeBuilder for KprobeBuilder {
         hooks: Vec<Hook>,
         filters: Vec<Filter>,
     ) -> Result<()> {
-        if self.obj.is_some() {
+        if self.skel.is_some() {
             bail!("Kprobe builder already initialized");
         }
 
-        let mut skel = KprobeSkelBuilder::default().open()?;
-        skel.rodata_mut().nhooks = hooks.len() as u32;
-        skel.rodata_mut().log_level = log::max_level() as u8;
+        let mut skel = OpenSkelStorage::new::<KprobeSkelBuilder>()?;
+
+        skel.maps.rodata_data.nhooks = hooks.len() as u32;
+        skel.maps.rodata_data.log_level = log::max_level() as u8;
 
         filters.iter().for_each(|f| {
             if let Filter::Meta(m) = f {
-                skel.rodata_mut().nmeta = m.0.len() as u32
+                skel.maps.rodata_data.nmeta = m.0.len() as u32
             }
         });
 
-        let open_obj = skel.obj;
-        reuse_map_fds(&open_obj, &map_fds)?;
+        reuse_map_fds(skel.open_object_mut(), &map_fds)?;
 
-        let obj = open_obj.load()?;
-        let fd = obj
-            .prog("probe_kprobe")
+        let skel = SkelStorage::load(skel)?;
+        let fd = skel
+            .object()
+            .progs()
+            .find(|p| p.name() == "probe_kprobe")
             .ok_or_else(|| anyhow!("Couldn't get program"))?
             .as_fd()
             .as_raw_fd();
         let mut links = replace_hooks(fd, &hooks)?;
         self.links.append(&mut links);
 
-        self.obj = Some(obj);
+        self.skel = Some(skel);
         Ok(())
     }
 
     fn attach(&mut self, probe: &Probe) -> Result<()> {
-        let obj = match &mut self.obj {
-            Some(obj) => obj,
+        let obj = match &mut self.skel {
+            Some(skel) => skel.object(),
             _ => bail!("Kprobe builder is uninitialized"),
         };
         let probe = match probe.r#type() {
@@ -74,7 +76,8 @@ impl ProbeBuilder for KprobeBuilder {
         };
 
         self.links.push(
-            obj.prog_mut("probe_kprobe")
+            obj.progs_mut()
+                .find(|p| p.name() == "probe_kprobe")
                 .ok_or_else(|| anyhow!("Couldn't get program"))?
                 .attach_kprobe(false, probe.symbol.attach_name())?,
         );

retis/src/core/probe/kernel/kretprobe.rs
@@ -11,23 +11,23 @@
 use std::os::fd::{AsFd, AsRawFd, RawFd};
 
 use anyhow::{anyhow, bail, Result};
-use libbpf_rs::skel::SkelBuilder;
+use libbpf_rs::skel::{OpenSkel, Skel};
 
-use crate::core::{filters::Filter, probe::builder::*, probe::*};
+use crate::core::{filters::Filter, probe::builder::*, probe::*, workaround::*};
 
 mod kretprobe_bpf {
     include!("bpf/.out/kretprobe.skel.rs");
 }
-use kretprobe_bpf::KretprobeSkelBuilder;
+use kretprobe_bpf::*;
 
 #[derive(Default)]
-pub(crate) struct KretprobeBuilder {
+pub(crate) struct KretprobeBuilder<'a> {
     links: Vec<libbpf_rs::Link>,
-    obj: Option<libbpf_rs::Object>,
+    skel: Option<SkelStorage<KretprobeSkel<'a>>>,
 }
 
-impl ProbeBuilder for KretprobeBuilder {
-    fn new() -> KretprobeBuilder {
+impl<'a> ProbeBuilder for KretprobeBuilder<'a> {
+    fn new() -> KretprobeBuilder<'a> {
         KretprobeBuilder::default()
     }
 
@@ -37,39 +37,41 @@ impl ProbeBuilder for KretprobeBuilder {
         hooks: Vec<Hook>,
         filters: Vec<Filter>,
     ) -> Result<()> {
-        if self.obj.is_some() {
+        if self.skel.is_some() {
             bail!("Kretprobe builder already initialized");
         }
 
-        let mut skel = KretprobeSkelBuilder::default().open()?;
-        skel.rodata_mut().nhooks = hooks.len() as u32;
-        skel.rodata_mut().log_level = log::max_level() as u8;
+        let mut skel = OpenSkelStorage::new::<KretprobeSkelBuilder>()?;
+
+        skel.maps.rodata_data.nhooks = hooks.len() as u32;
+        skel.maps.rodata_data.log_level = log::max_level() as u8;
 
         filters.iter().for_each(|f| {
             if let Filter::Meta(m) = f {
-                skel.rodata_mut().nmeta = m.0.len() as u32
+                skel.maps.rodata_data.nmeta = m.0.len() as u32
             }
         });
 
-        let open_obj = skel.obj;
-        reuse_map_fds(&open_obj, &map_fds)?;
+        reuse_map_fds(skel.open_object_mut(), &map_fds)?;
 
-        let obj = open_obj.load()?;
-        let fd = obj
-            .prog("probe_kretprobe_kretprobe")
+        let skel = SkelStorage::load(skel)?;
+        let fd = skel
+            .object()
+            .progs()
+            .find(|p| p.name() == "probe_kretprobe_kretprobe")
             .ok_or_else(|| anyhow!("Couldn't get program"))?
             .as_fd()
             .as_raw_fd();
         let mut links = replace_hooks(fd, &hooks)?;
         self.links.append(&mut links);
 
-        self.obj = Some(obj);
+        self.skel = Some(skel);
         Ok(())
     }
 
     fn attach(&mut self, probe: &Probe) -> Result<()> {
-        let obj = match &mut self.obj {
-            Some(obj) => obj,
+        let obj = match &mut self.skel {
+            Some(skel) => skel.object(),
             _ => bail!("Kretprobe builder is uninitialized"),
         };
 
@@ -80,14 +82,16 @@ impl ProbeBuilder for KretprobeBuilder {
 
         // Attach the kretprobe
         self.links.push(
-            obj.prog_mut("probe_kretprobe_kretprobe")
+            obj.progs_mut()
+                .find(|p| p.name() == "probe_kretprobe_kretprobe")
                 .ok_or_else(|| anyhow!("Couldn't get kretprobe program"))?
                 .attach_kprobe(true, probe.symbol.attach_name())?,
         );
 
         // Attach the kprobe
         self.links.push(
-            obj.prog_mut("probe_kretprobe_kprobe")
+            obj.progs_mut()
+                .find(|p| p.name() == "probe_kretprobe_kprobe")
                 .ok_or_else(|| anyhow!("Couldn't get kprobe program"))?
                 .attach_kprobe(false, probe.symbol.attach_name())?,
         );

retis/src/core/probe/manager.rs
@@ -7,6 +7,7 @@ use std::{
 };
 
 use anyhow::{anyhow, bail, Result};
+use libbpf_rs::MapCore;
 use log::{debug, info, warn};
 use plain::Plain;
 

commit 74962a1d8d3254635d73527f8301c3393d4db9d9

core: probes: fix segfault on exit
A segmentation fault can happen when exiting a collection and using USDT
probes. This is because a manager is allocated in libbpf when using USDT
probes, which must lives until the probe link is dropped as it is then
freed.

The segfault resurfaced as the mentioned commit actually reverted a
previous commit fixing the same issue, 9baa6fc2ca7f ("usdt: remove
ManuallyDrop<> workaround.").

Fix this by keeping the skel alive during the collection. This did not
occur for raw tracepoints as there is no manager in libbpf for non-USDT
probes, but for completeness also do the change there.

Fixes #475.

Fixes: 125136798f5f ("tree: update libbpf-rs/sys")
Reported-by: Paolo Valerio <pvalerio@redhat.com>
Signed-off-by: Antoine Tenart <atenart@redhat.com>

retis/src/core/probe/kernel/raw_tracepoint.rs
@@ -5,31 +5,29 @@
 //! in two parts, the Rust code (here) and the eBPF one
 //! (bpf/raw_tracepoint.bpf.c and its auto-generated part in bpf/.out/).
 
-use std::{
-    mem::MaybeUninit,
-    os::fd::{AsFd, AsRawFd, RawFd},
-};
+use std::os::fd::{AsFd, AsRawFd, RawFd};
 
 use anyhow::{anyhow, bail, Result};
-use libbpf_rs::skel::{OpenSkel, Skel, SkelBuilder};
+use libbpf_rs::skel::{OpenSkel, Skel};
 
-use crate::core::{filters::Filter, probe::builder::*, probe::*};
+use crate::core::{filters::Filter, probe::builder::*, probe::*, workaround::*};
 
 mod raw_tracepoint_bpf {
     include!("bpf/.out/raw_tracepoint.skel.rs");
 }
-use raw_tracepoint_bpf::RawTracepointSkelBuilder;
+use raw_tracepoint_bpf::*;
 
 #[derive(Default)]
-pub(crate) struct RawTracepointBuilder {
+pub(crate) struct RawTracepointBuilder<'a> {
     hooks: Vec<Hook>,
     filters: Vec<Filter>,
     links: Vec<libbpf_rs::Link>,
+    skel: Option<SkelStorage<RawTracepointSkel<'a>>>,
     map_fds: Vec<(String, RawFd)>,
 }
 
-impl ProbeBuilder for RawTracepointBuilder {
-    fn new() -> RawTracepointBuilder {
+impl<'a> ProbeBuilder for RawTracepointBuilder<'a> {
+    fn new() -> RawTracepointBuilder<'a> {
         RawTracepointBuilder::default()
     }
 
@@ -47,8 +45,7 @@ impl ProbeBuilder for RawTracepointBuilder {
     }
 
     fn attach(&mut self, probe: &Probe) -> Result<()> {
-        let mut open_object = MaybeUninit::uninit();
-        let mut skel = RawTracepointSkelBuilder::default().open(&mut open_object)?;
+        let mut skel = OpenSkelStorage::new::<RawTracepointSkelBuilder>()?;
 
         let probe = match probe.r#type() {
             ProbeType::RawTracepoint(probe) => probe,
@@ -68,7 +65,7 @@ impl ProbeBuilder for RawTracepointBuilder {
 
         reuse_map_fds(skel.open_object_mut(), &self.map_fds)?;
 
-        let skel = skel.load()?;
+        let skel = SkelStorage::load(skel)?;
         let prog = skel
             .object()
             .progs_mut()
@@ -80,6 +77,7 @@ impl ProbeBuilder for RawTracepointBuilder {
 
         self.links
             .push(prog.attach_raw_tracepoint(probe.symbol.attach_name())?);
+        self.skel = Some(skel);
         Ok(())
     }
 

retis/src/core/probe/user/usdt.rs
@@ -1,30 +1,29 @@
-use std::{
-    mem::MaybeUninit,
-    os::fd::{AsFd, AsRawFd, RawFd},
-};
+use std::os::fd::{AsFd, AsRawFd, RawFd};
 
 use anyhow::{anyhow, bail, Result};
-use libbpf_rs::skel::{OpenSkel, Skel, SkelBuilder};
+use libbpf_rs::skel::{OpenSkel, Skel};
 
 use crate::core::{
     filters::Filter,
     probe::{builder::*, Hook, Probe, ProbeType},
+    workaround::*,
 };
 
 mod usdt_bpf {
     include!("bpf/.out/usdt.skel.rs");
 }
-use usdt_bpf::UsdtSkelBuilder;
+use usdt_bpf::*;
 
 #[derive(Default)]
-pub(crate) struct UsdtBuilder {
+pub(crate) struct UsdtBuilder<'a> {
     links: Vec<libbpf_rs::Link>,
+    skel: Option<SkelStorage<UsdtSkel<'a>>>,
     map_fds: Vec<(String, RawFd)>,
     hooks: Vec<Hook>,
 }
 
-impl ProbeBuilder for UsdtBuilder {
-    fn new() -> UsdtBuilder {
+impl<'a> ProbeBuilder for UsdtBuilder<'a> {
+    fn new() -> UsdtBuilder<'a> {
         UsdtBuilder::default()
     }
 
@@ -48,13 +47,12 @@ impl ProbeBuilder for UsdtBuilder {
             _ => bail!("Wrong probe type"),
         };
 
-        let mut open_object = MaybeUninit::uninit();
-        let mut skel = UsdtSkelBuilder::default().open(&mut open_object)?;
+        let mut skel = OpenSkelStorage::new::<UsdtSkelBuilder>()?;
         skel.maps.rodata_data.log_level = log::max_level() as u8;
 
         reuse_map_fds(skel.open_object_mut(), &self.map_fds)?;
 
-        let skel = skel.load()?;
+        let skel = SkelStorage::load(skel)?;
         let prog = skel
             .object()
             .progs_mut()
@@ -65,6 +63,7 @@ impl ProbeBuilder for UsdtBuilder {
 
         self.links
             .push(prog.attach_usdt(probe.pid, &probe.path, &probe.provider, &probe.name)?);
+        self.skel = Some(skel);
 
         Ok(())
     }