retis-org/retis

commit e883af89e368821cf134f183d12d40e1a8f70f10

module: add conntrack module
Add module scheleton that extracts a simple field from
skb's `struct nf_conn`.

Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

src/module/ct/bpf/ct.bpf.c
@@ -1,0 +1,75 @@
+#include <vmlinux.h>
+#include <bpf/bpf_core_read.h>
+
+#include <common.h>
+
+/* Retis-specific flags */
+#define RETIS_CT_DIR_ORIG	1 << 0
+#define RETIS_CT_DIR_REPLY	1 << 1
+
+/* Please keep these in sync with
+* include/linux/netfilter/nf_conntrack_common.h.
+*/
+#define NFCT_INFOMASK	7UL
+#define NFCT_PTRMASK	~(NFCT_INFOMASK)
+
+/* Keep in sync with include/linux/netfilter/nf_conntrack_zones_common.h */
+#define NF_CT_ZONE_DIR_ORIG	(1 << IP_CT_DIR_ORIGINAL)
+#define NF_CT_ZONE_DIR_REPL	(1 << IP_CT_DIR_REPLY)
+#define NF_CT_DEFAULT_ZONE_DIR	(NF_CT_ZONE_DIR_ORIG | NF_CT_ZONE_DIR_REPL)
+
+/* Conntrack entry information */
+struct ct_event {
+	u32 flags;
+	u16 zone_id;
+} __attribute__((packed));
+
+static __always_inline int process_nf_conn(struct retis_raw_event *event,
+					   struct nf_conn *ct)
+{
+	struct nf_conntrack_zone *zone;
+	struct nf_conn *nf_conn;
+	struct ct_event *e;
+	u8 zone_dir;
+
+	e = get_event_section(event, COLLECTOR_CT, 0, sizeof(*e));
+	if (!e)
+		return 0;
+
+	if (bpf_core_field_exists(ct->zone)) {
+		zone_dir = (u8) BPF_CORE_READ(ct, zone.dir);
+		if (zone_dir & NF_CT_ZONE_DIR_ORIG)
+			e->flags |= RETIS_CT_DIR_ORIG;
+		if (zone_dir & NF_CT_ZONE_DIR_REPL)
+			e->flags |= RETIS_CT_DIR_REPLY;
+
+		e->zone_id = (u8) BPF_CORE_READ(ct, zone.id);
+	}
+
+	return 0;
+}
+
+DEFINE_HOOK(F_AND, RETIS_F_PACKET_PASS,
+	struct nf_conn *nf_conn;
+	struct sk_buff *skb;
+	unsigned long nfct;
+
+	skb = retis_get_sk_buff(ctx);
+	if (!skb)
+		return 0;
+
+	if (!bpf_core_field_exists(skb->_nfct))
+		return 0;
+
+	nfct = (unsigned long) BPF_CORE_READ(skb, _nfct);
+	if (!nfct)
+		return 0;
+
+	nf_conn = (struct nf_conn *)(nfct & NFCT_PTRMASK);
+	if (!nf_conn)
+		return 0;
+
+	return process_nf_conn(event, nf_conn);
+)
+
+char __license[] SEC("license") = "GPL";

build.rs
@@ -208,6 +208,7 @@ fn main() {
     build_hook("src/module/ovs/bpf/user_op_exec.bpf.c", Some(OVS_INCLUDES));
     build_hook("src/module/ovs/bpf/user_op_put.bpf.c", Some(OVS_INCLUDES));
     build_hook("src/module/nft/bpf/nft.bpf.c", None);
+    build_hook("src/module/ct/bpf/ct.bpf.c", None);
     build_extract_stub("src/core/filters/packets/bpf/stub.bpf.c");
 
     for inc in INCLUDE_PATHS.iter() {

src/core/events/bpf/include/events.h
@@ -19,6 +19,7 @@ enum retis_event_owners {
 	COLLECTOR_SKB = 7,
 	COLLECTOR_OVS = 8,
 	COLLECTOR_NFT = 9,
+	COLLECTOR_CT = 10,
 };
 
 struct retis_raw_event {

src/module/ct/bpf.rs
@@ -1,0 +1,43 @@
+//! Rust<>BPF types definitions for the ct module.
+//! Please keep this file in sync with its BPF counterpart in bpf/ct.bpf.c
+use anyhow::Result;
+use plain::Plain;
+
+use super::*;
+use crate::{
+    core::events::bpf::{parse_single_raw_section, BpfRawSection},
+    module::ModuleId,
+};
+
+/// Retis-specific flags.
+pub(super) const RETIS_CT_DIR_ORIG: u32 = 1 << 0;
+pub(super) const RETIS_CT_DIR_REPLY: u32 = 1 << 1;
+
+#[derive(Default)]
+#[repr(C, packed)]
+struct RawCtEvent {
+    flags: u32,
+    zone_id: u16,
+}
+unsafe impl Plain for RawCtEvent {}
+
+pub(super) fn unmarshal_ct(sections: Vec<BpfRawSection>) -> Result<CtEvent> {
+    let raw = parse_single_raw_section::<RawCtEvent>(ModuleId::Ct, sections)?;
+    let flags = raw.flags;
+
+    let zone_dir = match flags {
+        x if (x & (RETIS_CT_DIR_ORIG | RETIS_CT_DIR_REPLY)
+            == (RETIS_CT_DIR_ORIG | RETIS_CT_DIR_REPLY)) =>
+        {
+            ZoneDir::Default
+        }
+        x if (x & RETIS_CT_DIR_ORIG != 0) => ZoneDir::Original,
+        x if (x & RETIS_CT_DIR_REPLY != 0) => ZoneDir::Reply,
+        _ => ZoneDir::None,
+    };
+
+    Ok(CtEvent {
+        zone_id: raw.zone_id,
+        zone_dir,
+    })
+}

src/module/ct/ct.rs
@@ -1,0 +1,63 @@
+use anyhow::{bail, Result};
+
+use super::{ct_hook, CtEventFactory};
+use crate::{
+    cli::{dynamic::DynamicCommand, CliConfig},
+    collect::Collector,
+    core::{
+        events::EventSectionFactory,
+        inspect,
+        probe::{Hook, ProbeManager},
+    },
+    module::{Module, ModuleId},
+};
+
+#[derive(Default)]
+pub(crate) struct CtModule {}
+
+impl Collector for CtModule {
+    fn new() -> Result<Self> {
+        Ok(Self::default())
+    }
+
+    fn known_kernel_types(&self) -> Option<Vec<&'static str>> {
+        Some(vec!["struct sk_buff *"])
+    }
+
+    fn register_cli(&self, cmd: &mut DynamicCommand) -> Result<()> {
+        cmd.register_module_noargs(ModuleId::Ct)
+    }
+
+    fn can_run(&mut self, _cli: &CliConfig) -> Result<()> {
+        let kernel = &inspect::inspector()?.kernel;
+
+        match kernel.get_config_option("CONFIG_NF_CONNTRACK") {
+            Ok(Some("y")) => (),
+            Ok(Some("m")) => {
+                if kernel.is_module_loaded("nf_conntrack") == Some(false) {
+                    bail!("'nf_conntrack' is not loaded");
+                }
+            }
+            // If the Kernel Config is not available, the collector is not guaranteed
+            // to work, but let's try.
+            Err(_) => (),
+            _ => bail!("This kernel does not support connection tracking"),
+        }
+        Ok(())
+    }
+
+    fn init(&mut self, _cli: &CliConfig, probes: &mut ProbeManager) -> Result<()> {
+        // Register our generic conntrack hook.
+        probes.register_kernel_hook(Hook::from(ct_hook::DATA))?;
+        Ok(())
+    }
+}
+
+impl Module for CtModule {
+    fn collector(&mut self) -> &mut dyn Collector {
+        self
+    }
+    fn section_factory(&self) -> Result<Box<dyn EventSectionFactory>> {
+        Ok(Box::<CtEventFactory>::default())
+    }
+}

src/module/ct/event.rs
@@ -1,0 +1,48 @@
+use std::fmt;
+
+use anyhow::Result;
+
+use super::bpf::*;
+use crate::{
+    core::events::{bpf::BpfRawSection, *},
+    event_section, event_section_factory,
+};
+
+#[derive(Default, Debug, serde::Deserialize, serde::Serialize)]
+pub(crate) enum ZoneDir {
+    Original,
+    Reply,
+    Default,
+    #[default]
+    None,
+}
+
+/// Conntrack event
+#[event_section]
+pub(crate) struct CtEvent {
+    /// Zone ID
+    pub(crate) zone_id: u16,
+    /// Zone direction
+    pub(crate) zone_dir: ZoneDir,
+}
+impl EventFmt for CtEvent {
+    fn event_fmt(&self, f: &mut fmt::Formatter, _: DisplayFormat) -> fmt::Result {
+        match self.zone_dir {
+            ZoneDir::Original => write!(f, "orig-zone {}", self.zone_id)?,
+            ZoneDir::Reply => write!(f, "reply-zone {}", self.zone_id)?,
+            ZoneDir::Default => write!(f, "zone {}", self.zone_id)?,
+            ZoneDir::None => (),
+        }
+        Ok(())
+    }
+}
+
+#[derive(Default)]
+#[event_section_factory(CtEvent)]
+pub(crate) struct CtEventFactory {}
+
+impl RawEventSectionFactory for CtEventFactory {
+    fn from_raw(&mut self, raw_sections: Vec<BpfRawSection>) -> Result<Box<dyn EventSection>> {
+        Ok(Box::new(unmarshal_ct(raw_sections)?))
+    }
+}

src/module/ct/mod.rs
@@ -1,0 +1,12 @@
+// Re-export ct.rs
+#[allow(clippy::module_inception)]
+pub(crate) mod ct;
+pub(crate) use ct::*;
+
+pub(crate) mod event;
+pub(crate) use event::*;
+
+mod bpf;
+mod ct_hook {
+    include!("bpf/.out/ct.rs");
+}

src/module/mod.rs
@@ -8,6 +8,7 @@
 pub(crate) mod module;
 pub(crate) use module::*;
 
+pub(crate) mod ct;
 pub(crate) mod nft;
 pub(crate) mod ovs;
 pub(crate) mod skb;

commit 4ba6daa777ef832458dc770f850ae422ae691012

collector: ct: fix truncation of zone id
Zone id is 16-bit, but for some reason is casted to u8 on read.
This makes retis report incorrect values.

Fixes: e883af89e368 ("module: add conntrack module")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

retis/src/collect/collector/ct/bpf/ct.bpf.c
@@ -128,7 +128,7 @@ static __always_inline int process_nf_conn(struct ct_event *e,
 		if (zone_dir & NF_CT_ZONE_DIR_REPL)
 			e->flags |= RETIS_CT_DIR_REPLY;
 
-		e->zone_id = (u8) BPF_CORE_READ(ct, zone.id);
+		e->zone_id = (u16) BPF_CORE_READ(ct, zone.id);
 	}
 
 	if (bpf_core_field_exists(ct->mark))

tests/test_ovs.py
@@ -38,12 +38,14 @@ def test_ovs_conntrack(two_port_ovs):
     # Allow ARP
     ovs.ofctl("add-flow", "test", "table=0,arp actions=NORMAL")
     # Send untracked traffic through ct
-    ovs.ofctl("add-flow", "test", "table=0,ct_state=-trk,ip actions=ct(table=1)")
+    ovs.ofctl(
+        "add-flow", "test", "table=0,ct_state=-trk,ip actions=ct(table=1,zone=43210)"
+    )
     # Commit new connections
     ovs.ofctl(
         "add-flow",
         "test",
-        "table=1,ct_state=+trk+new,ip actions=ct(commit),NORMAL",
+        "table=1,ct_state=+trk+new,ip actions=ct(zone=43210,commit),NORMAL",
     )
     # Accept established connection
     ovs.ofctl("add-flow", "test", "table=1,ct_state=+trk+est,ip actions=NORMAL")
@@ -117,7 +119,7 @@ def test_ovs_conntrack(two_port_ovs):
         {
             "kernel": ovs_exec,
             "skb": syn,
-            "ovs": {"action": "ct", "flags": 4},
+            "ovs": {"action": "ct", "flags": 4, "zone_id": 43210},
         },
         {
             "kernel": ovs_exec,
@@ -125,7 +127,7 @@ def test_ovs_conntrack(two_port_ovs):
             "ovs": {
                 "action": "recirc",
             },
-            "ct": {"state": "new"},
+            "ct": {"state": "new", "zone_id": 43210},
         },
         {
             "kernel": ovs_exec,
@@ -133,19 +135,20 @@ def test_ovs_conntrack(two_port_ovs):
             "ovs": {
                 "action": "ct",
                 "flags": 5,
+                "zone_id": 43210,
             },
         },
         {
             "kernel": ovs_exec,
             "skb": syn,
             "ovs": {"action": "output"},
-            "ct": {"state": "new"},
+            "ct": {"state": "new", "zone_id": 43210},
         },
         # SYN+ACK actions: ct, recirc, output
         {
             "kernel": ovs_exec,
             "skb": syn_ack,
-            "ovs": {"action": "ct", "flags": 4},
+            "ovs": {"action": "ct", "flags": 4, "zone_id": 43210},
         },
         {
             "kernel": ovs_exec,
@@ -153,7 +156,7 @@ def test_ovs_conntrack(two_port_ovs):
             "ovs": {
                 "action": "recirc",
             },
-            "ct": {"state": "reply"},
+            "ct": {"state": "reply", "zone_id": 43210},
         },
         {
             "kernel": ovs_exec,
@@ -163,14 +166,18 @@ def test_ovs_conntrack(two_port_ovs):
             },
         },
         # ACK actions: ct, recirc, output
-        {"kernel": ovs_exec, "skb": ack, "ovs": {"action": "ct", "flags": 4}},
+        {
+            "kernel": ovs_exec,
+            "skb": ack,
+            "ovs": {"action": "ct", "flags": 4, "zone_id": 43210},
+        },
         {
             "kernel": ovs_exec,
             "skb": ack,
             "ovs": {
                 "action": "recirc",
             },
-            "ct": {"state": "established"},
+            "ct": {"state": "established", "zone_id": 43210},
         },
         {
             "kernel": ovs_exec,
@@ -180,7 +187,11 @@ def test_ovs_conntrack(two_port_ovs):
             },
         },
         # FIN actions: ct, recirc, output
-        {"kernel": ovs_exec, "skb": fin, "ovs": {"action": "ct", "flags": 4}},
+        {
+            "kernel": ovs_exec,
+            "skb": fin,
+            "ovs": {"action": "ct", "flags": 4, "zone_id": 43210},
+        },
         {
             "kernel": ovs_exec,
             "skb": fin,
@@ -188,7 +199,7 @@ def test_ovs_conntrack(two_port_ovs):
                 "action": "recirc",
                 "id": "&recirc_id_orig",  # Store orig recirc_id
             },
-            "ct": {"state": "established"},
+            "ct": {"state": "established", "zone_id": 43210},
         },
         {
             "kernel": ovs_exec,
@@ -197,10 +208,14 @@ def test_ovs_conntrack(two_port_ovs):
                 "action": "output",
                 "recirc_id": "*recirc_id_orig",  # Check orig recirc_id
             },
-            "ct": {"state": "established"},
+            "ct": {"state": "established", "zone_id": 43210},
         },
         # ACK actions: ct, recirc, output
-        {"kernel": ovs_exec, "skb": ack, "ovs": {"action": "ct", "flags": 4}},
+        {
+            "kernel": ovs_exec,
+            "skb": ack,
+            "ovs": {"action": "ct", "flags": 4, "zone_id": 43210},
+        },
         {
             "kernel": ovs_exec,
             "skb": ack,
@@ -208,7 +223,7 @@ def test_ovs_conntrack(two_port_ovs):
                 "action": "recirc",
                 "id": "&recirc_id_reply",  # Store reply recirc_id
             },
-            "ct": {"state": "established"},
+            "ct": {"state": "established", "zone_id": 43210},
         },
         {
             "kernel": ovs_exec,
@@ -217,7 +232,7 @@ def test_ovs_conntrack(two_port_ovs):
                 "action": "output",
                 "recirc_id": "*recirc_id_reply",  # Check reply recirc_id
             },
-            "ct": {"state": "established"},
+            "ct": {"state": "established", "zone_id": 43210},
         },
     ]
 