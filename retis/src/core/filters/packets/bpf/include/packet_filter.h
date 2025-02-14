36ccf497d47d30f1b3d2ecee07774265c2bd3064
Paolo Valerio <pvalerio@redhat.com>
2025-02-14 14:32:18 +0100

    filter/packet: remove nop frame.
    Filtering is no longer inlined in the main program, but it is now part
    of a subprog. This has multiple benefits:
    
    - stack separation
    - reduced risk of unintentional register clobbering
    - potentially reduced load time
    
    Given the removal of the nop trick, pseudo calls are now fixed up
    while scanning the program, also func_info and line_info offsets are
    adjusted accordingly.
    All the instructions need to be checked as relative jumps may point to
    negative offset and so preceding subprogs, this is a bit tedious but
    doesn't add much overhead compared to the previous way as the only
    additional instructions scanned would be the hooks stubs.
    
    This whole change also paves the way for dynamically generate
    meta-filtering.
    
    Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

--- retis/src/core/filters/packets/bpf/include/packet_filter.h
+++ retis/src/core/filters/packets/bpf/include/packet_filter.h
@@ -20,12 +20,6 @@ BINDING_DEF(FILTER_MAX_INSNS, __FILTER_MAX_INSNS)
 #define __s(v) #v
 #define s(v) __s(v)
 
-/* Reserve FILTER_MAX_INSNS - (instruction placeholder) */
-#define RESERVE_NOP				\
-	".rept " s(__FILTER_MAX_INSNS) " - 1;"	\
-	"goto +0x0;"				\
-	".endr;"
-
 BINDING_DEF(STACK_RESERVED, 8)
 BINDING_DEF(SCRATCH_MEM_SIZE, 4)
 
@@ -34,49 +28,11 @@ BINDING_DEF(SCRATCH_MEM_SIZE, 4)
  */
 BINDING_DEF(SCRATCH_MEM_START, 16 * SCRATCH_MEM_SIZE + STACK_RESERVED)
 
-#define STACK_SIZE		SCRATCH_MEM_START
-
+#define l2 0xdeadbeef
+#define l3 0xdeadc0de
 enum filter_type {
-	FILTER_L2 = 0xdeadbeef,
-	FILTER_L3 = 0xdeadc0de,
+	L2 = l2,
+	L3 = l3,
 } __binding;
-
-/* The function below defines a placeholder instruction and a
- * nop frame that will be replaced on load with the actual filtering
- * instructions.
- * Normally, if no filter gets set, a simple mov r0, 0x40000 will replace
- * the call. 0x40000 is used as it is also used by generated cBPF filters,
- * whereas 0 means no match, instead. The exceeding nops will get removed
- * from the kernel during the load. If no explicit, nor default filter gets
- * set, call 0xdeadbeef for the l2 variant or 0xdeadc0de for the l3 will
- * fail to load and the verifier will report an error.
- */
-static __always_inline
-unsigned int packet_filter(struct retis_packet_filter_ctx *ctx, u32 placeholder)
-{
-	register struct retis_packet_filter_ctx *ctx_reg asm("r1");
-	u8 stack[STACK_SIZE] __attribute__ ((aligned (8)));
-	register u64 *fp asm("r9");
-
-	if (!ctx)
-		return 0;
-
-	ctx_reg = ctx;
-	fp = (u64 *)((void *)stack + sizeof(stack));
-
-	asm volatile (
-		"call %[filter];"
-		RESERVE_NOP
-		"*(u32 *)%[ret] = r0;"
-		: [ret] "=m" (ctx->ret)
-		: [filter] "i" (placeholder),
-		  "r" (ctx_reg),
-		  "r" (fp)
-		: "r0", "r1", "r2", "r3",
-		  "r4", "r5", "r6", "r7",
-		  "r8", "r9");
-
-	return ctx->ret;
-}
 
 #endif
