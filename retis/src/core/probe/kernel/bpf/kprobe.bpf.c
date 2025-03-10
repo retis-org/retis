266db8d914c00453da283abb88b61e297b0611bd
Antoine Tenart <atenart@redhat.com>
2025-03-10 19:33:05 +0100

    core: probe: kernel: merge kprobe and kretprobe builders
    Most of the logic is shared, merge the two to improve maintenance.
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- retis/src/core/probe/kernel/bpf/kprobe.bpf.c
+++ retis/src/core/probe/kernel/bpf/kprobe.bpf.c
@@ -4,7 +4,19 @@
 
 #include <common.h>
 
-static __always_inline void get_regs(struct retis_regs *regs, struct pt_regs *ctx)
+/* Are we operating a kprobe or a kprobe+kretprobe pair */
+const volatile bool kretprobe = false;
+
+#define MAX_INFLIGHT_PROBES 20
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_INFLIGHT_PROBES);
+	__type(key, u64);
+	__type(value, struct retis_context);
+} kretprobe_context SEC(".maps");
+
+static __always_inline void kprobe_get_regs(struct retis_regs *regs,
+					    struct pt_regs *ctx)
 {
 	regs->reg[0] = PT_REGS_PARM1(ctx);
 	regs->reg[1] = PT_REGS_PARM2(ctx);
@@ -23,7 +35,57 @@ int probe_kprobe(struct pt_regs *ctx)
 	context.ksym = kprobe_get_func_ip(ctx);
 	context.probe_type = KERNEL_PROBE_KPROBE;
 	context.orig_ctx = ctx;
-	get_regs(&context.regs, ctx);
+	kprobe_get_regs(&context.regs, ctx);
+
+	if (kretprobe) {
+		u64 tgid = bpf_get_current_pid_tgid();
+
+		/* Store the current context and let the kretprobe run the hooks. */
+		if (bpf_map_update_elem(&kretprobe_context, &tgid, &context,
+					BPF_NOEXIST))
+			return -1;
+
+		return 0;
+	}
+
+	return chain(&context);
+}
+
+/* Merge the registers with the ones coming from the matching kprobe */
+static __always_inline void kretprobe_get_regs(struct retis_regs *regs,
+					       struct retis_regs *kprobe_regs,
+					       struct pt_regs *ctx)
+{
+	regs->reg[0] = kprobe_regs->reg[0];
+	regs->reg[1] = kprobe_regs->reg[1];
+	regs->reg[2] = kprobe_regs->reg[2];
+	regs->reg[3] = kprobe_regs->reg[3];
+	regs->reg[4] = kprobe_regs->reg[4];
+	regs->num = kprobe_regs->num;
+
+	regs->ret = PT_REGS_RC(ctx);
+}
+
+SEC("kprobe/retprobe")
+int probe_kretprobe(struct pt_regs *ctx)
+{
+	struct retis_context context = {};
+	struct retis_context *kprobe_ctx;
+	u64 tid = bpf_get_current_pid_tgid();
+
+	/* Look if the matching kprobe has left a context for us to pick up. */
+	kprobe_ctx = bpf_map_lookup_elem(&kretprobe_context, &tid);
+	if (!kprobe_ctx)
+		return 0;
+
+	bpf_map_delete_elem(&kretprobe_context, &tid);
+
+	context.timestamp = bpf_ktime_get_ns();
+	context.ksym = kprobe_ctx->ksym;
+	context.probe_type = KERNEL_PROBE_KRETPROBE;
+	context.orig_ctx = ctx;
+
+	kretprobe_get_regs(&context.regs, &kprobe_ctx->regs, ctx);
 
 	return chain(&context);
 }
