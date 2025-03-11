9ce8ddfc895374bd35b55ac03e89cc62f7c81343
Antoine Tenart <atenart@redhat.com>
2025-03-11 23:19:37 +0100

    events: make EventSectionFactory Send + Sync
    This will ensure we can use the factories in different threads.
    
    We kept `as_any_mut` as this is only used during initialization where factories
    are not yet shared in threads. We could move to using internal mutability and
    locks, at the expense of taking a read lock in the fast path later.
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- retis/src/core/events/bpf.rs
+++ retis/src/core/events/bpf.rs
@@ -546,11 +546,13 @@ unsafe impl Plain for BpfRawSectionHeader {}
 ///
 /// Please use `#[retis_derive::event_section_factory]` to implement the common
 /// traits.
-pub(crate) trait EventSectionFactory: RawEventSectionFactory {
+pub(crate) trait EventSectionFactory: RawEventSectionFactory + Send + Sync {
     #[allow(dead_code)]
     fn id() -> u8
     where
         Self: Sized;
+    /// This should only be used during initialization, before factories are
+    /// shared between threads!
     fn as_any_mut(&mut self) -> &mut dyn any::Any;
 }
 
