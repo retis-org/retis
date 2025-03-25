retis-org/retis

commit 1bfe31cc9dcb4b16404000d6447a71c7e5379d8a

tree: fix new clippy warnings in bitwise ops
Recent clippy genereates new warnings on bitwise operations.
Specifically, it suggests adding some parenthesys to make operation
precedence clearer and remove "<< 0" operations.

Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

retis-events/src/skb.rs
@@ -148,13 +148,13 @@ impl EventFmt for SkbEvent {
 
                     let mut flags = Vec::new();
                     // Same order as tcpdump.
-                    if v4.flags & 1 << 2 != 0 {
+                    if (v4.flags & 1) << 2 != 0 {
                         flags.push("+");
                     }
-                    if v4.flags & 1 << 1 != 0 {
+                    if (v4.flags & 1) << 1 != 0 {
                         flags.push("DF");
                     }
-                    if v4.flags & 1 << 0 != 0 {
+                    if (v4.flags & 1) != 0 {
                         flags.push("rsvd");
                     }
 
@@ -186,22 +186,22 @@ impl EventFmt for SkbEvent {
             space.write(f)?;
 
             let mut flags = Vec::new();
-            if tcp.flags & 1 << 0 != 0 {
+            if (tcp.flags & 1) != 0 {
                 flags.push('F');
             }
-            if tcp.flags & 1 << 1 != 0 {
+            if (tcp.flags & 1) << 1 != 0 {
                 flags.push('S');
             }
-            if tcp.flags & 1 << 2 != 0 {
+            if (tcp.flags & 1) << 2 != 0 {
                 flags.push('R');
             }
-            if tcp.flags & 1 << 3 != 0 {
+            if (tcp.flags & 1) << 3 != 0 {
                 flags.push('P');
             }
-            if tcp.flags & 1 << 4 != 0 {
+            if (tcp.flags & 1) << 4 != 0 {
                 flags.push('.');
             }
-            if tcp.flags & 1 << 5 != 0 {
+            if (tcp.flags & 1) << 5 != 0 {
                 flags.push('U');
             }
             write!(f, "flags [{}]", flags.into_iter().collect::<String>())?;
@@ -213,7 +213,7 @@ impl EventFmt for SkbEvent {
                 write!(f, " seq {}", tcp.seq)?;
             }
 
-            if tcp.flags & 1 << 4 != 0 {
+            if (tcp.flags & 1) << 4 != 0 {
                 write!(f, " ack {}", tcp.ack_seq)?;
             }
 

retis-events/src/helpers.rs
@@ -71,7 +71,7 @@ impl U128 {
     }
 
     pub fn bits(&self) -> u128 {
-        (self.hi as u128) << 64 | self.lo as u128
+        ((self.hi as u128) << 64) | self.lo as u128
     }
 }
 

retis-events/src/skb_tracking.rs
@@ -32,7 +32,7 @@ pub struct SkbTrackingEvent {
 impl SkbTrackingEvent {
     /// Get the tracking id.
     pub fn tracking_id(&self) -> u128 {
-        (self.timestamp as u128) << 64 | self.orig_head as u128
+        ((self.timestamp as u128) << 64) | self.orig_head as u128
     }
 
     /// Check if two tracking event sections are from related skbs, including

retis/src/core/filters/packets/ebpfinsn.rs
@@ -303,7 +303,7 @@ impl eBpfInsn {
     }
 
     pub(crate) fn to_vec(self) -> Vec<u8> {
-        let mut byte_insn = vec![self.code, self.src << 4 | self.dst];
+        let mut byte_insn = vec![self.code, (self.src << 4) | self.dst];
 
         byte_insn.extend(self.off.to_le_bytes());
         byte_insn.extend(self.imm.to_le_bytes());

retis/src/process/cli/pcap.rs
@@ -117,7 +117,7 @@ impl<'a, W: Write> EventParser<'a, W> {
         };
 
         // If we see this iface for the first time, add a description block.
-        let key: u64 = (netns as u64) << 32 | ifindex as u64;
+        let key: u64 = ((netns as u64) << 32) | ifindex as u64;
         let id = match self.ifaces.contains_key(&key) {
             // Unwrap if contains is true.
             true => *self.ifaces.get(&key).unwrap(),

commit 4dffb3941e1da1fcce8d31915fddfc316c0e9dc3

events: fix flags output
While fixing Clippy warnings we introduced an issue with parsing flags
as the parenthesis where not added at the right place. Fix this.

Fixes: 1bfe31cc9dcb ("tree: fix new clippy warnings in bitwise ops")
Signed-off-by: Antoine Tenart <atenart@redhat.com>

retis-events/src/skb.rs
@@ -148,13 +148,13 @@ impl EventFmt for SkbEvent {
 
                     let mut flags = Vec::new();
                     // Same order as tcpdump.
-                    if (v4.flags & 1) << 2 != 0 {
+                    if v4.flags & (1 << 2) != 0 {
                         flags.push("+");
                     }
-                    if (v4.flags & 1) << 1 != 0 {
+                    if v4.flags & (1 << 1) != 0 {
                         flags.push("DF");
                     }
-                    if (v4.flags & 1) != 0 {
+                    if v4.flags & 1 != 0 {
                         flags.push("rsvd");
                     }
 
@@ -186,22 +186,22 @@ impl EventFmt for SkbEvent {
             space.write(f)?;
 
             let mut flags = Vec::new();
-            if (tcp.flags & 1) != 0 {
+            if tcp.flags & 1 != 0 {
                 flags.push('F');
             }
-            if (tcp.flags & 1) << 1 != 0 {
+            if tcp.flags & (1 << 1) != 0 {
                 flags.push('S');
             }
-            if (tcp.flags & 1) << 2 != 0 {
+            if tcp.flags & (1 << 2) != 0 {
                 flags.push('R');
             }
-            if (tcp.flags & 1) << 3 != 0 {
+            if tcp.flags & (1 << 3) != 0 {
                 flags.push('P');
             }
-            if (tcp.flags & 1) << 4 != 0 {
+            if tcp.flags & (1 << 4) != 0 {
                 flags.push('.');
             }
-            if (tcp.flags & 1) << 5 != 0 {
+            if tcp.flags & (1 << 5) != 0 {
                 flags.push('U');
             }
             write!(f, "flags [{}]", flags.into_iter().collect::<String>())?;
@@ -213,7 +213,7 @@ impl EventFmt for SkbEvent {
                 write!(f, " seq {}", tcp.seq)?;
             }
 
-            if (tcp.flags & 1) << 4 != 0 {
+            if tcp.flags & (1 << 4) != 0 {
                 write!(f, " ack {}", tcp.ack_seq)?;
             }
 