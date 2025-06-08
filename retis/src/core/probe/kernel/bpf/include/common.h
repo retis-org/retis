ebf1c46d59b1aa447a1a2a5a10b256b07aebaeb9
Paolo Valerio <pvalerio@redhat.com>
2025-06-08 22:26:44 +0200

    ebpf: use __noinline instead of __attribute__()
    Prefer the macro over the bare attribute.
    
    Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

--- retis/src/core/probe/kernel/bpf/include/common.h
+++ retis/src/core/probe/kernel/bpf/include/common.h
@@ -132,7 +132,7 @@ const volatile u32 nhooks = 0;
  * it. Credits to the XDP dispatcher.
  */
 #define HOOK(x)									\
-	__attribute__ ((noinline))						\
+	__noinline								\
 	int hook##x(struct retis_context *ctx, struct retis_raw_event *event) {	\
 		volatile int ret = 0;						\
 		if (!ctx || !event)						\
@@ -152,7 +152,7 @@ HOOK(9)
 /* Keep in sync with its Rust counterpart in crate::core::probe::kernel */
 #define HOOK_MAX 10
 
-__attribute__ ((noinline))
+__noinline
 int ctx_hook(struct retis_context *ctx)
 {
 	volatile int ret = 0;
