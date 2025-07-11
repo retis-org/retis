retis-org/retis

commit 5cf33263abec9198b7b39a4ea61267a98cb80041

btf-filter: fix load issue on c8s
Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

retis/src/core/filters/meta/filter.rs
@@ -750,7 +750,9 @@ impl<'a> MetaExpr<'a> {
             ));
 
             idx_sz += 1;
-            if idx_sz >= ctx.target.sz {
+            // idx_sz >= ctx.target.sz is just enough, but this is
+            // needed to make old verifier happy.
+            if idx_sz >= std::mem::size_of_val(&ctx.target.md) {
                 break;
             }
         }
@@ -818,6 +820,7 @@ impl<'a> MetaExpr<'a> {
         ]);
 
         tf_list.push_false(self.filter.len());
+
         self.filter.add_multi(&[
             eBpfInsn::jmp_a(0),
             eBpfInsn::jmp(
@@ -845,9 +848,6 @@ impl<'a> MetaExpr<'a> {
                     imm: -1,
                 },
             ),
-        ]);
-
-        self.filter.add_multi(&[
             eBpfInsn::jmp(
                 eBpfJmpOpExt::Bpf(BpfJmpOp::Gt),
                 JmpInfo::Imm {

commit 5681944f40bb3246d96c57846bc39ce1d0e1bbeb

filter/btf: fix array issue due to wrong jump offset
An apparently innocuous block coalescing added last minute in the
cited commit resulted in a weird but pretty obvious bug in string
checks because one of the blocks set the jump offset based on the
current instrunction length.
Split the instructions back and add a comment to avoid any potential
future overlook.

Spotted because of the failure of:

core::filters::meta::filter::tests::meta_filter_runtime::negative_two_fields_false_or_false

Fixes: 5cf33263abec ("btf-filter: fix load issue on c8s")
Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

retis/src/core/filters/meta/filter.rs
@@ -848,6 +848,12 @@ impl<'a> MetaExpr<'a> {
                     imm: -1,
                 },
             ),
+        ]);
+
+        // These two blocks cannot be coalesced because the following
+        // has a dependency on the filter length based on the previous
+        // instructions.
+        self.filter.add_multi(&[
             eBpfInsn::jmp(
                 eBpfJmpOpExt::Bpf(BpfJmpOp::Gt),
                 JmpInfo::Imm {