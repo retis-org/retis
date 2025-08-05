retis-org/retis

commit 1e0840b3d29fa62237fc68d53f0027d444d0814e

cli: move kconf option to subcommands
Early initializing of the inspector based on kconf option is only used
in "collect" and "inspect". Move the option there (no need to keep it
global), and remove the ugly hack in main.rs.

In order to keep `retis_in_container.sh` working, remove the use of
RETIS_KCONF and instead map all well known config paths and suggest
users to use the `--kconf` option as failback.

Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

retis/src/collect/cli.rs
@@ -8,7 +8,7 @@ use anyhow::Result;
 use clap::{builder::PossibleValuesParser, Parser};
 
 use super::Collectors;
-use crate::{cli::*, collect::collector::*};
+use crate::{cli::*, collect::collector::*, core::inspect::init_inspector};
 
 /// Collect events.
 ///
@@ -154,6 +154,12 @@ fully operational:
 "#
     )]
     pub(crate) allow_system_changes: bool,
+    pub(crate) profile: Vec<String>,
+    #[arg(
+        long,
+        help = "Path to kernel configuration (e.g. /boot/config-6.3.8-200.fc38.x86_64; default: auto-detect)"
+    )]
+    pub(crate) kconf: Option<PathBuf>,
     #[arg(long, help = "Print the time as UTC")]
     pub(super) utc: bool,
     #[arg(long, help = "Format used when printing an event.")]
@@ -179,6 +185,9 @@ pub(crate) struct CollectorsArgs {
 
 impl SubCommandParserRunner for Collect {
     fn run(&mut self, main_config: &MainConfig) -> Result<()> {
+        if let Some(kconf) = &self.kconf {
+            init_inspector(kconf)?;
+        }
         let mut collectors = Collectors::new()?;
 
         collectors.check(self)?;

retis/src/cli/cli.rs
@@ -3,9 +3,7 @@
 //! Cli module, providing tools for registering and accessing command line interface arguments
 //! as well as defining the subcommands that the tool supports.
 #![allow(dead_code)] // FIXME
-use std::{
-    any::Any, convert::From, env, ffi::OsString, fmt::Debug, path::PathBuf, str::FromStr, sync::Arc,
-};
+use std::{any::Any, convert::From, env, ffi::OsString, fmt::Debug, str::FromStr, sync::Arc};
 
 use anyhow::{anyhow, bail, Result};
 use clap::{
@@ -172,11 +170,6 @@ pub(crate) struct MainConfig {
         help = "Comma separated list of profile names to apply"
     )]
     pub(crate) profile: Vec<String>,
-    #[arg(
-        long,
-        help = "Path to kernel configuration (e.g. /boot/config-6.3.8-200.fc38.x86_64; default: auto-detect)"
-    )]
-    pub(crate) kconf: Option<PathBuf>,
 }
 
 #[derive(Debug, Default)]

retis/src/inspect/inspect.rs
@@ -1,10 +1,12 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
 use clap::{arg, Parser};
 
 use crate::{
     cli::*,
     collect::collector::get_known_types,
-    core::{kernel::Symbol, probe::kernel::utils::probe_from_cli},
+    core::{inspect::init_inspector, kernel::Symbol, probe::kernel::utils::probe_from_cli},
 };
 
 /// Inspect the current machine.
@@ -23,10 +25,18 @@ a compatibility check is performed for each one.
 Eg. '-p tp:*'. See `retis collect --help` for more details on the probe format."
     )]
     pub(crate) probe: Option<String>,
+    #[arg(
+        long,
+        help = "Path to kernel configuration (e.g. /boot/config-6.3.8-200.fc38.x86_64; default: auto-detect)"
+    )]
+    pub(crate) kconf: Option<PathBuf>,
 }
 
 impl SubCommandParserRunner for Inspect {
     fn run(&mut self, _: &MainConfig) -> Result<()> {
+        if let Some(kconf) = &self.kconf {
+            init_inspector(kconf)?;
+        }
         if let Some(probe) = &self.probe {
             let known_types = get_known_types()?;
 

retis/src/main.rs
@@ -13,7 +13,7 @@ mod profiles;
 #[cfg(feature = "benchmark")]
 mod benchmark;
 
-use crate::{cli::RetisCli, core::inspect::init_inspector, helpers::pager::try_enable_pager};
+use crate::{cli::RetisCli, helpers::pager::try_enable_pager};
 
 // Re-export events crate. It's not really an import but a re-export so events appear as module
 // inside the crate rather than an external crate. However, clippy doesn't like it.
@@ -25,20 +25,8 @@ use retis_derive::*;
 fn main() -> Result<()> {
     let cli = RetisCli::new()?.parse();
 
-    // Save the --kconf option value before using the cli object to dispatch the
-    // command.
-    let kconf_opt = cli.main_config.kconf.clone();
-
     // Per-command early fixups.
     match cli.subcommand.name().as_str() {
-        // If the user provided a custom kernel config location, use it early to
-        // initialize the inspector. As the inspector is only used by the
-        // collect command, only initialize it there for now.
-        "collect" => {
-            if let Some(kconf) = &kconf_opt {
-                init_inspector(kconf)?;
-            }
-        }
         // Try setting up the pager for a selected subset of commands.
         "print" | "sort" => {
             try_enable_pager(&cli.logger.clone());

tools/retis_in_container.sh
@@ -28,19 +28,18 @@ fi
 # EOL char is added (see commit 9f3361ac39c3).
 [[ ! $@ =~ "pcap" ]] && term_opts="-it"
 
-# Look for a kernel configuration file.
-if [ ! -z $RETIS_KCONF ]; then
-	kconfig=$RETIS_KCONF
-elif [ -f /proc/config.gz ]; then
-	kconfig=/proc/config.gz
-elif [ -f /boot/config-$(uname -r) ]; then
-	kconfig=/boot/config-$(uname -r)
-elif [ -f /lib/modules/$(uname -r)/config ]; then
-	kconfig=/lib/modules/$(uname -r)/config
-else
-	echo "Could not auto-detect kernel configuration location:"
-	echo "You can set the RETIS_KCONF environment variable to manually set it."
-	exit -1
+# Map well-known kernel configuration files.
+kconfig_map=""
+for kconfig in /proc/config.gz \
+               /boot/config-$(uname -r) \
+               /lib/modules/$(uname -r)/config; do
+    if [ -f $kconfig ]; then
+        kconfig_map="$kconfig_map -v ${kconfig}:${kconfig}:ro"
+    fi
+done
+if [ $kconfig_map == "" ]; then
+	echo "WARN: Could not auto-detect kernel configuration location. "
+	echo "You can place your configuration file in the current directory and use the '--kconf' option"
 fi
 
 # Map local config if exist.
@@ -58,8 +57,8 @@ exec $runtime run $extra_args $term_opts --privileged --rm --pid=host \
       --cap-add SYS_ADMIN --cap-add BPF --cap-add SYSLOG \
       -v /sys/kernel/btf:/sys/kernel/btf:ro \
       -v /sys/kernel/debug:/sys/kernel/debug:ro \
-      -v $kconfig:/kconfig:ro \
       -v $(pwd):/data:rw \
+      $kconfig_map \
       $local_conf \
       $ovs_binary_mount \
       $RETIS_IMAGE:$RETIS_TAG "$@"

commit e4e0c1eb61e5a6702a97bb4899f8566cebc79dc0

cli/collect: do not accept invalid parameters
Since the cited commit an unconfigured String vector profile slipped
in, causing clap to accept multiple arbitrary values that normally
should be rejected. This allowed invalid invocations like:

    retis collect foobar foobaz

While this had no functional side effects, it prevented proper
validation and error reporting for incorrect command usage.

Fix it by removing the field.

Fixes: 1e0840b3d29f ("cli: move kconf option to subcommands")
Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

retis/src/collect/cli.rs
@@ -154,7 +154,6 @@ fully operational:
 "#
     )]
     pub(crate) allow_system_changes: bool,
-    pub(crate) profile: Vec<String>,
     #[arg(
         long,
         help = "Path to kernel configuration (e.g. /boot/config-6.3.8-200.fc38.x86_64; default: auto-detect)"