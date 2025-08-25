retis-org/retis

commit d55a7f759dd1b5334527a9cc44fe81ca9a9095d8

core: probe: kernel: set the symbol address in the BPF cookie
The kernel symbol address is used as the primary probe configuration
key. It could be retrieved dynamically in k(ret)probes but not in
raw_tracepoints and was there set as rodata. In an effort to move away
from probe-specific rodata, set the symbol address as the BPF cookie
when attaching all probes and use it to get the symbol address.

When setting cookies is not supported, fallback to the old behavior.
This is mainly for c8s.

Signed-off-by: Antoine Tenart <atenart@redhat.com>

retis/src/core/probe/kernel/raw_tracepoint.rs
@@ -8,9 +8,12 @@
 use std::os::fd::{AsFd, AsRawFd, RawFd};
 
 use anyhow::{anyhow, bail, Result};
-use libbpf_rs::skel::{OpenSkel, Skel};
+use libbpf_rs::{
+    skel::{OpenSkel, Skel},
+    RawTracepointOpts,
+};
 
-use crate::core::{probe::builder::*, probe::*, workaround::*};
+use crate::core::{inspect, probe::builder::*, probe::*, workaround::*};
 
 mod raw_tracepoint_bpf {
     include!("bpf/.out/raw_tracepoint.skel.rs");
@@ -20,6 +23,7 @@ use raw_tracepoint_bpf::*;
 #[derive(Default)]
 pub(crate) struct RawTracepointBuilder<'a> {
     skels: Vec<SkelStorage<RawTracepointSkel<'a>>>,
+    cookie_support: bool,
     probes: Vec<Probe>,
     hooks: Vec<Hook>,
     ctx_hook: Option<Hook>,
@@ -29,7 +33,16 @@ pub(crate) struct RawTracepointBuilder<'a> {
 
 impl<'a> ProbeBuilder for RawTracepointBuilder<'a> {
     fn new() -> Result<RawTracepointBuilder<'a>> {
-        Ok(RawTracepointBuilder::default())
+        let cookie_support = Self::cookie_support()?;
+        log::debug!(
+            "Raw tracepoint builder {} cookie support",
+            if cookie_support { "with" } else { "without" }
+        );
+
+        Ok(Self {
+            cookie_support,
+            ..Default::default()
+        })
     }
 
     fn init(
@@ -62,6 +75,16 @@ impl<'a> ProbeBuilder for RawTracepointBuilder<'a> {
 }
 
 impl RawTracepointBuilder<'_> {
+    // Checks whether the underlying kernel supports setting/retrieving cookies
+    // in raw tracepoints.
+    fn cookie_support() -> Result<bool> {
+        Ok(inspect::inspector()?
+            .kernel
+            .btf
+            .resolve_types_by_name("bpf_get_attach_cookie_tracing")
+            .is_ok())
+    }
+
     fn attach_raw_tracepoint(&mut self, probe: &Probe) -> Result<()> {
         let mut skel = OpenSkelStorage::new::<RawTracepointSkelBuilder>()?;
 
@@ -75,10 +98,13 @@ impl RawTracepointBuilder<'_> {
             .rodata_data
             .as_deref_mut()
             .ok_or_else(|| anyhow!("Can't access eBPF rodata: not memory mapped"))?;
-        rodata.ksym = probe.symbol.addr()?;
         rodata.nargs = probe.symbol.nargs()?;
         rodata.nhooks = self.hooks.len() as u32;
         rodata.log_level = log::max_level() as u8;
+
+        if !self.cookie_support {
+            rodata.ksym = probe.symbol.addr()?;
+        }
 
         reuse_map_fds(skel.open_object_mut(), &self.map_fds)?;
 
@@ -97,8 +123,19 @@ impl RawTracepointBuilder<'_> {
             self.links.push(replace_ctx_hook(fd, ctx_hook)?);
         }
 
-        self.links
-            .push(prog.attach_raw_tracepoint(probe.symbol.attach_name())?);
+        if self.cookie_support {
+            let opts = RawTracepointOpts {
+                cookie: probe.symbol.addr()?,
+                ..Default::default()
+            };
+
+            self.links
+                .push(prog.attach_raw_tracepoint_with_opts(probe.symbol.attach_name(), opts)?);
+        } else {
+            self.links
+                .push(prog.attach_raw_tracepoint(probe.symbol.attach_name())?);
+        }
+
         self.skels.push(skel);
         Ok(())
     }

retis/src/core/inspect/inspect.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use anyhow::{bail, Result};
+use btf_rs::Type;
 use once_cell::sync::OnceCell;
 
 use super::kernel::KernelInspector;
@@ -35,6 +36,23 @@ impl Inspector {
             kernel: KernelInspector::from(kconf)?,
         })
     }
+}
+
+/// Parses a struct and returns its field names.
+pub(crate) fn parse_struct(r#struct: &str) -> Result<Vec<String>> {
+    let mut fields = Vec::new();
+
+    if let Ok(types) = inspector()?.kernel.btf.resolve_types_by_name(r#struct) {
+        if let Some((btf, Type::Struct(r#enum))) =
+            types.iter().find(|(_, t)| matches!(t, Type::Struct(_)))
+        {
+            for member in r#enum.members.iter() {
+                fields.push(btf.resolve_name(member)?);
+            }
+        }
+    }
+
+    Ok(fields)
 }
 
 #[cfg(test)]

retis/src/core/probe/kernel/bpf/kprobe.bpf.c
@@ -32,10 +32,17 @@ int probe_kprobe(struct pt_regs *ctx)
 	struct retis_context context = {};
 
 	context.timestamp = bpf_ktime_get_ns();
-	context.ksym = kprobe_get_func_ip(ctx);
 	context.probe_type = KERNEL_PROBE_KPROBE;
 	context.orig_ctx = ctx;
 	kprobe_get_regs(&context.regs, ctx);
+
+	/* Check if cookies can be set and retrieved from kprobes, otherwise
+	 * fallback to getting the symbol address using our own helper.
+	 */
+	if (bpf_core_field_exists(((struct bpf_trace_run_ctx *) 0)->bpf_cookie))
+		context.ksym = bpf_get_attach_cookie(ctx);
+	else
+		context.ksym = kprobe_get_func_ip(ctx);
 
 	if (kretprobe) {
 		u64 tgid = bpf_get_current_pid_tgid();

retis/src/core/probe/kernel/bpf/raw_tracepoint.bpf.c
@@ -6,8 +6,10 @@
 /* It is safe to have these values per-object as the loaded object won't be
  * shared between attached programs for raw tracepoints.
  */
-const volatile u64 ksym = 0;
 const volatile u32 nargs = 0;
+
+/* Only used in legacy mode where the raw tracepoint program can't be reused. */
+const volatile u64 ksym = 0;
 
 /* We unroll the loop bellow as the verifier disallow arithmetic operations on
  * context pointer. The loop unrolling pragma doesn't work here, do it manually,
@@ -47,10 +49,17 @@ int probe_raw_tracepoint(struct bpf_raw_tracepoint_args *ctx)
 	struct retis_context context = {};
 
 	context.timestamp = bpf_ktime_get_ns();
-	context.ksym = ksym;
 	context.probe_type = KERNEL_PROBE_TRACEPOINT;
 	context.orig_ctx = ctx;
 	get_regs(&context.regs, ctx);
+
+	/* Check if cookies can be set and retrieved from raw tracepoints,
+	 * otherwise fallback to a per-program kernel symbol being provided.
+	 */
+	if (bpf_core_field_exists(((struct bpf_raw_tp_link *) 0)->cookie))
+		context.ksym = bpf_get_attach_cookie(ctx);
+	else
+		context.ksym = ksym;
 
 	return chain(&context);
 }

retis/src/core/probe/kernel/kprobe.rs
@@ -7,9 +7,12 @@
 use std::os::fd::{AsFd, AsRawFd, RawFd};
 
 use anyhow::{anyhow, bail, Result};
-use libbpf_rs::skel::{OpenSkel, Skel};
+use libbpf_rs::{
+    skel::{OpenSkel, Skel},
+    KprobeOpts,
+};
 
-use crate::core::{probe::builder::*, probe::*, workaround::*};
+use crate::core::{inspect, probe::builder::*, probe::*, workaround::*};
 
 mod kprobe_bpf {
     include!("bpf/.out/kprobe.skel.rs");
@@ -19,6 +22,8 @@ use kprobe_bpf::*;
 #[derive(Default)]
 pub(crate) struct KprobeBuilder<'a> {
     skel: Option<SkelStorage<KprobeSkel<'a>>>,
+    // Use the compatibility mode, aka. older APIs, to support older kernels.
+    compat: bool,
     kretprobe: bool,
     probes: Vec<Probe>,
     links: Vec<libbpf_rs::Link>,
@@ -26,7 +31,15 @@ pub(crate) struct KprobeBuilder<'a> {
 
 impl<'a> ProbeBuilder for KprobeBuilder<'a> {
     fn new() -> Result<KprobeBuilder<'a>> {
-        Ok(Default::default())
+        let compat = Self::check_compat()?;
+        if compat {
+            log::debug!("Kprobe builder using compat mode");
+        }
+
+        Ok(Self {
+            compat,
+            ..Default::default()
+        })
     }
 
     fn init(
@@ -85,7 +98,7 @@ impl<'a> ProbeBuilder for KprobeBuilder<'a> {
     }
 
     fn attach(&mut self) -> Result<()> {
-        self.attach_kprobes()
+        self.attach_kprobes(self.compat)
     }
 
     fn detach(&mut self) -> Result<()> {
@@ -100,7 +113,15 @@ impl KprobeBuilder<'_> {
         self
     }
 
-    fn attach_kprobes(&mut self) -> Result<()> {
+    /// Inspect the kprobe support in the running kernel, returning true if the
+    /// compat' mode should be used.
+    fn check_compat() -> Result<bool> {
+        Ok(!inspect::parse_struct("bpf_trace_run_ctx")?
+            .iter()
+            .any(|field| field == "bpf_cookie"))
+    }
+
+    fn attach_kprobes(&mut self, compat: bool) -> Result<()> {
         let obj = match &mut self.skel {
             Some(skel) => skel.object(),
             _ => bail!("Kprobe builder is uninitialized"),
@@ -126,10 +147,22 @@ impl KprobeBuilder<'_> {
                 _ => bail!("Wrong probe type {}", probe),
             };
 
-            self.links
-                .push(prog.attach_kprobe(false, symbol.attach_name())?);
+            if !compat {
+                let opts = KprobeOpts {
+                    cookie: symbol.addr()?,
+                    ..Default::default()
+                };
+
+                self.links
+                    .push(prog.attach_kprobe_with_opts(false, symbol.attach_name(), opts)?);
+            } else {
+                self.links
+                    .push(prog.attach_kprobe(false, symbol.attach_name())?);
+            }
 
             if let Some(ref prog_ret) = prog_ret {
+                // No need to set the cookie in the kretprobe as the symbol
+                // address is retrieved in the kprobe.
                 self.links
                     .push(prog_ret.attach_kprobe(true, symbol.attach_name())?);
             }

commit b940b50080aa8301067980653fe42719b7c39c34

probe: raw_tp: fix cookie support detection
Cookie support in raw tracepoints was done looking at if the
bpf_get_attach_cookie_tracing type existed, but this[1] actually precede the
introduction of the raw tp cookie support. Fix this by also checking if the
cookie field exist in the bpf_raw_tp_link struct instead, which was introduced
as part of the raw tp cookie support[2].

Fixes #560.

[1] 2fcc82411e74 ("bpf, x86: Attach a cookie to fentry/fexit/fmod_ret/lsm.").
[2] 68ca5d4eebb8 ("bpf: support BPF cookie in raw tracepoint (raw_tp, tp_btf)
    programs").

Fixes: d55a7f759dd1 ("core: probe: kernel: set the symbol address in the BPF cookie").
Signed-off-by: Antoine Tenart <atenart@redhat.com>

retis/src/core/probe/kernel/raw_tracepoint.rs
@@ -95,13 +95,20 @@ impl<'a> ProbeBuilder for RawTracepointBuilder<'a> {
 
 impl<'a> RawTracepointBuilder<'a> {
     // Checks whether the underlying kernel supports setting/retrieving cookies
-    // in raw tracepoints.
+    // in raw tracepoints. We both check the availability of the get cookie API
+    // as well as the cookie support itself, as the two are part of unrelated
+    // commits that could be backported separately.
     fn cookie_support() -> Result<bool> {
-        Ok(inspect::inspector()?
+        let get_cookie = inspect::inspector()?
             .kernel
             .btf
             .resolve_types_by_name("bpf_get_attach_cookie_tracing")
-            .is_ok())
+            .is_ok();
+        let raw_tp_cookie = inspect::parse_struct("bpf_raw_tp_link")?
+            .iter()
+            .any(|field| field == "cookie");
+
+        Ok(get_cookie && raw_tp_cookie)
     }
 
     fn init_skel(