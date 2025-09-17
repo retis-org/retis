e50c0e1a749a5d0ac574a91561bc1f88635efd11
Antoine Tenart <atenart@redhat.com>
2025-09-17 11:20:29 +0200

    events: allow reading rotated event data
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- retis/src/process/cli/print.rs
+++ retis/src/process/cli/print.rs
@@ -13,21 +13,20 @@ use clap::Parser;
 
 use crate::{
     cli::*,
-    events::{
-        file::{FileEventsFactory, FileType},
-        *,
-    },
-    helpers::signals::Running,
+    events::{file::*, *},
+    helpers::{file_rotate::*, signals::Running},
     process::display::*,
 };
 
 #[derive(Parser, Debug, Default)]
 #[command(name = "print", about = "Print stored events to stdout.")]
 pub(crate) struct Print {
-    /// File from which to read events.
-    #[arg(default_value = "retis.data")]
-    pub(super) input: PathBuf,
-    #[arg(long, help = "Format used when printing an event.")]
+    #[arg(help = "File from which to read events:
+- If a file name is given, it is read and processing stops at EOF. E.g. 'retis.data'.
+- If '..' is appended to a file name, it is read and if it is a split file following ones will be read at EOF (if any). E.g. 'retis.data.2..'.
+[default: 'retis.data..', then 'retis.data.0..']")]
+    pub(super) input: Option<PathBuf>,
+    #[arg(long, help = "Format used when printing an event")]
     #[clap(value_enum, default_value_t=CliDisplayFormat::MultiLine)]
     pub(super) format: CliDisplayFormat,
     #[arg(long, help = "Print the time as UTC")]
@@ -43,7 +42,7 @@ impl SubCommandParserRunner for Print {
         run.register_term_signals()?;
 
         // Create event factory.
-        let mut factory = FileEventsFactory::new(self.input.as_path())?;
+        let mut factory = factory_from_retis_data(self.input.as_ref())?;
 
         // Format.
         let format = DisplayFormat::new()
