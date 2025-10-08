185b57642fda849151d97b61e5b108603a547c43
Antoine Tenart <atenart@redhat.com>
2025-10-08 09:56:44 +0200

    retis_in_container: mount legacy kconfig as ro
    This aligns with all the other volumes, explicitly setting their access rights.
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- tools/retis_in_container.sh
+++ tools/retis_in_container.sh
@@ -42,7 +42,7 @@ for kconfig in /proc/config.gz \
         kconfig_map="$kconfig_map -v ${kconfig}:${kconfig}:ro"
 	# Map the first item to /kconfig to support older versions of the container
 	# image.
-	[[ -z $kconfig_legacy_map ]] && kconfig_legacy_map="-v ${kconfig}:/kconfig"
+	[[ -z $kconfig_legacy_map ]] && kconfig_legacy_map="-v ${kconfig}:/kconfig:ro"
     fi
 done
 if [[ -z $kconfig_map ]]; then
