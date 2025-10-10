retis-org/retis

commit a6dba52316d431e36b1732b3527694ea3d9f4ae6

retis_in_container: be less privileged
Make the security parameters less permissive while running Retis in a container
as this can run on production systems.

Signed-off-by: Antoine Tenart <atenart@redhat.com>

tools/retis_in_container.sh
@@ -4,7 +4,6 @@ set -e
 RETIS_IMAGE=${RETIS_IMAGE:-quay.io/retis/retis}
 RETIS_TAG=${RETIS_TAG:-latest}
 
-
 # Auto-detect the available runtime.
 if command -v podman >/dev/null; then
 	runtime=podman
@@ -64,9 +63,11 @@ if [ -f ${OVS_RUNDIR:-/var/run/openvswitch}/ovs-vswitchd.pid ]; then
 fi
 
 # Run the Retis container.
-exec $runtime run $extra_args $term_opts --privileged --rm --pid=host \
+exec $runtime run $extra_args $term_opts --rm --pid=host \
+      --cap-drop all --security-opt no-new-privileges --read-only --net none \
+      --cap-add SYS_ADMIN --cap-add BPF --cap-add CAP_PERFMON --cap-add SYSLOG \
+      --cap-add DAC_OVERRIDE --cap-add CAP_NET_ADMIN --cap-add SYS_PTRACE \
       -e PAGER -e NOPAGER -e TERM -e LC_ALL="C.UTF-8" \
-      --cap-add SYS_ADMIN --cap-add BPF --cap-add SYSLOG \
       -v /sys/kernel/btf:/sys/kernel/btf:ro \
       $tracefs $debugfs \
       -v $(pwd):/data:rw \

commit ca8204d48a40493ca55bf6e0ad7aa7b69f60dbd7

retis_in_container: fix mounting fs when using Docker and AppArmor
When using Docker as the runtime on an AppArmor enabled system, if
--allow-system-changes is enabled Retis can mount tracefs/debugfs. This will
fail with EACCES as the default Docker AppArmor profile does not allow mount
operations (as we run without --privileged).

Fix this by disabling the AppArmor profile when using Docker as the runtime,
limiting the behavior to when --allow-system-changes is enabled.

Fixes: a6dba52316d4 ("retis_in_container: be less privileged")
Signed-off-by: Antoine Tenart <atenart@redhat.com>

tools/retis_in_container.sh
@@ -17,6 +17,10 @@ if command -v podman >/dev/null; then
 elif command -v docker >/dev/null; then
 	docker pull $RETIS_IMAGE:$RETIS_TAG &>/dev/null
 	runtime=docker
+	# Mount permission is disabled in the default Docker AppArmor profile,
+	# but is needed to mount tracefs/debugfs.
+	[[ $@ =~ "--allow-system-changes" ]] && \
+		extra_args="--security-opt apparmor=unconfined"
 else
 	echo "No container runtime detected. Please install 'podman' or 'docker'."
 	exit -1