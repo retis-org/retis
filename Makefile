retis-org/retis

commit 89885ae48079a7462b82dc837d13213d048a1d7d

Makefile: set ebpf variables on demand
Some of the eBPF variables are computed in the main Makefile to avoid
duplicating things for every eBPF file being compiled. However those variables
are not used for other purposes than building eBPF objects and we can compute
then on-demand only when needed.

It's worth noting computing the eBPF architecture related variables depends on
external tools being available (rustc). It also takes some time.

Setting those variables on demand has three advantages:
- Stop depending on external tools to be available for all other targets.
- Speed up all other targets (`make help` too).
- Speed up target completion.

Signed-off-by: Antoine Tenart <atenart@redhat.com>

Makefile
@@ -4,8 +4,6 @@ OBJCOPY := llvm-objcopy
 
 CARGO := cargo $(CARGO_OPTS)
 BINDGEN := bindgen
-DEFAULT_ARCH := $(patsubst target_arch="%",%,$(filter target_arch="%",$(shell rustc --print cfg)))
-ARCH := $(if $(CARGO_BUILD_TARGET),$(firstword $(subst -, ,$(CARGO_BUILD_TARGET))),$(DEFAULT_ARCH))
 
 define retis_set_version
 RELEASE_VERSION := $(shell tools/localversion)
@@ -15,6 +13,10 @@ endef
 
 RELEASE_FLAGS = -Dwarnings
 
+define ebpf_set_vars
+DEFAULT_ARCH := $(patsubst target_arch="%",%,$(filter target_arch="%",$(shell rustc --print cfg)))
+ARCH := $(if $(CARGO_BUILD_TARGET),$(firstword $(subst -, ,$(CARGO_BUILD_TARGET))),$$(DEFAULT_ARCH))
+
 # Needs to be set because of PT_REGS_PARMx() and any other target
 # specific facility.
 x86_64 := x86
@@ -22,7 +24,7 @@ aarch64 := arm64
 powerpc64 := powerpc
 s390x := s390
 # Mappings takes precedence over custom ARCH
-BPF_ARCH := $(if $($(ARCH)),$($(ARCH)),$(ARCH))
+BPF_ARCH := $(if $$($$(ARCH)),$$($$(ARCH)),$$(ARCH))
 
 BPF_CFLAGS_PAHOLE := \
 	-Wno-gnu-variable-sized-type-not-at-end
@@ -34,10 +36,11 @@ BPF_CFLAGS := -target bpf \
               -Wno-pointer-sign \
               -Wno-compare-distinct-pointer-types \
               -Wno-unused-command-line-argument \
-              $(BPF_CFLAGS_PAHOLE) \
+              $$(BPF_CFLAGS_PAHOLE) \
               -fno-stack-protector \
-              -D__TARGET_ARCH_$(BPF_ARCH) \
+              -D__TARGET_ARCH_$$(BPF_ARCH) \
               -O2
+endef
 
 export LCC OBJCOPY
 
@@ -175,7 +178,10 @@ $(LIBBPF_INCLUDES): $(LIBBPF_SYS_LIBBPF_INCLUDES)
 	cp $^ $(LIBBPF_INCLUDES)/bpf/
 endif
 
-ebpf: $(EBPF_PROBES) $(EBPF_HOOKS)
+ebpf_prereqs:
+	$(eval $(call ebpf_set_vars))
+
+ebpf: ebpf_prereqs $(EBPF_PROBES) $(EBPF_HOOKS)
 
 $(EBPF_PROBES): OUT_NAME := PROBE
 $(EBPF_HOOKS):  OUT_NAME := HOOK
@@ -316,6 +322,6 @@ help:
 	$(call help_once, COV                   --  Enable code coverage for testing. Applies only to the target "test".)
 	$(call help_once,                           Requires llvm-cov and preferably rustup toolchain.)
 
-.PHONY: all bench ebpf $(EBPF_PROBES) $(EBPF_HOOKS) gen-bindings help install release pylib report-cov wireshark wireshark-install wireshark-clean
+.PHONY: all bench ebpf ebpf_prereqs $(EBPF_PROBES) $(EBPF_HOOKS) gen-bindings help install release pylib report-cov wireshark wireshark-install wireshark-clean
 .PHONY: test pytest-deps pytest lint-ebpf functional-tests functional-tests-list lint-rust lint-python lints
 .PHONY: clean clean-bindings clean-cov clean-ebpf

commit 466a1fd03d6547fd7fe684f3c86e05d341b90283

Makefile: Include ebpf_prereqs in gen-bindings prereqs list.
Since 89885ae48079 ("Makefile: set ebpf variables on demand")
binding generation was broken because it requires ebpf flags
and target architecture.
The cited commit overlooked that target while including the new
prerequisite. Fix it by adding ebpf_prereqs to gen-bindings.

While at it, replace the underscore with a dash in ebpf_prereqs
for consistency with the current naming adopted for targets/prereqs.

Fixes: 89885ae48079 ("Makefile: set ebpf variables on demand")
Signed-off-by: Paolo Valerio <pvalerio@redhat.com>

Makefile
@@ -101,7 +101,7 @@ install: release
 
 # Skip out path and vmlinux.h
 BINDINGS := $(shell find $(ROOT_DIR)/retis/src -name '*.[ch]' -not -name 'vmlinux.h' -not -path '*/.out/*' -exec grep -l "__binding" {} \;)
-gen-bindings: clean-bindings
+gen-bindings: clean-bindings ebpf-prereqs
 	-mkdir -p $(ROOT_DIR)/retis/src/bindings; \
 	for binding in $(BINDINGS); do \
 	    $(call out_console,BINDINGS,processing $$binding ...); \
@@ -176,10 +176,10 @@ $(LIBBPF_INCLUDES): $(LIBBPF_SYS_LIBBPF_INCLUDES)
 	cp $^ $(LIBBPF_INCLUDES)/bpf/
 endif
 
-ebpf_prereqs:
+ebpf-prereqs:
 	$(eval $(call ebpf_set_vars))
 
-ebpf: ebpf_prereqs $(EBPF_PROBES) $(EBPF_HOOKS)
+ebpf: ebpf-prereqs $(EBPF_PROBES) $(EBPF_HOOKS)
 
 $(EBPF_PROBES): OUT_NAME := PROBE
 $(EBPF_HOOKS):  OUT_NAME := HOOK
@@ -320,6 +320,6 @@ help:
 	$(call help_once, COV                   --  Enable code coverage for testing. Applies only to the target "test".)
 	$(call help_once,                           Requires llvm-cov and preferably rustup toolchain.)
 
-.PHONY: all bench ebpf ebpf_prereqs $(EBPF_PROBES) $(EBPF_HOOKS) gen-bindings help install release pylib report-cov wireshark wireshark-install wireshark-clean
+.PHONY: all bench ebpf ebpf-prereqs $(EBPF_PROBES) $(EBPF_HOOKS) gen-bindings help install release pylib report-cov wireshark wireshark-install wireshark-clean
 .PHONY: test pytest-deps pytest lint-ebpf functional-tests functional-tests-list lint-rust lint-python lints
 .PHONY: clean clean-bindings clean-cov clean-ebpf