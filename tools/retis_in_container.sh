8313fc2977f9e4c9e72cf5b074e858bf3c1c6520
Antoine Tenart <atenart@redhat.com>
2026-02-12 14:12:52 +0100

    retis_in_container: refactor argument parsing
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- tools/retis_in_container.sh
+++ tools/retis_in_container.sh
@@ -4,6 +4,8 @@ set -e
 RETIS_IMAGE=${RETIS_IMAGE:-quay.io/retis/retis}
 RETIS_TAG=${RETIS_TAG:-latest}
 
+term_opts="-it"
+
 errcho() {
 	>&2 echo $*
 }
@@ -12,29 +14,43 @@ errcho() {
 if command -v podman >/dev/null; then
 	runtime=podman
 	extra_args="--pull=newer"
-
-	# Retis cannot run in an unprivileged container to collect events.
-	if [[ $(id -u) -ne 0 && $@ =~ "collect" ]]; then
-		errcho "Error: Retis collection cannot run in an unprivileged container."
-		exit -1
-	fi
 elif command -v docker >/dev/null; then
 	docker pull $RETIS_IMAGE:$RETIS_TAG &>/dev/null
 	runtime=docker
-	# Mount permission is disabled in the default Docker AppArmor profile,
-	# but is needed to mount tracefs/debugfs.
-	[[ $@ =~ "--allow-system-changes" ]] && \
-		extra_args="--security-opt apparmor=unconfined"
 else
 	errcho "No container runtime detected. Please install 'podman' or 'docker'."
 	exit -1
 fi
 
-# We can't use a pseudo-tty (see `-t` option in `man podman run`) when using
-# a command outputting a specific format to stdout (which could be piped into
-# another utility parsing it), like the pcap command. This is because an extra
-# EOL char is added (see commit 9f3361ac39c3).
-[[ ! $@ =~ "pcap" ]] && term_opts="-it"
+# Parse (w/o enforcing validity) Retis arguments to allow custom actions.
+while [ $# -gt 0 ]; do
+	case $1 in
+	collect)
+		# Retis cannot run in an unprivileged Podman container to collect events.
+		if [[ $runtime == "podman" && $(id -u) -ne 0 ]]; then
+			errcho "Error: Retis collection cannot run in an unprivileged container."
+			exit -1
+		fi
+		;;
+	--allow-system-changes)
+		# Mount permission is disabled in the default Docker AppArmor profile,
+		# but is needed to mount tracefs/debugfs.
+		[ $runtime == "docker" ] && extra_args="--security-opt apparmor=unconfined"
+		;;
+	pcap)
+		# We can't use a pseudo-tty (see `-t` option in `man podman
+		# run`) when using a command outputting a specific format to
+		# stdout (which could be piped into another utility parsing
+		# it), like the pcap command. This is because an extra EOL char
+		# is added (see commit 9f3361ac39c3).
+		term_opts=""
+		;;
+	*)	;;
+	esac
+
+	cmdline="$cmdline $1"
+	shift
+done
 
 # Map well-known kernel configuration files.
 kconfig_map=""
@@ -84,4 +100,4 @@ exec $runtime run $extra_args $term_opts --rm --pid=host \
       $local_conf \
       $ovs_binary_mount \
       $ovs_rundir_mount \
-      $RETIS_IMAGE:$RETIS_TAG "$@"
+      $RETIS_IMAGE:$RETIS_TAG $cmdline
