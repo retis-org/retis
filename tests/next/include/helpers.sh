7b88fa647719307b1b6e46724d941797535f7b57
Antoine Tenart <atenart@redhat.com>
2026-02-18 18:10:46 +0100

    tests: convert tracking test to the next infra
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- tests/next/include/helpers.sh
+++ tests/next/include/helpers.sh
@@ -31,3 +31,30 @@ two_ns_vlan() {
 	ip -net ns0 address add 2222::1/64 dev veth01.123
 	ip -net ns1 address add 2222::2/64 dev veth10.123
 }
+
+# Create three network namespaces, with veth pairs. Configuration is the
+# following:
+# ns0 - veth01 - 10.0.42.1/24 - 1111::1/64
+# ns1 - veth10 - 10.0.42.2/24 - 1111::2/64
+# ns1 - veth12 - 10.0.43.1/24 - 2222::1/64
+# ns2 - veth21 - 10.0.43.2/24 - 2222::2/64
+#
+# The correct routes are installed in ns0 and ns2. Forwarding is enabled on ns1.
+three_ns() {
+	two_ns
+
+	add_ns ns2
+	add_veth_pair veth12 ns1 veth21 ns2
+	ip -net ns1 address add 10.0.43.1/24 dev veth12
+	ip -net ns2 address add 10.0.43.2/24 dev veth21
+	ip -net ns1 address add 2222::1/64 dev veth12
+	ip -net ns2 address add 2222::2/64 dev veth21
+
+	ip -net ns0 route add 10.0.43.0/24 via 10.0.42.2
+	ip -net ns2 route add 10.0.42.0/24 via 10.0.43.1
+	ip -net ns0 route add 2222::/64 via 1111::2
+	ip -net ns2 route add 1111::/64 via 2222::1
+
+	ip netns exec ns1 sysctl -w net.ipv4.conf.all.forwarding=1
+	ip netns exec ns1 sysctl -w net.ipv6.conf.all.forwarding=1
+}
---
+++ tests/next/tracking.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+source $(dirname $0)/include/lib.sh
+source $(dirname $0)/include/helpers.sh
+
+tracking_sanity() {
+	three_ns
+
+	# Add a DNAT rule and a route in ns0.
+	ip -net ns0 route add 10.0.255.1/32 via 10.0.42.2
+	ip netns exec ns1 nft -f - <<EOF
+table nat {
+        chain prerouting {
+                type nat hook prerouting priority -100;
+                ip daddr 10.0.255.1/32 dnat to 10.0.43.2
+        }
+}
+EOF
+
+	$retis collect -o -c skb,skb-tracking,dev,ns \
+		-f "icmp and host 10.0.255.1" \
+		-p net:netif_rx \
+		--cmd "ip netns exec ns0 ping -c1 10.0.255.1; sleep 0.1"
+
+	cat >test.py <<EOF
+from helpers import assert_events_present
+
+expected_events = [
+    {
+        "dev": {
+            "name": "veth10",
+        },
+        "parsed_packet": {
+            "icmp": {
+                "code": "0",
+                "type": "echo-request",
+            },
+            "ip": {
+                "src": "10.0.42.1",
+                "dst": "10.0.255.1",
+            },
+        },
+        "skb-tracking": {
+            "orig_head": "&orig_head",
+            "timestamp": "&timestamp",
+            "skb": "&skb",
+        },
+    },
+    {
+        "dev": {
+            "name": "veth21",
+        },
+        "parsed_packet": {
+            "icmp": {
+                "code": "0",
+                "type": "echo-request",
+            },
+            "ip": {
+                "src": "10.0.42.1",
+                "dst": "10.0.43.2",
+            },
+        },
+        "skb-tracking": {
+            "orig_head": "*orig_head",
+            "timestamp": "*timestamp",
+            "skb": "*skb",
+        },
+    },
+    {
+        "dev": {
+            "name": "veth01",
+        },
+        "parsed_packet": {
+            "icmp": {
+                "code": "0",
+                "type": "echo-reply",
+            },
+            "ip": {
+                "src": "10.0.255.1",
+                "dst": "10.0.42.1",
+            },
+        },
+        "skb-tracking": {
+            "orig_head": "!orig_head",
+            "timestamp": "!timestamp",
+        },
+    },
+]
+
+assert_events_present("retis.data", expected_events)
+EOF
+	python test.py
+}
+
+run_tests tracking_sanity
